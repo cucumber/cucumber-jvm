32e82dca4156f9f923f02197a57f4d666712ccdb
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-04-01 16:44:54 +0200

    Cleanup

--- examples/simple/features/support/env.rb
+++ examples/simple/features/support/env.rb
@@ -1,15 +1,3 @@
-# Cucumber Java depends on PicoContainer to instantiate classes with Step definitions
-require File.expand_path('~/.m2/repository/org/picocontainer/picocontainer/2.8/picocontainer-2.8.jar')
-
-# Load Cucumber Java support
 require 'cucumber/java'
 
-# Load JUnit - our Step definitions use it for assertions
-require File.expand_path('~/.m2/repository/junit/junit/4.4/junit-4.4.jar')
-
-# Load our step definitions
-require File.expand_path(File.dirname(__FILE__) + '/../../target/cucumber-simple-example-1.0.0.jar')
-
-# Register our steps definitions.
-import 'simple.StuffSteps'
-register_steps(StuffSteps)
+register_steps(Java::simple.StuffSteps)
