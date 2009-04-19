5f8f31b3d2b34050a93e27990ed25ce9274f49ac
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-04-20 00:07:47 +0200

    Fix Ambiguous problem for Cucumber Java

--- examples/simple/features/demo.feature
+++ examples/simple/features/demo.feature
@@ -6,3 +6,13 @@ Feature: Simple
       |a|b|
       |1|2|
     Then I should have 3 green cukes
+    
+  Scenario: 4 cukes
+    Given I have 4 green cukes
+    When I add a table
+      |a|b|
+      |1|2|
+    Then I should have 4 green cukes
+    
+  Scenario: 99 cukes
+    Given I have 99 green cukes
--- examples/simple/features/support/env.rb
+++ examples/simple/features/support/env.rb
@@ -1,3 +1,7 @@
+#require File.expand_path('~/.m2/repository/org/picocontainer/picocontainer/2.8/picocontainer-2.8.jar')
+#require File.expand_path('~/.m2/repository/junit/junit/4.4/junit-4.4.jar')
+#require File.expand_path(File.dirname(__FILE__) + '/../../target/cucumber-simple-example-1.0.0.jar')
+
 require 'cucumber/java'
 
 register_steps(Java::simple.StuffSteps)
