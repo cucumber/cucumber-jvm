e588abd12d7fbf95cdd9386857da82db01effb61
Torbjørn Vatn (jobb) <torbjorn@vatn.org>
2009-04-21 12:48:45 +0200

    Loading the StuffSteps in the simple example directly from the .class file

--- examples/simple/features/support/env.rb
+++ examples/simple/features/support/env.rb
@@ -1,6 +1,6 @@
-#require File.expand_path('~/.m2/repository/org/picocontainer/picocontainer/2.8/picocontainer-2.8.jar')
-#require File.expand_path('~/.m2/repository/junit/junit/4.4/junit-4.4.jar')
-#require File.expand_path(File.dirname(__FILE__) + '/../../target/cucumber-simple-example-1.0.0.jar')
+require File.expand_path('~/.m2/repository/org/picocontainer/picocontainer/2.8/picocontainer-2.8.jar')
+require File.expand_path('~/.m2/repository/junit/junit/4.4/junit-4.4.jar')
+$CLASSPATH << File.dirname(__FILE__) + '/../../target/classes'
 
 require 'cucumber/java'
 
