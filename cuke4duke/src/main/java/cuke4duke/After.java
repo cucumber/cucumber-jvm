f6d0fba9af09f659c8c1893a34e82c354d653f11
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-09-01 06:19:14 +0200

    @Before tag should have optional scenario parameter. Closes #11

--- cuke4duke/src/main/java/cuke4duke/After.java
+++ cuke4duke/src/main/java/cuke4duke/After.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface After {
-    String value();
+    String value() default "";
 }
