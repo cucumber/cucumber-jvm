5ae8cb79fc3ff3a2756622e544d9e1489ad2570a
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-10-04 00:35:52 +0200

    Better backtrace filtering

--- cuke4duke/src/main/resources/cucumber/jvm_support/backtrace_filter.rb
+++ cuke4duke/src/main/resources/cucumber/jvm_support/backtrace_filter.rb
@@ -1,1 +1,1 @@
-Cucumber::Ast::StepInvocation::BACKTRACE_FILTER_PATTERNS << /cuke4duke|org\/jruby|org\/junit|org\/hamcrest|java\/lang|sun\/reflect/
+Cucumber::Ast::StepInvocation::BACKTRACE_FILTER_PATTERNS << /cuke4duke|org\/jruby|org\/junit|org\/hamcrest|java\/lang|java\.lang|sun\/reflect|sun\.reflect/
