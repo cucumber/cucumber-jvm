fb029a2f3b788e970e3b0999808c23eb941d0da5
Torbjørn Vatn (jobb) <torbjorn@vatn.org>
2009-10-19 08:19:12 +0200

    Added for sceanrios to transform.feature
    !! I broke the groovy example !!

--- examples/java/features/transform.feature
+++ examples/java/features/transform.feature
@@ -4,11 +4,17 @@ Feature: Step argument transformations
   As a step definition editor
   I want to register a @Transform annotated method to capture and tranform step definition arguments.
 
-  Scenario: transform with matches
+  Scenario: transform with default match
     Given I pass '10' to a method with int as parameter
     When something happens
     Then all is good
-
+    
+  Scenario: transform with custom match
+    Given I pass '10' to a method with User as parameter
+    When something happens
+    Then a User with age '10' is created
       
-  Scenario: transform without matches
+  Scenario: transform without match
     Given I pass '10' to a method with Car as parameter
+    When something happens
+    Then an exception is thrown
--- examples/java/src/test/java/simple/TransformSteps.java
+++ examples/java/src/test/java/simple/TransformSteps.java
@@ -1,29 +1,79 @@
 package simple;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 import cuke4duke.Given;
+import cuke4duke.StepMother;
+import cuke4duke.Steps;
 import cuke4duke.Then;
+import cuke4duke.Transform;
 import cuke4duke.When;
-public class TransformSteps {
+
+public class TransformSteps extends Steps {
     
+    private boolean exceptionThrown = false;
+    private User user;
+
+    public TransformSteps(StepMother stepMother) {
+        super(stepMother);
+    }
+
     @Given("^I pass '(.*)' to a method with int as parameter$")
-    public void transformedToA(int value) {
+    public void transformToA(int value) {
         assertEquals(10, value);
     }
-    
+
     @Given("^I pass '(.*)' to a method with Car as parameter$")
-    public void transformedToA(Car value) {
-        assertEquals(10, value);
+    public void transformToA(String value) {
+        try {
+            Given("pass '(.*)' as a Car", value);
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+    }
+    
+    @Given("^pass '(.*)' as a Car$")
+    public void passACar(Car value) {
+    }
+    
+    @Transform
+    public User transformStringToUserWithAge(String age) {
+        return new User(Integer.valueOf(age));
+    }
+    
+    @Given("^I pass '(.*)' to a method with User as parameter$")
+    public void transformToA(User user) {
+        this.user = user;
     }
     
     @When("^something happens$")
     public void somethingHappens() {
     }
+
     @Then("^all is good$")
     public void allIsGood() {
+        assertFalse(exceptionThrown);
     }
     
+    @Then("^a User with age '(.*)' is created$")
+    public void aUserWithAgeIsCreated(int age) {
+        assertTrue(this.user.age == age);
+    }
+    
+    @Then("^an exception is thrown$")
+    public void exceptionIsThrown() {
+        assertTrue(exceptionThrown);
+    }
+
     private class Car {
-        
+    }
+    
+    private class User {
+        public final int age;
+
+        public User(int age){
+            this.age = age;
+        }
     }
 }
