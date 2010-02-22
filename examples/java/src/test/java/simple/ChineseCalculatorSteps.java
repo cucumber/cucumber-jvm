0ce587a5279d239c74411a64b065f12e646f21de
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2010-02-22 12:51:41 +0100

    Added a chinese example, just to verify that i18n step definitions work (they do).

---
+++ examples/java/src/test/java/simple/ChineseCalculatorSteps.java
@@ -0,0 +1,18 @@
+package simple;
+
+import cuke4duke.annotation.ZHCN.*;
+
+public class ChineseCalculatorSteps {
+
+    @假如("^我已经在计算器里输入(\\d+)$")
+    public void 我已经在计算器里输入(int n) {
+    }
+
+    @当("^我按相加按钮$")
+    public void 我按相加按钮() {
+    }
+
+    @那么("^我应该在屏幕上看到的结果是(\\d+)$")
+    public void 我应该在屏幕上看到的结果是(int n) {
+    }
+}
