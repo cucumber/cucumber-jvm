a239626535e70fcd43b1b5b07eaa721ba1c37ecd
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-10-07 17:15:27 +0200

    Moved logic from Runtime (facade) to ExecuteFormatter

--- cuke4duke/src/main/java/cucumber/Runtime.java
+++ cuke4duke/src/main/java/cucumber/Runtime.java
@@ -1,33 +1,17 @@
 package cucumber;
 
 import cucumber.runtime.ExecuteFormatter;
-import cucumber.runtime.StepMatch;
 import gherkin.FeatureParser;
 import gherkin.GherkinParser;
-import gherkin.formatter.Argument;
 import gherkin.formatter.PrettyFormatter;
-import gherkin.formatter.model.BasicStatement;
-import gherkin.formatter.model.Feature;
-import gherkin.formatter.model.Step;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Runtime {
-    private final List<StepDefinition> stepDefinitions;
-    private final PrettyFormatter formatter;
     private final FeatureParser parser;
-    private final List<Step> steps = new ArrayList<Step>();
-
-    private BasicStatement featureElement;
-    private String uri;
-    private Feature feature;
 
     public Runtime(List<StepDefinition> stepDefinitions, PrettyFormatter formatter) {
-        this.stepDefinitions = stepDefinitions;
-        this.formatter = formatter;
-        ExecuteFormatter executeFormatter = new ExecuteFormatter(this, formatter);
+        ExecuteFormatter executeFormatter = new ExecuteFormatter(stepDefinitions, formatter);
         parser = new GherkinParser(executeFormatter);
     }
 
@@ -37,38 +21,5 @@ public class Runtime {
 
     public void execute(String source, String location) {
         parser.parse(source, location, 0);
-    }
-
-    public void uri(String uri) {
-        this.uri = uri;
-    }
-
-
-    public void feature(Feature feature) {
-        this.feature = feature;
-    }
-
-    public void featureElement(BasicStatement featureElement) {
-        this.featureElement = featureElement;
-    }
-
-    public void step(Step step) {
-        steps.add(step);
-    }
-    
-    public void replay() {
-        if(featureElement != null) {
-            List<List<String>> stepStrings = new ArrayList<List<String>>();
-            for(Step step: steps) {
-                stepStrings.add(Arrays.asList(step.getKeyword(), step.getName()));
-            }
-            formatter.steps(stepStrings);
-
-            featureElement.replay(formatter);
-            for(Step step: steps) {
-                List<Argument> arguments = Arrays.asList(new Argument(7, "3"));
-                new StepMatch(stepDefinitions.get(0), arguments, step, uri, feature.getName(), featureElement.getName()).execute(formatter);
-            }
-        }
     }
 }
--- cuke4duke/src/main/java/cucumber/runtime/ExecuteFormatter.java
+++ cuke4duke/src/main/java/cucumber/runtime/ExecuteFormatter.java
@@ -1,33 +1,41 @@
 package cucumber.runtime;
 
-import cucumber.Runtime;
+import cucumber.StepDefinition;
+import gherkin.formatter.Argument;
 import gherkin.formatter.Formatter;
 import gherkin.formatter.PrettyFormatter;
 import gherkin.formatter.model.Background;
+import gherkin.formatter.model.DescribedStatement;
 import gherkin.formatter.model.Examples;
 import gherkin.formatter.model.Feature;
 import gherkin.formatter.model.Scenario;
 import gherkin.formatter.model.ScenarioOutline;
 import gherkin.formatter.model.Step;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ExecuteFormatter implements Formatter {
+    private final List<StepDefinition> stepDefinitions;
     private final PrettyFormatter formatter;
-    private final cucumber.Runtime runtime;
+    private String uri;
+    private Feature feature;
+    private DescribedStatement featureElement;
+    private final List<Step> steps = new ArrayList<Step>();
 
-    public ExecuteFormatter(Runtime runtime, PrettyFormatter formatter) {
-        this.runtime = runtime;
+    public ExecuteFormatter(List<StepDefinition> stepDefinitions, PrettyFormatter formatter) {
+        this.stepDefinitions = stepDefinitions;
         this.formatter = formatter;
     }
 
     public void uri(String uri) {
-        runtime.uri(uri);
+        this.uri = uri;
         formatter.uri(uri);
     }
 
     public void feature(Feature feature) {
-        runtime.feature(feature);
+        this.feature = feature;
         formatter.feature(feature);
     }
 
@@ -36,12 +44,13 @@ public class ExecuteFormatter implements Formatter {
     }
 
     public void scenario(Scenario scenario) {
-        runtime.replay();
-        runtime.featureElement(scenario);
+        replay();
+        featureElement = scenario;
     }
 
     public void scenarioOutline(ScenarioOutline scenarioOutline) {
-        formatter.scenarioOutline(scenarioOutline);
+        replay();
+        featureElement = scenarioOutline;
     }
 
     public void examples(Examples examples) {
@@ -49,15 +58,31 @@ public class ExecuteFormatter implements Formatter {
     }
 
     public void step(Step step) {
-        runtime.step(step);
+        steps.add(step);
     }
 
     public void eof() {
-        runtime.replay();
+        replay();
         formatter.eof();
     }
 
     public void syntaxError(String state, String event, List<String> legalEvents, String uri, int line) {
         formatter.syntaxError(state, event, legalEvents, uri, line);
+    }
+
+    private void replay() {
+        if(featureElement != null) {
+            List<List<String>> stepStrings = new ArrayList<List<String>>();
+            for(Step step: steps) {
+                stepStrings.add(Arrays.asList(step.getKeyword(), step.getName()));
+            }
+            formatter.steps(stepStrings);
+
+            featureElement.replay(formatter);
+            for(Step step: steps) {
+                List<Argument> arguments = Arrays.asList(new Argument(7, "3"));
+                new StepMatch(stepDefinitions.get(0), arguments, step, uri, feature.getName(), featureElement.getName()).execute(formatter);
+            }
+        }
     }
 }
