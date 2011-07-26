ba699ae8d8d11854cccaf0a32a4f9f16d5257938
cedric-lamalle <cedric.lamalle@gmail.com>
2011-07-25 22:43:59 -0300

    Adding new Transformable

---
+++ core/src/main/java/cucumber/runtime/transformers/BigDecimalTransformable.java
@@ -0,0 +1,13 @@
+package cucumber.runtime.transformers;
+
+import java.math.BigDecimal;
+import java.util.Locale;
+
+public class BigDecimalTransformable implements Transformable<BigDecimal> {
+
+	public BigDecimal transform(String argument, Locale locale) {
+		
+		return null;
+	}
+
+}
--- core/src/test/java/cucumber/runtime/transformers/StandardTransformersTest.java
+++ core/src/test/java/cucumber/runtime/transformers/StandardTransformersTest.java
@@ -1,8 +1,12 @@
 package cucumber.runtime.transformers;
 
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class StandardTransformersTest {
@@ -19,5 +23,35 @@ public class StandardTransformersTest {
 	@Test(expected = TransformationException.class)
 	public void shouldThrowTransformationExceptionWhenConvertingBoolean() {
 		new BooleanTransformable().transform("vrai", Locale.ENGLISH);
+	}
+	
+	@Test
+	@Ignore("wip")
+	public void shouldTransformBigDecimal() {
+		BigDecimal englishBigDecimal = new BigDecimalTransformable().transform("300.15", Locale.ENGLISH);
+		BigDecimal frenchBigDecimal = new BigDecimalTransformable().transform("300,15", Locale.FRENCH);
+		Assert.assertEquals(new BigDecimal("300.15"), englishBigDecimal);
+		Assert.assertEquals(new BigDecimal("300.15"), frenchBigDecimal);
+	}
+
+	@Test
+	public void shouldTransformDate() {
+		Assert.assertEquals(getDateToTest(),
+				new DateTransformable().transform("11/29/2011", Locale.ENGLISH));
+		Assert.assertEquals(getDateToTest(),
+				new DateTransformable().transform("29/11/2011", Locale.FRENCH));
+	}
+
+	@Test(expected = TransformationException.class)
+	public void shouldThrowTransformationExceptionWhenConvertingDate() {
+		Assert.assertEquals(getDateToTest(),
+				new DateTransformable().transform("29/11/2011", Locale.ENGLISH));
+	}
+
+	private Date getDateToTest() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2011, 10, 29, 0, 0, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		return calendar.getTime();
 	}
 }
