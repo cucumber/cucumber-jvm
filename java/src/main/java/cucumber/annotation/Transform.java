e6cbe1d7f2e548064d09f0212d98043102322a15
cedric-lamalle <cedric.lamalle@gmail.com>
2011-08-04 23:53:44 -0300

    Documenting @Transform

--- java/src/main/java/cucumber/annotation/Transform.java
+++ java/src/main/java/cucumber/annotation/Transform.java
@@ -1,12 +1,20 @@
 package cucumber.annotation;
 
-import cucumber.runtime.java.CucumberAnnotation;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import cucumber.runtime.java.CucumberAnnotation;
+
+/**
+ * 
+ * Annotation to mark methods that can transform Strings arguments of steps to a
+ * higher level, domain specific object.
+ * 
+ * @see cucumber.runtime.transformers.Transformable<T>
+ * 
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 @CucumberAnnotation("en")
