a0fe4c766ea880389c9d4017414d372f30b3475f
cedric.lamalle <cedric.lamalle@gmail.com>
2011-08-21 11:15:14 -0300

    Fixing merge problemin @Transform

--- java/src/main/java/cucumber/annotation/Transform.java
+++ java/src/main/java/cucumber/annotation/Transform.java
@@ -5,16 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import cucumber.runtime.java.CucumberAnnotation;
-
-/**
- * 
- * Annotation to mark methods that can transform Strings arguments of steps to a
- * higher level, domain specific object.
- * 
- * @see cucumber.runtime.transformers.Transformable<T>
- * 
- */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Transform {
