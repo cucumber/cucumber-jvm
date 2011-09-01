62eefac489cb1d7848fa94be9b010b6780e1a67d
jbandi <dev@jonasbandi.net>
2011-09-01 12:31:14 +0200

    Merge branch 'master', remote-tracking branch 'upstream/master'

---
+++ examples/java-ejb3/src/main/java/cucumber/examples/java/calculator/Book.java
@@ -0,0 +1,11 @@
+package cucumber.examples.java.calculator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jbandi
+ * Date: 8/30/11
+ * Time: 5:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Book {
+}
