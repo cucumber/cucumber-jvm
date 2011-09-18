0ed7444790d95f91caf9535da73261aaba3b6535
Cedric Lamalle <cedric.lamalle@gmail.com>
2011-09-18 19:43:55 -0300

    Starting work on the js formatter Should be easy to use existing json formatter or Statement.toMap() with it. Added some microdata properties. See #42

---
+++ core/src/main/resources/cucumber/formatter/formatter.js
@@ -0,0 +1,88 @@
+var Cucumber = {};
+
+Cucumber.DOMFormatter = function(rootNode) {
+    var rootNode = rootNode;
+    var featureElementsNode;
+    var scenarioElementsNode;
+    var currentNode;
+
+    this.feature = function(feature) {
+        currentNode = $('#templates .blockelement').clone().appendTo(rootNode);
+        currentNode.addClass('feature');
+        printStatement(feature, '<h1>');
+        featureElementsNode = currentNode.find('.childrenElements');
+        featureElementsNode.addClass('featureElements');
+    }
+
+    this.background = function(background) {
+        currentNode = $('#templates .blockelement').clone().appendTo(featureElementsNode);
+        currentNode.addClass('background');
+        printStatement(background, '<h2>');
+        scenarioElementsNode = currentNode.find('.childrenElements');
+        scenarioElementsNode.addClass('steps');
+    }
+
+    this.scenario = function(scenario) {
+        currentNode = $('#templates .blockelement').clone().appendTo(featureElementsNode);
+        currentNode.addClass('scenario');
+        printStatement(scenario, '<h2>');
+        scenarioElementsNode = currentNode.find('.childrenElements');
+        scenarioElementsNode.addClass('steps');
+    }
+
+    this.scenarioOutline = function(outline) {
+        this.scenario(outline);
+        currentNode.addClass('outline');
+    }
+
+    this.step = function(step) {
+        currentNode = $('#templates .step').clone().appendTo(scenarioElementsNode);
+        currentNode.attr('id', step.id);
+        printStatement(step, '<h3>');
+        printStepExamples(step);
+    }
+    
+    this.examples = function(examples) {
+        currentNode = $('#templates .examples').clone().appendTo(featureElementsNode);
+        printStatement(examples, '<h2>');
+        printExamples(examples.rows, currentNode.find('.examples'));
+    }
+    
+    var hasExamples = function(step) {
+        return step.multiline_arg !== undefined && step.multiline_arg.type === 'table';
+    }
+    
+    var printExamples = function(examples, node) {
+        var table = $('<table></table>').appendTo(node);
+        table.addClass('examples');
+        $.each(examples, function(index, example) {
+            var tr = $('<tr></tr>').appendTo(table);
+            tr.addClass('exampleRow');
+            $.each(example.cells,function(index, cell) {
+                var td = $('<td></td>').appendTo(tr);
+                td.addClass('exampleCell');
+                td.text(cell);
+            });
+        });
+    }
+
+    var printStatement = function(statement, heading) {
+        currentNode.find('.keyword').text(statement.keyword);
+        currentNode.find('.name').text(statement.name);
+        if (statement.description !== undefined) {
+            currentNode.find('.description').text(statement.description);
+        } else {
+            currentNode.find('.description').remove();
+        }
+        currentNode.find('header').wrapInner(heading);
+    }
+    
+    var printStepExamples = function(step) {
+        if (hasExamples(step)) {
+            printExamples(step.multiline_arg.value, currentNode.find('.examples'));
+        } else {
+            currentNode.find('.examples').remove();
+        }
+    }
+
+}
--- core/src/main/resources/cucumber/formatter/report.js
+++ core/src/main/resources/cucumber/formatter/report.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-    function feature(keyword, title, description) {
-        var e = $('#templates .feature').clone().appendTo('body');
-        e.find('.keyword').text(keyword);
-        e.find('.title').text(title);
-        e.find('.description').text(description);
-    }
-
-    feature('Feature', 'English', 'Hello\nWorld');
-    feature('Feature', 'French', 'Bonjour\nMonde');
+    var formatter = new Cucumber.DOMFormatter($('body'));
+    formatter.feature({keyword:'Feature', name:'Generating html report', description: 'It could be useful to have an html report to facilitate documentation reading.'});
+    formatter.background({keyword:'Background', name:'Setting up the context', description: 'These steps will be executed before each scenario.'});
+    formatter.step({keyword:'Given', name:'I have a background'});
+    formatter.step({keyword:'And', name:'I set some context'});
+    formatter.scenario({keyword:'Scenario', name: 'Creating a simple report'});
+    formatter.step({keyword:'Given', name:'I have a feature'});
+    formatter.step({keyword:'When', name:'I format it'});
+    formatter.step({keyword:'Then', name:'It should look pretty'});
+    formatter.step({keyword:'And', name:'It should show tables', multiline_arg:{type:'table', value: [{cells:['name', 'price'], line:4}, {cells:['milk', '9'], line:5}]}});
+    formatter.scenarioOutline({keyword:'Scenario Outline', name: 'Scenario with examples', description:'It should be good to format outlined arguments.'});
+    formatter.step({keyword:'Given', name:'I have a <name> which costs <price>'});
+    formatter.examples({description:'', name:'Some good examples', keyword:'Examples', line:'10', rows:[{cells:['name', 'price'], line:11}, {cells:['milk', '9'], line:12}, {cells:['bread', '7'], line:13}, {cells:['soap', '5'], line:14}]})
 });
