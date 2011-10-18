ed96fe12fe54ca49a1a430a1cebd334966739fae
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-10-18 19:38:08 +0100

    Format code for spring txn support. Closes #46.

--- spring/src/main/java/cucumber/runtime/java/spring/hooks/SpringTransactionHooks.java
+++ spring/src/main/java/cucumber/runtime/java/spring/hooks/SpringTransactionHooks.java
@@ -1,32 +1,34 @@
 package cucumber.runtime.java.spring.hooks;
 
+import cucumber.annotation.After;
+import cucumber.annotation.Before;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
-import cucumber.annotation.After;
-import cucumber.annotation.Before;
-
 public class SpringTransactionHooks {
 
     TransactionStatus txStatus;
-    
+
     private PlatformTransactionManager txMgr;
-    /** Setter for Dependency Injection - autowiring would rely on too many client-specific details */
+
+    /**
+     * Setter for Dependency Injection - autowiring would rely on too many client-specific details
+     */
     public void setTxMgr(PlatformTransactionManager txMgr) {
-       this.txMgr = txMgr;
+        this.txMgr = txMgr;
     }
 
-    
+
     @Before({"@txn"})
     public void rollBackBeforeHook() {
         txStatus = txMgr.getTransaction(new DefaultTransactionDefinition());
     }
-    
+
     @After({"@txn"})
     public void rollBackAfterHook() {
         txMgr.rollback(txStatus);
     }
-    
+
 }
 
--- spring/src/test/java/cucumber/runtime/java/spring/hooks/SpringTransactionHooksTest.java
+++ spring/src/test/java/cucumber/runtime/java/spring/hooks/SpringTransactionHooksTest.java
@@ -1,8 +1,5 @@
 package cucumber.runtime.java.spring.hooks;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,39 +9,42 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.support.SimpleTransactionStatus;
 
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.*;
+
 @RunWith(MockitoJUnitRunner.class)
 public class SpringTransactionHooksTest {
 
-	private SpringTransactionHooks target;
-	
-	@Mock
-	private PlatformTransactionManager mockedPlatformTransactionManager;
-	
-	@Before
-	public void setUp() {
-		target = new SpringTransactionHooks();
-		target.setTxMgr(mockedPlatformTransactionManager);
-	}
-	
-	@Test
-	public void shouldObtainOrStartTransactionInBeforeHook() {
-		final SimpleTransactionStatus dummyTxStatus = new SimpleTransactionStatus();
-		when(mockedPlatformTransactionManager.getTransaction(isA(TransactionDefinition.class))).thenReturn(dummyTxStatus);
-		
-		target.rollBackBeforeHook();
-		
-		assertSame(target.txStatus, dummyTxStatus);
-	}
-	
-	@Test
-	public void shouldTriggerTransactionRollbackInAfterHook() {
-		final SimpleTransactionStatus dummyTxStatus = new SimpleTransactionStatus();
-		target.txStatus = dummyTxStatus;
-		
-		mockedPlatformTransactionManager.rollback(dummyTxStatus);
-		
-		verify(mockedPlatformTransactionManager).rollback(dummyTxStatus);
-	}
+    private SpringTransactionHooks target;
+
+    @Mock
+    private PlatformTransactionManager mockedPlatformTransactionManager;
+
+    @Before
+    public void setUp() {
+        target = new SpringTransactionHooks();
+        target.setTxMgr(mockedPlatformTransactionManager);
+    }
+
+    @Test
+    public void shouldObtainOrStartTransactionInBeforeHook() {
+        final SimpleTransactionStatus dummyTxStatus = new SimpleTransactionStatus();
+        when(mockedPlatformTransactionManager.getTransaction(isA(TransactionDefinition.class))).thenReturn(dummyTxStatus);
+
+        target.rollBackBeforeHook();
+
+        assertSame(target.txStatus, dummyTxStatus);
+    }
+
+    @Test
+    public void shouldTriggerTransactionRollbackInAfterHook() {
+        final SimpleTransactionStatus dummyTxStatus = new SimpleTransactionStatus();
+        target.txStatus = dummyTxStatus;
+
+        mockedPlatformTransactionManager.rollback(dummyTxStatus);
+
+        verify(mockedPlatformTransactionManager).rollback(dummyTxStatus);
+    }
 
 
 }
