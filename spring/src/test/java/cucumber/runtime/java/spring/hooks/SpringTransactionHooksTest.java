3a2e141c7c2b360a632f492d3a3c59e70ebf781f
Patrick W. McMichael <patrickmcmichael@mac.com>
2011-10-18 16:44:02 -0400

    formatting to deal with spaces vs tabs crap

--- spring/src/test/java/cucumber/runtime/java/spring/hooks/SpringTransactionHooksTest.java
+++ spring/src/test/java/cucumber/runtime/java/spring/hooks/SpringTransactionHooksTest.java
@@ -16,35 +16,35 @@ import org.springframework.transaction.support.SimpleTransactionStatus;
 @RunWith(MockitoJUnitRunner.class)
 public class SpringTransactionHooksTest {
 
-	private SpringTransactionHooks target;
-	
-	@Mock
-	private BeanFactory mockedBeanFactory;
-	@Mock
-	private PlatformTransactionManager mockedPlatformTransactionManager;
-	
-	@Before
-	public void setUp() {
-		target = new SpringTransactionHooks() {
+    private SpringTransactionHooks target;
+
+    @Mock
+    private BeanFactory mockedBeanFactory;
+    @Mock
+    private PlatformTransactionManager mockedPlatformTransactionManager;
+
+    @Before
+    public void setUp() {
+        target = new SpringTransactionHooks() {
             @Override
             PlatformTransactionManager obtainPlatformTransactionManager() {
                 return mockedPlatformTransactionManager;
             }
-		};
-		target.setBeanFactory(mockedBeanFactory);
-	}
-	
-	@Test
-	public void shouldObtainPlatformTransactionManagerByTypeWhenTxnManagerBeanNameNotSet() {
-	    SpringTransactionHooks localTarget = new SpringTransactionHooks();
-	    localTarget.setBeanFactory(mockedBeanFactory);
-	    
-	    when(mockedBeanFactory.getBean(PlatformTransactionManager.class)).thenReturn(mockedPlatformTransactionManager);
-	    
-	    assertSame(localTarget.obtainPlatformTransactionManager(), mockedPlatformTransactionManager);
-	    
-	    verify(mockedBeanFactory).getBean(PlatformTransactionManager.class);
-	}
+        };
+        target.setBeanFactory(mockedBeanFactory);
+    }
+
+    @Test
+    public void shouldObtainPlatformTransactionManagerByTypeWhenTxnManagerBeanNameNotSet() {
+        SpringTransactionHooks localTarget = new SpringTransactionHooks();
+        localTarget.setBeanFactory(mockedBeanFactory);
+
+        when(mockedBeanFactory.getBean(PlatformTransactionManager.class)).thenReturn(mockedPlatformTransactionManager);
+
+        assertSame(localTarget.obtainPlatformTransactionManager(), mockedPlatformTransactionManager);
+
+        verify(mockedBeanFactory).getBean(PlatformTransactionManager.class);
+    }
 
     @Test
     public void shouldObtainPlatformTransactionManagerByNameWhenTxnManagerBeanNameIsSet() {
@@ -52,33 +52,32 @@ public class SpringTransactionHooksTest {
         localTarget.setBeanFactory(mockedBeanFactory);
         final String txnManagerBeanName = "myTxnManagerBeanName";
         localTarget.setTxnManagerBeanName(txnManagerBeanName);
-        
+
         when(mockedBeanFactory.getBean(txnManagerBeanName, PlatformTransactionManager.class)).thenReturn(mockedPlatformTransactionManager);
-        
+
         assertSame(localTarget.obtainPlatformTransactionManager(), mockedPlatformTransactionManager);
-        
+
         verify(mockedBeanFactory).getBean(txnManagerBeanName, PlatformTransactionManager.class);
     }
-	   
-	@Test
-	public void shouldObtainOrStartTransactionInBeforeHook() {
-		final SimpleTransactionStatus dummyTxStatus = new SimpleTransactionStatus();
-		when(mockedPlatformTransactionManager.getTransaction(isA(TransactionDefinition.class))).thenReturn(dummyTxStatus);
-		
-		target.rollBackBeforeHook();
-		
-		assertSame(target.txStatus, dummyTxStatus);
-	}
-	
-	@Test
-	public void shouldTriggerTransactionRollbackInAfterHook() {
-		final SimpleTransactionStatus dummyTxStatus = new SimpleTransactionStatus();
-		target.txStatus = dummyTxStatus;
-		
-		target.rollBackAfterHook();
-		
-		verify(mockedPlatformTransactionManager).rollback(dummyTxStatus);
-	}
 
+    @Test
+    public void shouldObtainOrStartTransactionInBeforeHook() {
+        final SimpleTransactionStatus dummyTxStatus = new SimpleTransactionStatus();
+        when(mockedPlatformTransactionManager.getTransaction(isA(TransactionDefinition.class))).thenReturn(dummyTxStatus);
+
+        target.rollBackBeforeHook();
+
+        assertSame(target.txStatus, dummyTxStatus);
+    }
+
+    @Test
+    public void shouldTriggerTransactionRollbackInAfterHook() {
+        final SimpleTransactionStatus dummyTxStatus = new SimpleTransactionStatus();
+        target.txStatus = dummyTxStatus;
+
+        target.rollBackAfterHook();
+
+        verify(mockedPlatformTransactionManager).rollback(dummyTxStatus);
+    }
 
 }
