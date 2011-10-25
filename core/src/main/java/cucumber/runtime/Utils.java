a90a6849a2b8635b10dcfe3572d1af901a6c9c67
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-10-25 19:34:57 +0100

    Close stream. Remove dead code.

--- core/src/main/java/cucumber/runtime/Utils.java
+++ core/src/main/java/cucumber/runtime/Utils.java
@@ -1,9 +1,5 @@
 package cucumber.runtime;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.Reader;
-
 public class Utils {
     public static Class<?>[] classArray(int size, Class<?> clazz) {
         Class<?>[] arr = new Class<?>[size];
@@ -11,25 +7,5 @@ public class Utils {
             arr[i] = clazz;
         }
         return arr;
-    }
-
-    public static void closeQuietly(Reader input) {
-        try {
-            if (input != null) {
-                input.close();
-            }
-        } catch (IOException ioe) {
-            // ignore
-        }
-    }
-
-    public static void closeQuietly(OutputStream output) {
-        try {
-            if (output != null) {
-                output.close();
-            }
-        } catch (IOException ioe) {
-            // ignore
-        }
     }
 }
