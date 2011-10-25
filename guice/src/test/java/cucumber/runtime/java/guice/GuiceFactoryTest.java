67541db845006e651ec062a2ab5991e9209407f5
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-10-25 08:53:01 +0100

    Clarify intent of test

--- guice/src/test/java/cucumber/runtime/java/guice/GuiceFactoryTest.java
+++ guice/src/test/java/cucumber/runtime/java/guice/GuiceFactoryTest.java
@@ -1,18 +1,12 @@
 package cucumber.runtime.java.guice;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertThat;
-
-import org.junit.Before;
-import org.junit.Test;
-
 import cucumber.runtime.java.ObjectFactory;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.util.Properties;
+
+import static org.junit.Assert.*;
 
 public class GuiceFactoryTest {
     @Test
@@ -34,11 +28,12 @@ public class GuiceFactoryTest {
         assertNotSame(o1, o2);
     }
 
+    // TODO: verify that this is what the author meant
     @Test
-    public void copeWithMissingGuiceModuleProperty() throws Exception {
+    public void missing_guice_module_property_causes_mapping_to_be_null() throws Exception {
         ObjectFactory factory = new GuiceFactory(new Properties());
         factory.createInstances();
         Mappings mappings = factory.getInstance(Mappings.class);
-        assertThat(mappings, is(notNullValue()));
+        assertNull(mappings);
     }
 }
