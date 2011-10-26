cacaf05276188bfe05c8b4f365977e7ece364c60
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-10-26 01:00:07 +0100

    Added an example to illustrate how to use a custom XStream Converter. Closes #61.

--- core/src/test/java/cucumber/table/TableConverterTest.java
+++ core/src/test/java/cucumber/table/TableConverterTest.java
@@ -2,11 +2,15 @@ package cucumber.table;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.annotations.XStreamConverter;
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
 import com.thoughtworks.xstream.converters.javabean.JavaBeanConverter;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 import cucumber.runtime.converters.LocalizedXStreams;
 import org.junit.Test;
 
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.*;
 
@@ -32,11 +36,21 @@ public class TableConverterTest {
     }
 
     @Test
+    public void converts_table_to_list_of_class_with_special_fields() {
+        XStream xStream = new LocalizedXStreams().get(Locale.UK);
+        TableConverter tc = new TableConverter(xStream);
+        List<UserWithAgeField> users = tc.convert(UserWithAgeField.class, headerRow(), bodyRows());
+        assertEquals("Sid", users.get(0).name.first);
+        assertEquals("Vicious", users.get(0).name.last);
+    }
+
+    @Test
     public void converts_table_to_map_of_string_string() {
         XStream xStream = new LocalizedXStreams().get(Locale.UK);
         TableConverter tc = new TableConverter(xStream);
 
-        Type mapType = new TypeReference<Map<String,String>>(){}.getType();
+        Type mapType = new TypeReference<Map<String, String>>() {
+        }.getType();
         List<Map<String, String>> users = tc.convert(mapType, headerRow(), bodyRows());
         assertEquals("10/05/1957", users.get(0).get("birthDate"));
     }
@@ -100,4 +114,38 @@ public class TableConverterTest {
             this.creditsX = credits;
         }
     }
+
+    public static class UserWithAgeField {
+        public Name name;
+        public Date birthDate;
+        public Integer credits;
+    }
+
+    @XStreamConverter(NameConverter.class)
+    public static class Name {
+        public String first;
+        public String last;
+    }
+
+    public static class NameConverter implements Converter {
+        @Override
+        public void marshal(Object source, HierarchicalStreamWriter writer, MarshallingContext context) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+            Name name = new Name();
+            String[] firstLast = reader.getValue().split(" ");
+            name.first = firstLast[0];
+            name.last = firstLast[1];
+            return name;
+        }
+
+        @Override
+        public boolean canConvert(Class type) {
+            return type.equals(Name.class);
+        }
+    }
+
 }
