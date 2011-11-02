fc1316d02730c056281713984ab57e86cae18205
Erik Mejer Hansen <erik.mejer.hansen@gmail.com>
2011-11-02 12:48:32 +0100

    Corrected indentation to spaces

--- jruby/src/main/java/cucumber/runtime/jruby/JRubyHookDefinition.java
+++ jruby/src/main/java/cucumber/runtime/jruby/JRubyHookDefinition.java
@@ -13,31 +13,30 @@ import cucumber.runtime.ScenarioResult;
 
 public class JRubyHookDefinition implements HookDefinition {
 
-	private RubyObject hook;
-	private int order;
-	private final TagExpression tagExpression;
-	
-	public JRubyHookDefinition(String[] tagExpressions, RubyObject hook) {
+    private RubyObject hook;
+    private int order;
+    private final TagExpression tagExpression;
+
+    public JRubyHookDefinition(String[] tagExpressions, RubyObject hook) {
         tagExpression = new TagExpression(asList(tagExpressions));
         this.order = 0;
         this.hook = hook;
     }
-	
-	
-	@Override
-	public void execute(ScenarioResult scenarioResult) throws Throwable {
-		IRubyObject[] jrybyArgs = new IRubyObject[0];
-		hook.callMethod("execute", jrybyArgs);
-	}
 
-	@Override
-	public boolean matches(Collection<String> tags) {
-		return tagExpression.eval(tags);
-	}
+    @Override
+    public void execute(ScenarioResult scenarioResult) throws Throwable {
+        IRubyObject[] jrybyArgs = new IRubyObject[0];
+        hook.callMethod("execute", jrybyArgs);
+    }
 
-	@Override
-	public int getOrder() {
-		return order;
-	}
+    @Override
+    public boolean matches(Collection<String> tags) {
+        return tagExpression.eval(tags);
+    }
+
+    @Override
+    public int getOrder() {
+        return order;
+    }
 
 }
