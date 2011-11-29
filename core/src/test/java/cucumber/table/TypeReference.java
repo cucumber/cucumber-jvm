8fdb04553143258dee08612a979890fea2b90d85
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-11-30 04:39:07 +1100

    Remove unused code

--- core/src/test/java/cucumber/table/TypeReference.java
+++ core/src/test/java/cucumber/table/TypeReference.java
@@ -1,15 +1,11 @@
 // http://gafter.blogspot.com/2006/12/super-type-tokens.html
 package cucumber.table;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
 public abstract class TypeReference<T> {
-
     private final Type type;
-    private volatile Constructor<?> constructor;
 
     protected TypeReference() {
         Type superclass = getClass().getGenericSuperclass();
@@ -19,26 +15,6 @@ public abstract class TypeReference<T> {
         this.type = ((ParameterizedType) superclass).getActualTypeArguments()[0];
     }
 
-    /**
-     * Instantiates a new instance of {@code T} using the default, no-arg
-     * constructor.
-     */
-    @SuppressWarnings("unchecked")
-    public T newInstance()
-            throws NoSuchMethodException, IllegalAccessException,
-            InvocationTargetException, InstantiationException {
-        if (constructor == null) {
-            Class<?> rawType = type instanceof Class<?>
-                    ? (Class<?>) type
-                    : (Class<?>) ((ParameterizedType) type).getRawType();
-            constructor = rawType.getConstructor();
-        }
-        return (T) constructor.newInstance();
-    }
-
-    /**
-     * Gets the referenced type.
-     */
     public Type getType() {
         return this.type;
     }
