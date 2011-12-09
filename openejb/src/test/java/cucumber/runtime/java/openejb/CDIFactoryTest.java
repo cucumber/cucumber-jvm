0f08cfb5c7c8280e3774ad665ebabb0dc6c611b7
rmannibucau <rmannibucau@gmail.com>
2011-12-09 14:54:00 +0100

    moving openejb tests from weld package to openejb one

---
+++ openejb/src/test/java/cucumber/runtime/java/openejb/CDIFactoryTest.java
@@ -0,0 +1,29 @@
+package cucumber.runtime.java.openejb;
+
+import cucumber.runtime.java.ObjectFactory;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+
+public class CDIFactoryTest {
+    @Test
+    public void shouldGiveUsNewInstancesForEachScenario() {
+        ObjectFactory factory = new OpenEJBObjectFactory();
+        factory.addClass(BellyStepdefs.class);
+
+        // Scenario 1
+        factory.createInstances();
+        BellyStepdefs o1 = factory.getInstance(BellyStepdefs.class);
+        factory.disposeInstances();
+
+        // Scenario 2
+        factory.createInstances();
+        BellyStepdefs o2 = factory.getInstance(BellyStepdefs.class);
+        factory.disposeInstances();
+
+        assertNotNull(o1);
+        assertNotSame(o1, o2);
+    }
+
+}
