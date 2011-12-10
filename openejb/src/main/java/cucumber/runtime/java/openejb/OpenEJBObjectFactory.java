b204d72994710ffcb5c0284b50eadb31def5f9b1
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-12-10 17:27:55 +0000

    Only configure Log4J once

--- openejb/src/main/java/cucumber/runtime/java/openejb/OpenEJBObjectFactory.java
+++ openejb/src/main/java/cucumber/runtime/java/openejb/OpenEJBObjectFactory.java
@@ -5,45 +5,23 @@ import cucumber.runtime.java.ObjectFactory;
 import org.apache.openejb.OpenEjbContainer;
 
 import javax.ejb.embeddable.EJBContainer;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 public class OpenEJBObjectFactory implements ObjectFactory {
+    static {
+        configureLog4J();
+    }
+
     private final List<String> classes = new ArrayList<String>();
-    private final Map<Class<?>, Object> instances= new HashMap<Class<?>, Object>();
+    private final Map<Class<?>, Object> instances = new HashMap<Class<?>, Object>();
     private EJBContainer container;
 
     @Override
     public void createInstances() {
-        // logging
-        final ClassLoader cl = Thread.currentThread().getContextClassLoader();
-        if (System.getProperty("log4j.configuration") == null && System.getProperty("log4j.configurationClass") == null
-                && cl.getResource("log4j.xml") == null && cl.getResource("log4j.properties") == null) {
-            Properties log4jProp = new Properties();
-            log4jProp.setProperty("log4j.rootLogger", "info, stdout");
-            log4jProp.setProperty("log4j.appender.stdout", "org.apache.log4j.ConsoleAppender");
-            try {
-                cl.loadClass("org.apache.openejb.logging.SimpleJULLikeLayout");
-                log4jProp.setProperty("log4j.appender.stdout.layout", "org.apache.openejb.logging.SimpleJULLikeLayout");
-            } catch (Exception e) {
-                log4jProp.setProperty("log4j.appender.stdout.layout", "org.apache.log4j.SimpleLayout");
-            }
-
-            try {
-                cl.loadClass("org.apache.log4j.PropertyConfigurator")
-                        .getDeclaredMethod("configure", Properties.class)
-                        .invoke(null, log4jProp);
-            } catch (Exception notFound) {
-                // ignored
-            }
-        }
+        configureLog4J();
 
         final StringBuilder callers = new StringBuilder();
-        for (Iterator<String> it = classes.iterator(); it.hasNext();) {
+        for (Iterator<String> it = classes.iterator(); it.hasNext(); ) {
             callers.append(it.next());
             if (it.hasNext()) {
                 callers.append(",");
@@ -81,6 +59,30 @@ public class OpenEJBObjectFactory implements ObjectFactory {
         }
         instances.put(type, object);
         return object;
+    }
+
+    private static void configureLog4J() {
+        final ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        if (System.getProperty("log4j.configuration") == null && System.getProperty("log4j.configurationClass") == null
+                && cl.getResource("log4j.xml") == null && cl.getResource("log4j.properties") == null) {
+            Properties log4jProp = new Properties();
+            log4jProp.setProperty("log4j.rootLogger", "info, stdout");
+            log4jProp.setProperty("log4j.appender.stdout", "org.apache.log4j.ConsoleAppender");
+            try {
+                cl.loadClass("org.apache.openejb.logging.SimpleJULLikeLayout");
+                log4jProp.setProperty("log4j.appender.stdout.layout", "org.apache.openejb.logging.SimpleJULLikeLayout");
+            } catch (Exception e) {
+                log4jProp.setProperty("log4j.appender.stdout.layout", "org.apache.log4j.SimpleLayout");
+            }
+
+            try {
+                cl.loadClass("org.apache.log4j.PropertyConfigurator")
+                        .getDeclaredMethod("configure", Properties.class)
+                        .invoke(null, log4jProp);
+            } catch (Exception notFound) {
+                // ignored
+            }
+        }
     }
 }
 
