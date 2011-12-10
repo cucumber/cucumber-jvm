862a339ab9fdf6fc3a544891eb9fdd7b9557e247
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-12-10 07:37:28 +0000

    Formatting

--- java/src/test/java/cucumber/runtime/java/JavaSnippetGeneratorTest.java
+++ java/src/test/java/cucumber/runtime/java/JavaSnippetGeneratorTest.java
@@ -13,20 +13,20 @@ public class JavaSnippetGeneratorTest {
     @Test
     public void generatesPlainSnippet() {
         String expected = "" +
-            "@Given(\"^I have (\\\\d+) cukes in my \\\"([^\\\"]*)\\\" belly$\")\n" +
-            "public void I_have_cukes_in_my_belly(int arg1, String arg2) {\n" +
-            "    // Express the Regexp above with the code you wish you had\n" +
-            "}\n";
+                "@Given(\"^I have (\\\\d+) cukes in my \\\"([^\\\"]*)\\\" belly$\")\n" +
+                "public void I_have_cukes_in_my_belly(int arg1, String arg2) {\n" +
+                "    // Express the Regexp above with the code you wish you had\n" +
+                "}\n";
         assertEquals(expected, snippetFor("I have 4 cukes in my \"big\" belly"));
     }
 
     @Test
     public void generatesCopyPasteReadyStepSnippetForNumberParameters() throws Exception {
         String expected = "" +
-            "@Given(\"^before (\\\\d+) after$\")\n" +
-            "public void before_after(int arg1) {\n" +
-            "    // Express the Regexp above with the code you wish you had\n" +
-            "}\n";
+                "@Given(\"^before (\\\\d+) after$\")\n" +
+                "public void before_after(int arg1) {\n" +
+                "    // Express the Regexp above with the code you wish you had\n" +
+                "}\n";
         String snippet = snippetFor("before 5 after");
         assertEquals(expected, snippet);
     }
@@ -34,10 +34,10 @@ public class JavaSnippetGeneratorTest {
     @Test
     public void generatesCopyPasteReadySnippetWhenStepHasIllegalJavaIdentifierChars() {
         String expected = "" +
-            "@Given(\"^I have (\\\\d+) cukes in: my \\\"([^\\\"]*)\\\" red-belly!$\")\n" +
-            "public void I_have_cukes_in_my_red_belly(int arg1, String arg2) {\n" +
-            "    // Express the Regexp above with the code you wish you had\n" +
-            "}\n";
+                "@Given(\"^I have (\\\\d+) cukes in: my \\\"([^\\\"]*)\\\" red-belly!$\")\n" +
+                "public void I_have_cukes_in_my_red_belly(int arg1, String arg2) {\n" +
+                "    // Express the Regexp above with the code you wish you had\n" +
+                "}\n";
         assertEquals(expected, snippetFor("I have 4 cukes in: my \"big\" red-belly!"));
     }
 
@@ -45,10 +45,10 @@ public class JavaSnippetGeneratorTest {
     @Test
     public void generatesCopyPasteReadySnippetWhenStepHasIntegersInsideStringParameter() {
         String expected = "" +
-            "@Given(\"^the DI system receives a message saying \\\"([^\\\"]*)\\\"$\")\n" +
-            "public void the_DI_system_receives_a_message_saying(String arg1) {\n" +
-            "    // Express the Regexp above with the code you wish you had\n" +
-            "}\n";
+                "@Given(\"^the DI system receives a message saying \\\"([^\\\"]*)\\\"$\")\n" +
+                "public void the_DI_system_receives_a_message_saying(String arg1) {\n" +
+                "    // Express the Regexp above with the code you wish you had\n" +
+                "}\n";
         assertEquals(expected, snippetFor("the DI system receives a message saying \"{ dataIngestion: { feeds: [ feed: { merchantId: 666, feedId: 1, feedFileLocation: feed.csv } ] }\""));
     }
 
