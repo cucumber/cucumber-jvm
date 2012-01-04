3bc0ca3f7e4afe00e8a3adc054697e76e5e5a451
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2012-01-04 00:26:30 +0000

    Added weld

--- weld/src/test/java/cucumber/runtime/java/weld/RunCukesTest.java
+++ weld/src/test/java/cucumber/runtime/java/weld/RunCukesTest.java
@@ -1,10 +1,8 @@
 package cucumber.runtime.java.weld;
 
 import cucumber.junit.Cucumber;
-import cucumber.junit.Feature;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Feature("cukes.feature")
-public class cukes_Test {
+public class RunCukesTest {
 }
