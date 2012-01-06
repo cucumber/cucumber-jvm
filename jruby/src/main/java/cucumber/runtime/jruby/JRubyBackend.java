df198fecc27beb7b43eabb76198fe06146124d8f
David Kowis <david.kowis@rackspace.com>
2012-01-06 17:38:39 -0600

    Added some properties file parsing magic for the JrubyBackend
    This allows a cucumber-jruby.properties file to be specified
    
    Property values:
     * GEM_PATH to configure a path to load ruby gems from
     * RUBY_VERSION to tell JRuby which version of ruby you want to use
       - 1.9 selects ruby 1.9 anything else uses 1.8

--- jruby/src/main/java/cucumber/runtime/jruby/JRubyBackend.java
+++ jruby/src/main/java/cucumber/runtime/jruby/JRubyBackend.java
@@ -7,14 +7,18 @@ import cucumber.runtime.Backend;
 import cucumber.runtime.World;
 import gherkin.formatter.model.Comment;
 import gherkin.formatter.model.Step;
+import org.jruby.CompatVersion;
 import org.jruby.RubyObject;
 import org.jruby.embed.ScriptingContainer;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.Properties;
 
 public class JRubyBackend implements Backend {
     private static final String DSL = "/cucumber/runtime/jruby/dsl.rb";
@@ -23,6 +27,26 @@ public class JRubyBackend implements Backend {
 
     public JRubyBackend() throws UnsupportedEncodingException {
         jruby.put("$backend", this);
+        //Look for a cucumber-jruby.properties file and load in the things I might care about
+        Properties props = new Properties();
+        InputStream propsStream = this.getClass().getResourceAsStream("/cucumber-jruby.properties");
+        try {
+            if (propsStream != null) {
+                props.load(propsStream);
+            }
+        } catch (IOException e) {
+            //Oh well?
+        }
+
+        String gemPath = props.getProperty("GEM_PATH");
+        if (gemPath != null && !gemPath.isEmpty()) {
+            jruby.runScriptlet("ENV['GEM_PATH']='" + gemPath + "'");
+        }
+
+        String rubyVersion = props.getProperty("RUBY_VERSION");
+        if ("1.9".equals(rubyVersion)) {
+            jruby.setCompatVersion(CompatVersion.RUBY1_9);
+        }
         jruby.runScriptlet(new InputStreamReader(getClass().getResourceAsStream(DSL), "UTF-8"), DSL);
     }
 
