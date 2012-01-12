33f95f5dc41b06234e13f4ced8e21a5dcf9251f6
David Kowis <david.kowis@rackspace.com>
2012-01-11 18:43:38 -0600

    Converted it to use a ResourceBundle. Much cleaner.

--- jruby/src/main/java/cucumber/runtime/jruby/JRubyBackend.java
+++ jruby/src/main/java/cucumber/runtime/jruby/JRubyBackend.java
@@ -12,7 +12,6 @@ import org.jruby.RubyObject;
 import org.jruby.embed.ScriptingContainer;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.*;
@@ -28,25 +27,20 @@ public class JRubyBackend implements Backend {
         this.resourceLoader = resourceLoader;
         jruby.put("$backend", this);
         jruby.setClassLoader(getClass().getClassLoader());
-        //Look for a cucumber-jruby.properties file and load in the things I might care about
-        Properties props = new Properties();
-        InputStream propsStream = this.getClass().getResourceAsStream("/cucumber-jruby.properties");
         try {
-            if (propsStream != null) {
-                props.load(propsStream);
+            ResourceBundle bundle = ResourceBundle.getBundle("cucumber-jruby");
+
+            String gemPath = bundle.getString("GEM_PATH");
+            if (gemPath != null && !gemPath.isEmpty()) {
+                jruby.runScriptlet("ENV['GEM_PATH']='" + gemPath + "'");
             }
-        } catch (IOException e) {
-            //Oh well?
-        }
 
-        String gemPath = props.getProperty("GEM_PATH");
-        if (gemPath != null && !gemPath.isEmpty()) {
-            jruby.runScriptlet("ENV['GEM_PATH']='" + gemPath + "'");
-        }
-
-        String rubyVersion = props.getProperty("RUBY_VERSION");
-        if ("1.9".equals(rubyVersion)) {
-            jruby.setCompatVersion(CompatVersion.RUBY1_9);
+            String rubyVersion = bundle.getString("RUBY_VERSION");
+            if ("1.9".equals(rubyVersion)) {
+                jruby.setCompatVersion(CompatVersion.RUBY1_9);
+            }
+        } catch (MissingResourceException mre) {
+            //Don't actually care
         }
         jruby.runScriptlet(new InputStreamReader(getClass().getResourceAsStream(DSL), "UTF-8"), DSL);
     }
