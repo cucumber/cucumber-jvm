3e343992be8ac37f210673843e5da3e2def912d8
David Kowis <dkowis@shlrm.org>
2012-01-16 21:10:32 -0600

    Revert "caching of the step definitions"
    This reverts commit 321c656e3e4e4de9ae7d33e019288f2d43fff4fa.
    
    Conflicts:
    
    	jruby/src/main/java/cucumber/runtime/jruby/JRubyBackend.java
    
    Resolved the conflicts and this removes all the magic sauce that I had in the JRubyBackend
    to cache step definitions that is now in the super_happy_fun_world_branch

--- jruby/src/main/java/cucumber/runtime/jruby/JRubyBackend.java
+++ jruby/src/main/java/cucumber/runtime/jruby/JRubyBackend.java
@@ -18,24 +18,17 @@ import org.jruby.embed.ScriptingContainer;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.util.*;
+import java.util.List;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
 public class JRubyBackend implements Backend {
     private static final String DSL = "/cucumber/runtime/jruby/dsl.rb";
     private final SnippetGenerator snippetGenerator = new SnippetGenerator(new JRubySnippet());
     private final ScriptingContainer jruby = new ScriptingContainer();
-    private final Set<String> gluedPaths = new HashSet<String>();
     private World world;
     private ResourceLoader resourceLoader;
-
-    //Cache the items, because world is created new each scenario, but the jruby isn't
-    //and reloading the entire jruby each time is slow and ineffecient
-    private Set<String> loadedResources = new HashSet<String>();
-    private Set<RubyObject> stepDefs = new HashSet<RubyObject>();
-    private Set<RubyObject> beforeHooks = new HashSet<RubyObject>();
-    private Set<RubyObject> afterHooks = new HashSet<RubyObject>();
-    private Set<RubyObject> worldBlocks = new HashSet<RubyObject>();
-
 
     public JRubyBackend(ResourceLoader resourceLoader) throws UnsupportedEncodingException {
         this.resourceLoader = resourceLoader;
@@ -64,29 +57,9 @@ public class JRubyBackend implements Backend {
         this.world = world;
         jruby.put("$world", new Object());
 
-        //Load all the world preparatory stuff
-        for(RubyObject block : worldBlocks) {
-            new JRubyWorldBlock(block).execute();
-        }
-        
-        //Inject all the existing step definitions
-        for(RubyObject stepdef : stepDefs) {
-            world.addStepDefinition(new JRubyStepDefinition(stepdef));
-        }
-        for(RubyObject beforeHook : beforeHooks) {
-            world.addBeforeHook(new JRubyHookDefinition(new String[0], beforeHook));
-        }
-        for(RubyObject afterHook : afterHooks) {
-            world.addAfterHook(new JRubyHookDefinition(new String[0], afterHook));
-        }
-
         for (String gluePath : gluePaths) {
-            if (gluedPaths.add(gluePath)) {
-                for (Resource resource : resourceLoader.resources(gluePath, ".rb")) {
-                if (loadedResources.add(resource.getPath())) {
-                    runScriptlet(resource);
-                }
-                }
+            for (Resource resource : resourceLoader.resources(gluePath, ".rb")) {
+                runScriptlet(resource);
             }
         }
     }
@@ -115,7 +88,7 @@ public class JRubyBackend implements Backend {
     public void runStep(String uri, Locale locale, String stepKeyword, String stepName, int line, DataTable dataTable, DocString docString) throws Throwable {
         //TODO: convert the data table into a list of dataTableRows for this call
         List<DataTableRow> dataTableRows = null;
-        if(dataTable != null) {
+        if (dataTable != null) {
             dataTableRows = dataTable.getGherkinRows();
         }
 
@@ -123,27 +96,18 @@ public class JRubyBackend implements Backend {
     }
 
     public void addStepdef(RubyObject stepdef) {
-        if (stepDefs.add(stepdef)) {
-            world.addStepDefinition(new JRubyStepDefinition(stepdef));
-        }
+        world.addStepDefinition(new JRubyStepDefinition(stepdef));
     }
 
     public void addBeforeHook(RubyObject body) {
-        if (beforeHooks.add(body)) {
-            world.addBeforeHook(new JRubyHookDefinition(new String[0], body));
-        }
+        world.addBeforeHook(new JRubyHookDefinition(new String[0], body));
     }
 
     public void addAfterHook(RubyObject body) {
-        if (afterHooks.add(body)) {
-            world.addAfterHook(new JRubyHookDefinition(new String[0], body));
-        }
+        world.addAfterHook(new JRubyHookDefinition(new String[0], body));
     }
 
     public void addWorldBlock(RubyObject body) {
-        if(worldBlocks.add(body)) {
-            new JRubyWorldBlock(body).execute();
-        }
+        new JRubyWorldBlock(body).execute();
     }
-    
 }
