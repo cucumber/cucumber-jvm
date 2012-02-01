ce73bf3a2d91b679e6f03cff2d67a8dd4da84931
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2012-02-01 15:18:20 +0100

    Enhanced JUnit Reporting (mark Scenarios as failed if one of the scenario's steps fails)

---
+++ junit/src/test/java/cucumber/junit/JUnitReporterTest.java
@@ -0,0 +1,57 @@
+package cucumber.junit;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.Description;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunNotifier;
+import org.mockito.ArgumentCaptor;
+
+import gherkin.formatter.Formatter;
+import gherkin.formatter.Reporter;
+import gherkin.formatter.model.Result;
+
+/**
+ * @author Klaus Bayrhammer
+ */
+public class JUnitReporterTest
+{
+
+    private JUnitReporter jUnitReporter;
+
+    @Before
+    public void setUp()
+    {
+        Formatter formatter = mock(Formatter.class);
+        Reporter reporter = mock(Reporter.class);
+        
+        jUnitReporter = new JUnitReporter(reporter, formatter);
+    }
+
+    @Test
+    public void resultWithError() {
+        Result result = mock(Result.class);
+        Throwable exception = mock(Throwable.class);
+        when(result.getError()).thenReturn(exception);
+
+        RunNotifier runNotifier = mock(RunNotifier.class);
+        ExecutionUnitRunner executionUnitRunner = mock(ExecutionUnitRunner.class);
+        Description description = mock(Description.class);
+        when(executionUnitRunner.getDescription()).thenReturn(description);
+        jUnitReporter.startExecutionUnit(executionUnitRunner, runNotifier);
+
+        jUnitReporter.result(result);
+
+        ArgumentCaptor<Failure> failureArgumentCaptor = ArgumentCaptor.forClass(Failure.class);
+        verify(runNotifier).fireTestFailure(failureArgumentCaptor.capture());
+
+        Failure failure = failureArgumentCaptor.getValue();
+        assertEquals(description, failure.getDescription());
+        assertEquals(exception, failure.getException());
+    }
+}
