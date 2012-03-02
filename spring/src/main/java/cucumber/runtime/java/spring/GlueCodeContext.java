574afff31cc0dd8408c23100d593a711b67655c7
Vladimir Klyushnikov <vladimir.klyushnikov@gmail.com>
2012-03-02 19:30:05 +0400

    Resolved #241
    Removed child spring context and implemented custom scope for glue code.
    cucumber-glue.xml is internal now. All context configuration may be done in cucumber.xml

---
+++ spring/src/main/java/cucumber/runtime/java/spring/GlueCodeContext.java
@@ -0,0 +1,55 @@
+package cucumber.runtime.java.spring;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GlueCodeContext {
+    public static final GlueCodeContext INSTANCE = new GlueCodeContext();
+    private final Map<String, Object> objects = new HashMap<String, Object>();
+    private final Map<String, Runnable> callbacks = new HashMap<String, Runnable>();
+    private int counter;
+
+    private GlueCodeContext() {
+    }
+    
+    public void start () {
+        cleanUp();
+        counter++;
+    }
+
+    public String getCounter() {
+        return "cucumber_glue_"+ counter;
+    }
+    
+    public void stop() {
+        for (Runnable callback : callbacks.values()) {
+            callback.run();
+        }
+        cleanUp();
+    }
+
+    public Object get(String name){
+        return objects.get(name);
+    }
+
+    public void put(String name, Object object){
+        objects.put(name, object);
+    }
+
+    public Object remove (String name){
+        callbacks.remove(name);
+        return objects.remove(name);
+    }
+
+
+
+    private void cleanUp() {
+        objects.clear();
+        callbacks.clear();
+    }
+
+
+    public void registerDestructionCallback(String name, Runnable callback) {
+        callbacks.put(name, callback);
+    }
+}
---
+++ spring/src/main/java/cucumber/runtime/java/spring/GlueCodeScope.java
@@ -0,0 +1,41 @@
+package cucumber.runtime.java.spring;
+
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.config.Scope;
+
+public class GlueCodeScope implements Scope {
+    public static final String NAME = "cucumber-glue";
+
+    private final GlueCodeContext context =  GlueCodeContext.INSTANCE;
+
+    @Override
+    public Object get(String name, ObjectFactory<?> objectFactory) {
+        Object obj = context.get(name);
+        if (obj == null)  {
+            obj = objectFactory.getObject();
+            context.put(name, obj);
+        }
+
+        return obj;
+    }
+
+    @Override
+    public Object remove(String name) {
+        return context.remove(name);
+    }
+
+    @Override
+    public void registerDestructionCallback(String name, Runnable callback) {
+        context.registerDestructionCallback(name, callback);
+    }
+
+    @Override
+    public Object resolveContextualObject(String key) {
+        return null;
+    }
+
+    @Override
+    public String getConversationId() {
+        return context.getCounter();
+    }
+}
