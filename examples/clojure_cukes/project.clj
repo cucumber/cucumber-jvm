79c2000076ec6974db32de063f44c16ea2c53869
Nils Wloka <mail@nilswloka.com>
2012-03-21 01:18:07 +0100

    Migrated to lein-cucumber and new step definition syntax.

--- examples/clojure_cukes/project.clj
+++ examples/clojure_cukes/project.clj
@@ -1,4 +1,5 @@
 (defproject clojure_cukes "1.0.0.RC2-SNAPSHOT"
   :description "A demo of Cucumber with Clojure and Leiningen"
-  :dependencies [[org.clojure/clojure "1.4.0-alpha3"]
-                 [info.cukes/cucumber-clojure "1.0.0.RC2-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.4.0-alpha3"]]
+  :plugins [[lein-cucumber "1.0.0.M1-SNAPSHOT"]]
+  :cucumber-feature-paths ["test/features/"])
