6ff536b8478d78a450566ef81bdbaedbc3ea449e
Nils Wloka <mail@nilswloka.com>
2012-04-29 01:27:55 +0200

    Update version to match current cucumber-jvm snapshot

--- examples/clojure_cukes/project.clj
+++ examples/clojure_cukes/project.clj
@@ -1,4 +1,4 @@
-(defproject clojure_cukes "1.0.4"
+(defproject clojure_cukes "1.0.5-SNAPSHOT"
   :description "A demo of Cucumber with Clojure and Leiningen"
   :dependencies [[org.clojure/clojure "1.4.0-alpha3"]]
   :plugins [[lein-cucumber "1.0.0"]]
