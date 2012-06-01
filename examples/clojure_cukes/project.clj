73efd68756d3719e5455625118fb4836416e72b5
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2012-06-01 05:28:16 +0100

    Small Clojure tweaks

--- examples/clojure_cukes/project.clj
+++ examples/clojure_cukes/project.clj
@@ -1,5 +1,5 @@
-(defproject clojure_cukes "1.0.5-SNAPSHOT"
+(defproject clojure_cukes "1.0.X"
   :description "A demo of Cucumber with Clojure and Leiningen"
-  :dependencies [[org.clojure/clojure "1.4.0-alpha3"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-cucumber "1.0.0"]]
   :cucumber-feature-paths ["test/features/"])
