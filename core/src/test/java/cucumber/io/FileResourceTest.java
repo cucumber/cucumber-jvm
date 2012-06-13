065e7e20b178cda0211a3519365fb26653f4ece3
Matthew Harrison <matthew.harrison@equinox.co.nz>
2012-06-14 06:52:14 +1200

    fixing up tests, and removing windows assumption

--- core/src/test/java/cucumber/io/FileResourceTest.java
+++ core/src/test/java/cucumber/io/FileResourceTest.java
@@ -13,15 +13,15 @@ public class FileResourceTest {
         FileResource toTest = new FileResource(new File("test1/test.feature"), new File("test1/test.feature"));
 
         // test
-        assertEquals("test1\\test.feature", toTest.getPath());
+        assertEquals("test1" + File.separator + "test.feature", toTest.getPath());
     }
 
     @Test
     public void get_path_should_return_truncated_path_when_absolute_file_paths_are_input() {
         // setup
-        FileResource toTest = new FileResource(new File("c://testPath/test1"), new File("c://testPath/test1/test.feature"));
+        FileResource toTest = new FileResource(new File("/testPath/test1"), new File("/testPath/test1/test.feature"));
 
         // test
-        assertEquals("test1\\test.feature", toTest.getPath());
+        assertEquals("test.feature", toTest.getPath());
     }
 }
