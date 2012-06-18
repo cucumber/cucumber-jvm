deae442b358bc25804c498d40eed7492ca844317
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2012-06-18 09:18:54 +0100

    Cancel timer if we complete before it's done.

--- core/src/main/java/cucumber/runtime/Utils.java
+++ core/src/main/java/cucumber/runtime/Utils.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -40,16 +41,19 @@ public class Utils {
             } else {
                 final Thread executionThread = Thread.currentThread();
                 final AtomicBoolean done = new AtomicBoolean();
-                Executors.newSingleThreadScheduledExecutor().schedule(new Runnable() {
+                ScheduledFuture<?> timer = Executors.newSingleThreadScheduledExecutor().schedule(new Runnable() {
                     @Override
                     public void run() {
+                        System.out.println("done = " + done);
                         if (!done.get()) {
                             executionThread.interrupt();
                         }
                     }
                 }, timeoutMillis, TimeUnit.MILLISECONDS);
                 try {
-                    return invoke(target, method, 0, args);
+                    Object result = invoke(target, method, 0, args);
+                    timer.cancel(true);
+                    return result;
                 } catch (InterruptedException timeout) {
                     throw new TimeoutException("Timed out after " + timeoutMillis + "ms.");
                 }
