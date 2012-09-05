cced5ed8a2558a713f5618b05590bdc774259b2b
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2012-09-05 13:34:24 +0100

    Add a guard to make sure people don't accidentally close the shared driver from a hook or stepdef.

--- examples/java-webbit-websockets-selenium/src/test/java/cucumber/examples/java/websockets/SharedDriver.java
+++ examples/java-webbit-websockets-selenium/src/test/java/cucumber/examples/java/websockets/SharedDriver.java
@@ -21,18 +21,27 @@ import org.openqa.selenium.support.events.EventFiringWebDriver;
  */
 public class SharedDriver extends EventFiringWebDriver {
     private static final WebDriver REAL_DRIVER = new ChromeDriver();
+    private static final Thread CLOSE_THREAD = new Thread() {
+        @Override
+        public void run() {
+            REAL_DRIVER.close();
+        }
+    };
 
     static {
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                REAL_DRIVER.close();
-            }
-        });
+        Runtime.getRuntime().addShutdownHook(CLOSE_THREAD);
     }
 
     public SharedDriver() {
         super(REAL_DRIVER);
+    }
+
+    @Override
+    public void close() {
+        if(Thread.currentThread() != CLOSE_THREAD) {
+            throw new UnsupportedOperationException("You shouldn't close this WebDriver. It's shared and will close when the JVM exits.");
+        }
+        super.close();
     }
 
     @Before
