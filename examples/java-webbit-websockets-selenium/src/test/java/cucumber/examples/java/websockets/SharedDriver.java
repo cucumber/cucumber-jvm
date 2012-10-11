81d3e44df69d4b4737bb134be5c6dc7a77a03068
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2012-10-11 13:45:03 +0100

    Update javadoc

--- examples/java-webbit-websockets-selenium/src/test/java/cucumber/examples/java/websockets/SharedDriver.java
+++ examples/java-webbit-websockets-selenium/src/test/java/cucumber/examples/java/websockets/SharedDriver.java
@@ -10,14 +10,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 /**
- * Example of a WebDriver implementation that has an underlying instance that is used for all scenarios and only closed
- * when the JVM exits. This makes scenarios a lot faster since opening and closing a browser for each scenario is pretty slow.
- * To prevent browser state from leaking between scenarios, cookies are deleted before every scenario.
- * <p/>
+ * <p>
+ * Example of a WebDriver implementation that has delegates all methods to a static instance (REAL_DRIVER) that is only 
+ * created once for the duration of the JVM. The REAL_DRIVER is automatically closed when the JVM exits. This makes 
+ * scenarios a lot faster since opening and closing a browser for each scenario is pretty slow.
+ * To prevent browser state from leaking between scenarios, cookies are automatically deleted before every scenario.
+ * </p>
+ * <p>
+ * A new instance of SharedDriver is created for each Scenario and passed to yor Stepdef classes via Dependency Injection
+ * </p>
+ * <p>
  * As a bonus, screenshots are embedded into the report for each scenario. (This only works
  * if you're also using the HTML formatter).
- * <p/>
- * This class can be shared across step definitions via dependency injection.
+ * </p>
+ * <p>
+ * A new instance of the SharedDriver is created for each Scenario and then passed to the Step Definition classes'
+ * constructor. They all receive a reference to the same instance. However, the REAL_DRIVER is the same instance throughout
+ * the life of the JVM.
+ * </p>
  */
 public class SharedDriver extends EventFiringWebDriver {
     private static final WebDriver REAL_DRIVER = new ChromeDriver();
