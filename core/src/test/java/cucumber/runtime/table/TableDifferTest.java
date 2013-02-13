404bfb893d5166b6da0a8f4d1230d9ac36f39629
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2013-02-13 16:48:55 +0000

    Don't use deprecated methods

--- core/src/test/java/cucumber/runtime/table/TableDifferTest.java
+++ core/src/test/java/cucumber/runtime/table/TableDifferTest.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class TableDifferTest {
 
