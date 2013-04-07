840601ad0a44586ff87569a03db73ac386e9037d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2013-04-07 08:31:51 +0100

    Proposed solution to #487

--- java/src/test/java/cucumber/runtime/java/stepdefs/Stepdefs.java
+++ java/src/test/java/cucumber/runtime/java/stepdefs/Stepdefs.java
@@ -1,10 +1,16 @@
 package cucumber.runtime.java.stepdefs;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.sv.Så;
 
 public class Stepdefs {
     @Given("test")
     public void test() {
+
+    }
+
+    @Så("test")
+    public void testEncoding() {
 
     }
 }
