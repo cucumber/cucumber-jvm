1a388c36dc924a27fa6f0da2451b18e074978cc5
Jan Galinski <jan.galinski@holisticon.de>
2013-04-13 22:56:57 +0200

    moved to src/test

--- needle/src/test/java/cucumber/runtime/java/test/injectionprovider/ValueInjectionProvider.java
+++ needle/src/test/java/cucumber/runtime/java/test/injectionprovider/ValueInjectionProvider.java
@@ -1,37 +1,19 @@
 package cucumber.runtime.java.test.injectionprovider;
 
-import de.akquinet.jbosscc.needle.injection.InjectionProvider;
-import de.akquinet.jbosscc.needle.injection.InjectionTargetInformation;
+import cucumber.runtime.java.needle.injection.DefaultInstanceInjectionProvider;
 
 /**
  * Returns a value provider returning the value given in constructor.
  */
-public class ValueInjectionProvider implements InjectionProvider<ValueGetter> {
+public class ValueInjectionProvider extends DefaultInstanceInjectionProvider<ValueGetter> {
 
-    private final String value;
-
-    public ValueInjectionProvider(final String value) {
-        this.value = value;
-    }
-
-    @Override
-    public boolean verify(final InjectionTargetInformation injectionTargetInformation) {
-        return injectionTargetInformation.getType().isAssignableFrom(ValueGetter.class);
-    }
-
-    @Override
-    public ValueGetter getInjectedObject(final Class<?> injectionPointType) {
-        return new ValueGetter() {
-            @Override
-            public String getValue() {
-                return value;
-            }
-        };
-    }
-
-    @Override
-    public Object getKey(final InjectionTargetInformation injectionTargetInformation) {
-        return ValueGetter.class;
-    }
+  public ValueInjectionProvider(final String value) {
+    super(new ValueGetter() {
+      @Override
+      public String getValue() {
+        return value;
+      }
+    });
+  }
 
 }
