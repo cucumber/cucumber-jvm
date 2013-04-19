83d32143ab5ac8f1b3ebcca98e1acc3001152e5d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2013-04-19 18:29:12 +0100

    Renames

--- examples/spring-txn/src/test/java/cucumber/examples/spring/txn/SeeMessagesStepdefs.java
+++ examples/spring-txn/src/test/java/cucumber/examples/spring/txn/SeeMessagesStepdefs.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebAppConfiguration
 @ContextConfiguration("classpath:cucumber.xml")
-public class SeeMessagesSteps {
+public class SeeMessagesStepdefs {
     @Autowired
     private UserRepository userRepository;
 
@@ -27,29 +27,28 @@ public class SeeMessagesSteps {
     private MockMvc mockMvc;
 
     private User user;
-    private Message message;
 
     private ResultActions resultActions;
 
     @Given("^there is a User$")
-    public void there_is_a_User() throws Throwable {
+    public void there_is_a_User() {
         user = userRepository.save(new User("John Doe"));
     }
 
     @Given("^the User has posted the message \"([^\"]*)\"$")
-    public void the_User_has_posted_the_message(String content) throws Throwable {
-        message = messageRepository.save(new Message(user, content));
+    public void the_User_has_posted_the_message(String content) {
+        messageRepository.save(new Message(user, content));
     }
 
     @When("^I visit the page for the User$")
-    public void I_visit_the_page_for_the_User() throws Throwable {
+    public void I_visit_the_page_for_the_User() throws Exception {
         resultActions = mockMvc
                 .perform(get("/users/" + user.getId()))
                 .andExpect(status().isOk());
     }
 
     @Then("^I should see \"([^\"]*)\"$")
-    public void I_should_see(String content) throws Throwable {
+    public void I_should_see(String content) throws Exception {
         resultActions.andExpect(content().string(containsString(content)));
     }
 
