f771b3352f3e370cc6f724fa2df59bac16bdd99a
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2013-05-17 16:07:48 -0500

    Don't specify tags in runner

--- examples/java-helloworld/src/test/java/cucumber/examples/java/helloworld/RunCukesTest.java
+++ examples/java-helloworld/src/test/java/cucumber/examples/java/helloworld/RunCukesTest.java
@@ -4,8 +4,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-// Run scenarios tagged with @foo OR @bar. Can be overridden with e.g. -Dcucumber.options="--tags @foo"
-// on the command line.
-@Cucumber.Options(tags = {"@foo,@bar"}, format = {"html:target/cucumber-html-report", "json-pretty:target/cucumber-json-report.json"})
+@Cucumber.Options(format = {"html:target/cucumber-html-report", "json-pretty:target/cucumber-json-report.json"})
 public class RunCukesTest {
 }
