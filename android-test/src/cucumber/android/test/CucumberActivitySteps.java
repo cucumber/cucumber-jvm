7b5b2940a232edfe8e84067661a686bb666b1c38
Maximilian Fellner <max.fellner@gmail.com>
2013-05-19 23:14:01 +0200

    Added assertions to test.

--- android-test/src/cucumber/android/test/CucumberActivitySteps.java
+++ android-test/src/cucumber/android/test/CucumberActivitySteps.java
@@ -8,27 +8,34 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class CucumberActivitySteps extends ActivityInstrumentationTestCase2<CucumberActivity> {
+    private int mSteps;
+
     public CucumberActivitySteps() {
         super(CucumberActivity.class);
     }
 
     @Before
     public void before() {
+        assertEquals(0, mSteps);
     }
 
     @After
     public void after() {
+        assertEquals(3, mSteps);
     }
 
     @Given("^I have a test$")
     public void I_have_a_test() {
+        assertEquals(1, ++mSteps);
     }
 
     @When("^I test$")
     public void I_test() {
+        assertEquals(2, ++mSteps);
     }
 
     @Then("^I succeed$")
     public void I_succeed() {
+        assertEquals(3, ++mSteps);
     }
 }
