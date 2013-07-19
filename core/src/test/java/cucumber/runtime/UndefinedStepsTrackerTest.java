90272f15aedb1348de44032d54180d6c97c27be9
meza <meza@meza.hu>
2013-07-19 17:57:39 +0100

    Style fix

--- core/src/test/java/cucumber/runtime/UndefinedStepsTrackerTest.java
+++ core/src/test/java/cucumber/runtime/UndefinedStepsTrackerTest.java
@@ -10,7 +10,9 @@ import org.mockito.Mockito;
 import java.util.List;
 
 import static java.util.Arrays.asList;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class UndefinedStepsTrackerTest {
 
