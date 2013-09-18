0dbcda8ec689c89f7a3b72b16e850177e7e6c9ca
Frieder Bluemle <info@bluemle.net>
2013-09-18 15:04:12 -0700

    Improve CamelCaseFunctionNameSanitizer
    Function names are now correctly formatted if they contain acronyms. The
    name sanitizer also will not change the case of words that are already
    pascal or camel case in the input.

--- core/src/test/java/cucumber/runtime/snippets/CamelCaseFunctionNameSanitizerTest.java
+++ core/src/test/java/cucumber/runtime/snippets/CamelCaseFunctionNameSanitizerTest.java
@@ -14,13 +14,31 @@ public class CamelCaseFunctionNameSanitizerTest {
         String functionName = "I am a function name";
         String expected = "iAmAFunctionName";
         String actual = generator.sanitizeFunctionName(functionName);
-
         assertEquals(expected, actual);
 
         functionName = "Function name with multiple  spaces";
         expected = "functionNameWithMultipleSpaces";
         actual = generator.sanitizeFunctionName(functionName);
+        assertEquals(expected, actual);
 
+        functionName = "Function name with pascalCase word";
+        expected = "functionNameWithPascalCaseWord";
+        actual = generator.sanitizeFunctionName(functionName);
+        assertEquals(expected, actual);
+
+        functionName = "Function name with CamelCase word";
+        expected = "functionNameWithCamelCaseWord";
+        actual = generator.sanitizeFunctionName(functionName);
+        assertEquals(expected, actual);
+
+        functionName = "Function name with multi char acronym HTTP Server";
+        expected = "functionNameWithMultiCharAcronymHttpServer";
+        actual = generator.sanitizeFunctionName(functionName);
+        assertEquals(expected, actual);
+
+        functionName = "Function name with two char acronym US";
+        expected = "functionNameWithTwoCharAcronymUS";
+        actual = generator.sanitizeFunctionName(functionName);
         assertEquals(expected, actual);
     }
 }
