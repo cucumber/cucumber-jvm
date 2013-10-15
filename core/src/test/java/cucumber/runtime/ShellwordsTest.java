976111fb54d0807821939a0d244155eeeab0d158
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2013-10-15 13:26:33 +0100

    Added another shellwords test

--- core/src/test/java/cucumber/runtime/ShellwordsTest.java
+++ core/src/test/java/cucumber/runtime/ShellwordsTest.java
@@ -23,4 +23,10 @@ public class ShellwordsTest {
     public void parses_both_single_and_double_quoted_strings() {
         assertEquals(asList("--name", "The Fox", "--fur", "Brown White"), Shellwords.parse("--name \"The Fox\" --fur 'Brown White'"));
     }
+
+    @Ignore("TODO: fixme")
+    @Test
+    public void can_quote_both_single_and_double_quotes() {
+        assertEquals(asList("'", "\""), Shellwords.parse("\"'\" '\"'"));
+    }
 }
