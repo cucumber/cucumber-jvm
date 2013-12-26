f4e3eb8066b7ba911b3352469ed1b7157ecb76bf
Dmitry Sidorenko <dsidorenko@agent.ru>
2013-12-26 21:16:50 +0400

    AbstractTestNGCucumberTests runs each feature as separate test

--- testng/src/main/java/cucumber/api/testng/TestNGCucumberRunner.java
+++ testng/src/main/java/cucumber/api/testng/TestNGCucumberRunner.java
@@ -7,15 +7,21 @@ import cucumber.runtime.RuntimeOptionsFactory;
 import cucumber.runtime.io.MultiLoader;
 import cucumber.runtime.io.ResourceLoader;
 import cucumber.runtime.io.ResourceLoaderClassFinder;
+import cucumber.runtime.model.CucumberFeature;
 
-import java.io.IOException;
+import java.util.List;
 
 /**
  * Glue code for running Cucumber via TestNG.
  */
 public class TestNGCucumberRunner {
-
-    private final cucumber.runtime.Runtime runtime;
+    protected final Class                    clazz;
+    protected       TestNgReporter           reporter;
+    protected       cucumber.runtime.Runtime runtime;
+    protected       RuntimeOptions           runtimeOptions;
+    protected       ResourceLoader           resourceLoader;
+    protected       ClassLoader              classLoader;
+    protected       ClassFinder              classFinder;
 
     /**
      * Bootstrap the cucumber runtime
@@ -23,32 +29,54 @@ public class TestNGCucumberRunner {
      * @param clazz Which has the cucumber.api.CucumberOptions and org.testng.annotations.Test annotations
      */
     public TestNGCucumberRunner(Class clazz) {
-        ClassLoader classLoader = clazz.getClassLoader();
-        ResourceLoader resourceLoader = new MultiLoader(classLoader);
+        this.clazz = clazz;
+        classLoader = clazz.getClassLoader();
+        resourceLoader = new MultiLoader(classLoader);
 
         RuntimeOptionsFactory runtimeOptionsFactory = new RuntimeOptionsFactory(clazz);
-        RuntimeOptions runtimeOptions = runtimeOptionsFactory.create();
+        runtimeOptions = runtimeOptionsFactory.create();
 
         TestNgReporter reporter = new TestNgReporter(System.out);
         runtimeOptions.addPlugin(reporter);
-        ClassFinder classFinder = new ResourceLoaderClassFinder(resourceLoader, classLoader);
-        runtime = new cucumber.runtime.Runtime(resourceLoader, classFinder, classLoader, runtimeOptions);
+        classFinder = new ResourceLoaderClassFinder(resourceLoader, classLoader);
     }
 
     /**
      * Run the Cucumber features
      */
     public void runCukes() {
-        try {
-            runtime.run();
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
+        for (CucumberFeature cucumberFeature : getFeatures()) {
+            runCucumber(cucumberFeature);
         }
+    }
+
+    public void runCucumber(CucumberFeature cucumberFeature) {
+        //Runtime is recreated every time to ensure that runtime.getErrors() is empty
+        runtime = new cucumber.runtime.Runtime(resourceLoader, classFinder, classLoader, runtimeOptions);
+
+        cucumberFeature.run(
+            runtimeOptions.formatter(classLoader),
+            runtimeOptions.reporter(classLoader),
+            runtime);
+
         if (!runtime.getErrors().isEmpty()) {
             throw new CucumberException(runtime.getErrors().get(0));
         } else if (runtime.exitStatus() != 0x00) {
             throw new CucumberException("There are pending or undefined steps.");
         }
+        // If there is an undefined step, checking for exit status is the only way to conform CucumberOptions.strict contract
+        if (runtime.exitStatus() != 0) {
+            //runtime.getSnippets()
+            throw new CucumberException("There are undefined steps");
+        }
+    }
+
+    /**
+     *
+     * @return List of detected cucumber features
+     */
+    public List<CucumberFeature> getFeatures() {
+        return runtimeOptions.cucumberFeatures(resourceLoader);
     }
 
 }
--- testng/src/test/java/cucumber/api/testng/TestNGCucumberRunnerTest.java
+++ testng/src/test/java/cucumber/api/testng/TestNGCucumberRunnerTest.java
@@ -1,15 +1,34 @@
 package cucumber.api.testng;
 
 import cucumber.runtime.CucumberException;
+import cucumber.runtime.model.CucumberFeature;
 import cucumber.runtime.testng.RunCukesStrict;
+import cucumber.runtime.testng.RunCukesTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class TestNGCucumberRunnerTest {
+    private TestNGCucumberRunner testNGCucumberRunner;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        testNGCucumberRunner = new TestNGCucumberRunner(RunCukesTest.class);
+    }
 
     @Test(expectedExceptions = CucumberException.class)
     public void runCukesStrict() throws Exception {
-        TestNGCucumberRunner testNGCucumberRunner = new TestNGCucumberRunner(RunCukesStrict.class);
+        testNGCucumberRunner = new TestNGCucumberRunner(RunCukesStrict.class);
         testNGCucumberRunner.runCukes();
     }
 
+    @Test
+    public void getFeatures() throws Exception {
+        List<CucumberFeature> features = testNGCucumberRunner.getFeatures();
+
+        Assert.assertFalse(features.isEmpty());
+        Assert.assertEquals(features.size(), 3, "All features associated with RunCukesTest are loaded");
+    }
 }
