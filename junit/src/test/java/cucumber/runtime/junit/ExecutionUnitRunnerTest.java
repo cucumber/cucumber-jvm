2775bc10394fa93666b3fdef339374056dc8b753
Björn Rasmusson <B.Rasmusson@computer.org>
2014-01-19 14:06:28 +0100

    Correct JUnit notification for background steps
    The background steps are copied in the ExecutionUnitRunner for each
    scenario in a feature with background. This is done to make them unique
    for each scenario, so that the JUnit notification later will not be
    ambiguous. When the background steps are executed, it is the original
    ones that are executed. Therefore the background steps need to be
    'converted' from the original steps to the copies made earlier, when
    performing the actual JUnit notifications for an execution unit
    (scenario).

--- junit/src/test/java/cucumber/runtime/junit/ExecutionUnitRunnerTest.java
+++ junit/src/test/java/cucumber/runtime/junit/ExecutionUnitRunnerTest.java
@@ -1,12 +1,19 @@
 package cucumber.runtime.junit;
 
+import cucumber.runtime.FeatureBuilder;
 import cucumber.runtime.io.ClasspathResourceLoader;
+import cucumber.runtime.io.Resource;
 import cucumber.runtime.model.CucumberFeature;
 import cucumber.runtime.model.CucumberScenario;
 import gherkin.formatter.model.Step;
 import org.junit.Test;
 import org.junit.runner.Description;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -65,4 +72,65 @@ public class ExecutionUnitRunnerTest {
         assertEquals("description includes scenario name as class name", runner.getName(), stepDescription.getClassName());
         assertEquals("description includes step keyword and name as method name", step.getKeyword() + step.getName(), stepDescription.getMethodName());
     }
+
+    @Test
+    public void shouldPopulateRunnerStepsWithStepsUsedInStepDescriptions() throws Exception {
+        CucumberFeature cucumberFeature = feature("featurePath", "" +
+                "Feature: feature name\n" +
+                "  Background:\n" +
+                "    Given background step\n" +
+                "  Scenario:\n" +
+                "    Then scenario name\n");
+
+        ExecutionUnitRunner runner = new ExecutionUnitRunner(
+                null,
+                (CucumberScenario) cucumberFeature.getFeatureElements().get(0),
+                null
+        );
+
+        // fish out the data from runner
+        Description runnerDescription = runner.getDescription();
+        Description backgroundStepDescription = runnerDescription.getChildren().get(0);
+        Description scenarioStepDescription = runnerDescription.getChildren().get(1);
+        Step runnerBackgroundStep = runner.getRunnerSteps().get(0);
+        Step runnerScenarioStep = runner.getRunnerSteps().get(1);
+
+        assertDescriptionHasStepAsUniqueId(backgroundStepDescription, runnerBackgroundStep);
+        assertDescriptionHasStepAsUniqueId(scenarioStepDescription, runnerScenarioStep);
+    }
+
+    private void assertDescriptionHasStepAsUniqueId(Description stepDescription, Step step) {
+        // Note, JUnit uses the the serializable parameter (in this case the step)
+        // as the unique id when comparing Descriptions
+        assertEquals(stepDescription, Description.createTestDescription("", "", step));
+    }
+
+    private CucumberFeature feature(final String path, final String source) throws IOException {
+        ArrayList<CucumberFeature> cucumberFeatures = new ArrayList<CucumberFeature>();
+        FeatureBuilder featureBuilder = new FeatureBuilder(cucumberFeatures);
+        featureBuilder.parse(new Resource() {
+            @Override
+            public String getPath() {
+                return path;
+            }
+
+            @Override
+            public InputStream getInputStream() {
+                try {
+                    return new ByteArrayInputStream(source.getBytes("UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public String getClassName() {
+                throw new UnsupportedOperationException();
+            }
+        }, new ArrayList<Object>());
+        featureBuilder.close();
+        return cucumberFeatures.get(0);
+    }
+
+
 }
