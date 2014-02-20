6d4e5032ec338cadef89e883942d0f9fe5e3d757
Dominic Fox <dominic.fox@gmail.com>
2014-02-20 17:32:42 +0000

    Dominic: fix line number reference in test code

--- scala/src/test/scala/cucumber/api/scala/ScalaDslTest.scala
+++ scala/src/test/scala/cucumber/api/scala/ScalaDslTest.scala
@@ -129,7 +129,7 @@ class ScalaDslTest {
 
     assertEquals(1, Dummy.stepDefinitions.size)
     val step = Dummy.stepDefinitions.head
-    assertEquals("ScalaDslTest.scala:123", step.getLocation(true)) // be careful with formatting or this test will break
+    assertEquals("ScalaDslTest.scala:125", step.getLocation(true)) // be careful with formatting or this test will break
     assertEquals("x", step.getPattern)
     step.execute(new I18n("en"), Array())
     assertTrue(called)
