c0e2e632fdbc76260fec8d7072d1ca1ad941087a
Mykola Gurov <ngurov@gmail.com>
2014-03-15 14:18:11 +0100

    quick fix to table enum to null conversion issue by registering custom enum converter

--- core/src/main/java/cucumber/runtime/xstream/LocalizedXStreams.java
+++ core/src/main/java/cucumber/runtime/xstream/LocalizedXStreams.java
@@ -1,11 +1,9 @@
 package cucumber.runtime.xstream;
 
 import cucumber.deps.com.thoughtworks.xstream.XStream;
-import cucumber.deps.com.thoughtworks.xstream.converters.Converter;
-import cucumber.deps.com.thoughtworks.xstream.converters.ConverterLookup;
-import cucumber.deps.com.thoughtworks.xstream.converters.ConverterRegistry;
-import cucumber.deps.com.thoughtworks.xstream.converters.SingleValueConverter;
+import cucumber.deps.com.thoughtworks.xstream.converters.*;
 import cucumber.deps.com.thoughtworks.xstream.core.DefaultConverterLookup;
+import cucumber.deps.com.thoughtworks.xstream.io.HierarchicalStreamReader;
 import cucumber.runtime.ParameterInfo;
 
 import java.lang.reflect.Type;
@@ -57,6 +55,14 @@ public class LocalizedXStreams {
             register(converterRegistry, new IntegerConverter(locale));
             register(converterRegistry, new LongConverter(locale));
             register(converterRegistry, new PatternConverter());
+
+            converterRegistry.registerConverter(new cucumber.deps.com.thoughtworks.xstream.converters.enums.EnumConverter() {
+                public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+                    if (null == reader.getValue() || "".equals(reader.getValue())) {
+                        return null;
+                    }
+                    return super.unmarshal(reader, context);
+                } } , XStream.PRIORITY_VERY_HIGH);
 
             // Must be lower priority than the ones above, but higher than xstream's built-in ReflectionConverter
             converterRegistry.registerConverter(new SingleValueConverterWrapperExt(new ClassWithStringAssignableConstructorConverter()), XStream.PRIORITY_LOW);
