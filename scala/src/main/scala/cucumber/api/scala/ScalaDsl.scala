ab290ee2192cef9ec0dbfab35073bec0d873d18e
Nayan Hajratwala <nayan@chikli.com>
2014-03-18 12:09:49 -0400

    fixed scala warnings

--- scala/src/main/scala/cucumber/api/scala/ScalaDsl.scala
+++ scala/src/main/scala/cucumber/api/scala/ScalaDsl.scala
@@ -7,6 +7,7 @@ import _root_.cucumber.runtime.HookDefinition
 import _root_.cucumber.runtime.StepDefinition
 import collection.mutable.ArrayBuffer
 import java.lang.reflect.Type
+import scala.language.implicitConversions
 
 /**
  * Base trait for a scala step definition implementation.
