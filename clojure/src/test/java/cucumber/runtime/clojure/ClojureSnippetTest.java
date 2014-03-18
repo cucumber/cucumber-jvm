2d6a5b382130e6f0fbc10d32f26e0bf1c222bfeb
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2014-03-18 10:35:31 +0000

    Help my IDE not complain about Clojure classes

--- clojure/src/test/java/cucumber/runtime/clojure/ClojureSnippetTest.java
+++ clojure/src/test/java/cucumber/runtime/clojure/ClojureSnippetTest.java
@@ -1,6 +1,7 @@
 package cucumber.runtime.clojure;
 
-import cucumber.runtime.clj.Backend;
+import cucumber.runtime.Backend;
+import cucumber.runtime.io.ResourceLoader;
 import gherkin.formatter.model.Comment;
 import gherkin.formatter.model.DataTableRow;
 import gherkin.formatter.model.Step;
@@ -16,9 +17,9 @@ public class ClojureSnippetTest {
     private static final List<Comment> NO_COMMENTS = Collections.emptyList();
 
     @Test
-    public void generatesPlainSnippet() {
+    public void generatesPlainSnippet() throws Exception {
         Step step = new Step(NO_COMMENTS, "Given ", "I have 4 cukes in my \"big\" belly", 0, null, null);
-        String snippet = (new Backend(null)).getSnippet(step, null);
+        String snippet = newBackend().getSnippet(step, null);
         String expected = "" +
                 "(Given #\"^I have (\\d+) cukes in my \\\"(.*?)\\\" belly$\" [arg1 arg2]\n" +
                 "  (comment  Write code here that turns the phrase above into concrete actions  )\n" +
@@ -27,14 +28,18 @@ public class ClojureSnippetTest {
     }
 
     @Test
-    public void generatesSnippetWithDataTable() {
+    public void generatesSnippetWithDataTable() throws Exception {
         List<DataTableRow> dataTable = asList(new DataTableRow(NO_COMMENTS, asList("col1"), 1));
         Step step = new Step(NO_COMMENTS, "Given ", "I have:", 0, dataTable, null);
-        String snippet = (new Backend(null)).getSnippet(step, null);
+        String snippet = (newBackend()).getSnippet(step, null);
         String expected = "" +
                 "(Given #\"^I have:$\" [arg1]\n" +
                 "  (comment  Write code here that turns the phrase above into concrete actions  )\n" +
                 "  (throw (cucumber.api.PendingException.)))\n";
         assertEquals(expected, snippet);
+    }
+
+    private Backend newBackend() throws Exception {
+        return (Backend) Class.forName("cucumber.runtime.clj.Backend").getConstructor(ResourceLoader.class).newInstance(new Object[]{null});
     }
 }
