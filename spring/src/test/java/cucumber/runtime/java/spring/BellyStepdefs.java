10187403931b9d0d689e452e699f691d21c99062
Mykola Gurov <mykola.gurov@tomtom.com>
2014-04-19 11:45:35 +0200

    made BellyStepdefs sideffect-free so that they don't need @Dirty annotation and do not clash with dirtyCukes.feature

--- spring/src/test/java/cucumber/runtime/java/spring/BellyStepdefs.java
+++ spring/src/test/java/cucumber/runtime/java/spring/BellyStepdefs.java
@@ -1,11 +1,10 @@
 package cucumber.runtime.java.spring;
 
-import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @ContextConfiguration("classpath:cucumber.xml")
 public class BellyStepdefs {
@@ -16,28 +15,17 @@ public class BellyStepdefs {
     @Autowired
     private BellyBean bellyBean;
 
-    @Then("^there are (\\d+) cukes in my belly")
-    public void checkCukes(final int n) {
-        assertEquals(n, belly.getCukes());
-    }
-
-    @Given("^I have (\\d+) cukes in my belly")
-    public void haveCukes(final int n) {
-        belly.setCukes(n);
-    }
-
-    @Given("^I have (\\d+) beans in my belly$")
-    public void I_have_beans_in_my_belly(int n) {
-        bellyBean.setCukes(n);
-    }
-
-    @Then("^there are (\\d+) beans in my belly$")
-    public void there_are_beans_in_my_belly(int n) {
-        assertEquals(n, bellyBean.getCukes());
-    }
-
     public BellyBean getBellyBean() {
         return bellyBean;
     }
 
+    @Then("^I have belly$")
+    public void I_have_belly() throws Throwable {
+        assertNotNull(belly);
+    }
+
+    @Then("^I have belly bean$")
+    public void I_have_belly_bean() throws Throwable {
+        assertNotNull(bellyBean);
+    }
 }
--- spring/src/test/resources/cucumber/runtime/java/spring/xmlBasedSpring.feature
+++ spring/src/test/resources/cucumber/runtime/java/spring/xmlBasedSpring.feature
@@ -1,5 +1,4 @@
 Feature: First
 
   Scenario: Eat some beans
-    Given I have 4 beans in my belly
-    Then there are 4 beans in my belly
+    Then I have belly bean
