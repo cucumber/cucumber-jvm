4a17fd522d72b826adee90f610b5376e2b1cf7e6
liviu.carausu <liviu.carausu@gmail.com>
2014-09-02 10:45:39 +0200

    call examples()  see https://groups.google.com/forum/#!topic/cukes/ekQBHtnPiQQ.

--- junit/src/main/java/cucumber/runtime/junit/ExamplesRunner.java
+++ junit/src/main/java/cucumber/runtime/junit/ExamplesRunner.java
@@ -5,6 +5,7 @@ import cucumber.runtime.model.CucumberExamples;
 import cucumber.runtime.model.CucumberScenario;
 import org.junit.runner.Description;
 import org.junit.runner.Runner;
+import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.Suite;
 import org.junit.runners.model.InitializationError;
 
@@ -14,10 +15,12 @@ import java.util.List;
 class ExamplesRunner extends Suite {
     private final CucumberExamples cucumberExamples;
     private Description description;
+    private JUnitReporter jUnitReporter;
 
     protected ExamplesRunner(Runtime runtime, CucumberExamples cucumberExamples, JUnitReporter jUnitReporter) throws InitializationError {
         super(ExamplesRunner.class, new ArrayList<Runner>());
         this.cucumberExamples = cucumberExamples;
+        this.jUnitReporter = jUnitReporter;
 
         List<CucumberScenario> exampleScenarios = cucumberExamples.createExampleScenarios();
         for (CucumberScenario scenario : exampleScenarios) {
@@ -44,5 +47,11 @@ class ExamplesRunner extends Suite {
             }
         }
         return description;
+    }
+
+    @Override
+    public void run(final RunNotifier notifier) {
+        jUnitReporter.examples(cucumberExamples.getExamples());
+        super.run(notifier);
     }
 }
