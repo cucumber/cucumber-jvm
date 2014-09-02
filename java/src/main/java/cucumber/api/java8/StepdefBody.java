111e6f2c290dca71ad2ffd4dcb7fec14727bab7f
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2014-09-02 12:42:42 +0100

    Generate more methods so we don't need casting in the lambda DSL

--- java/src/main/java/cucumber/api/java8/StepdefBody.java
+++ java/src/main/java/cucumber/api/java8/StepdefBody.java
@@ -2,6 +2,11 @@ package cucumber.api.java8;
 
 public interface StepdefBody {
     @FunctionalInterface
+    public static interface A0 extends StepdefBody {
+        void accept();
+    }
+
+    @FunctionalInterface
     public static interface A1<T1> extends StepdefBody {
         void accept(T1 p1);
     }
@@ -34,5 +39,15 @@ public interface StepdefBody {
     @FunctionalInterface
     public static interface A7<T1, T2, T3, T4, T5, T6, T7> extends StepdefBody {
         void accept(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7);
+    }
+
+    @FunctionalInterface
+    public static interface A8<T1, T2, T3, T4, T5, T6, T7, T8> extends StepdefBody {
+        void accept(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7, T8 p8);
+    }
+
+    @FunctionalInterface
+    public static interface A9<T1, T2, T3, T4, T5, T6, T7, T8, T9> extends StepdefBody {
+        void accept(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7, T8 p8, T9 p9);
     }
 }
--- java8/src/test/java/cucumber/runtime/java/test/LambdaStepdefs.java
+++ java8/src/test/java/cucumber/runtime/java/test/LambdaStepdefs.java
@@ -3,7 +3,6 @@ package cucumber.runtime.java.test;
 import cucumber.api.Scenario;
 import cucumber.api.java8.En;
 
-import static cucumber.api.java8.StepdefBody.A2;
 import static org.junit.Assert.assertEquals;
 
 public class LambdaStepdefs implements En {
@@ -12,7 +11,7 @@ public class LambdaStepdefs implements En {
         Before((Scenario scenario) -> {
         });
 
-        Given("I have (\\d+) cukes in my (.*)", (A2<Integer, String>) (Integer cukes, String what) -> {
+        Given("I have (\\d+) cukes in my (.*)", (Integer cukes, String what) -> {
             assertEquals(42, cukes.intValue());
             assertEquals("belly", what);
         });
