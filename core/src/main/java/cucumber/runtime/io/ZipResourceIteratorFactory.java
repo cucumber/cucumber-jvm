cfd70afcf05c035fc12fc03b26d4bf4ff1b52c96
Rui Figueira <rui.figueira@vilt-group.com>
2014-12-22 01:20:16 +0000

    Handles zip and wsjar protocols properly (fixes #808)

--- core/src/main/java/cucumber/runtime/io/ZipResourceIteratorFactory.java
+++ core/src/main/java/cucumber/runtime/io/ZipResourceIteratorFactory.java
@@ -1,29 +1,45 @@
 package cucumber.runtime.io;
 
-import cucumber.runtime.CucumberException;
-
-import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
+
+import cucumber.runtime.CucumberException;
 
 /**
- * Factory which creates {@link ZipResourceIterator}s for URL's with the "jar"
- * protocol.
+ * Factory which creates {@link ZipResourceIterator}s for URL's with "jar", "zip" and "wsjar"
+ * protocols.
  */
 public class ZipResourceIteratorFactory implements ResourceIteratorFactory {
+
+    // weblogic uses zip as protocol for classpath resources inside jars,and webshpere uses wsjar, so
+    // those protocols must be handled too
+    private static final Set<String> SUPPORTED_PROTOCOLS
+        = new HashSet<String>(Arrays.asList("jar", "zip", "wsjar"));
+
     static String filePath(URL jarUrl) throws UnsupportedEncodingException, MalformedURLException {
-        String path = new File(new URL(jarUrl.getFile()).getFile()).getAbsolutePath();
-        String pathToJar = path.substring(0, path.lastIndexOf("!"));
-        return URLDecoder.decode(pathToJar, "UTF-8");
+        String urlFile = jarUrl.getFile();
+        if (urlFile.startsWith("file:")) {
+            urlFile = urlFile.substring("file:".length());
+        }
+
+        int separatorIndex = urlFile.indexOf("!/");
+        if (separatorIndex != -1) {
+            urlFile = urlFile.substring(0, separatorIndex);
+        }
+
+        return URLDecoder.decode(urlFile, "UTF-8");
     }
 
     @Override
     public boolean isFactoryFor(URL url) {
-        return "jar".equals(url.getProtocol());
+        return SUPPORTED_PROTOCOLS.contains(url.getProtocol().toLowerCase());
     }
 
     @Override
