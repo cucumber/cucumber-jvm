046d98c9d20497d2494876775179bbfcd6955222
Michael Wilkerson <mike@jmwilkerson.org>
2015-09-30 15:11:40 -0500

    Fix IllegalArgumentException on JDK 1.8.0_60
    To determine actual types of parameters for class passed in as lambda to
    step definition, code uses old Sun ConstantPool to look for dynamic
    linking data.  Previously these values were found at index size()-2;
    however as of 1.8.0_60 the location appears to have changed to size()-3.
    Given that the following indexes appear to have empty/invalid values,
    instead iterate downward from end to find first valid value and use for
    parameter type determination.

--- java8/src/main/java/cucumber/runtime/java8/ConstantPoolTypeIntrospector.java
+++ java8/src/main/java/cucumber/runtime/java8/ConstantPoolTypeIntrospector.java
@@ -34,8 +34,21 @@ public class ConstantPoolTypeIntrospector implements TypeIntrospector {
     }
 
     private String getTypeString(ConstantPool constantPool) {
-        String[] memberRef = constantPool.getMemberRefInfoAt(constantPool.getSize() - 2);
-        return memberRef[2];
+    	int size = constantPool.getSize();
+		String[] memberRef = null;
+
+    	// find last element in constantpool with valid memberRef
+		//   - previously always at size-2 index but changed with 1.8.0_60
+		for (int i=size-1; i>-1; i--) {
+			try {
+				memberRef = constantPool.getMemberRefInfoAt(i);
+				break;
+			} catch (IllegalArgumentException e) {
+				// eat error; null entry at ConstantPool index?
+			}
+		}
+    		
+		return memberRef[2];
     }
 
 }
