95fd797bb5bcb56c72778e19c50a248a6ae5d2d2
mfrantz <mfrantz@redsealnetworks.com>
2015-10-13 15:35:12 -0700

    java8: Add tests for each arity overload for step definition (Given)

--- java8/src/test/java/cucumber/runtime/java8/test/LambdaStepdefs.java
+++ java8/src/test/java/cucumber/runtime/java8/test/LambdaStepdefs.java
@@ -18,10 +18,97 @@ public class LambdaStepdefs implements En {
             lastInstance = this;
         });
 
+        // No-arg lambda is also OK.
+        Before(() -> {});
+
         Given("^this data table:$", (DataTable peopleTable) -> {
             List<Person> people = peopleTable.asList(Person.class);
             assertEquals("Hellesøy", people.get(0).last);
         });
+
+        // Arity steps
+        Given("0 args",
+              () -> {});
+
+        Given("1 arg: (.)",
+              (String arg1) -> {
+                  assertEquals("a", arg1);
+              });
+
+        Given("2 args: (.) (.)",
+              (String arg1, String arg2) -> {
+                  assertEquals("a", arg1);
+                  assertEquals("b", arg2);
+              });
+
+        Given("3 args: (.) (.) (.)",
+              (String arg1, String arg2, String arg3) -> {
+                  assertEquals("a", arg1);
+                  assertEquals("b", arg2);
+                  assertEquals("c", arg3);
+              });
+
+        Given("4 args: (.) (.) (.) (.)",
+              (String arg1, String arg2, String arg3, String arg4) -> {
+                  assertEquals("a", arg1);
+                  assertEquals("b", arg2);
+                  assertEquals("c", arg3);
+                  assertEquals("d", arg4);
+              });
+
+        Given("5 args: (.) (.) (.) (.) (.)",
+              (String arg1, String arg2, String arg3, String arg4, String arg5) -> {
+                  assertEquals("a", arg1);
+                  assertEquals("b", arg2);
+                  assertEquals("c", arg3);
+                  assertEquals("d", arg4);
+                  assertEquals("e", arg5);
+              });
+
+        Given("6 args: (.) (.) (.) (.) (.) (.)",
+              (String arg1, String arg2, String arg3, String arg4, String arg5, String arg6) -> {
+                  assertEquals("a", arg1);
+                  assertEquals("b", arg2);
+                  assertEquals("c", arg3);
+                  assertEquals("d", arg4);
+                  assertEquals("e", arg5);
+                  assertEquals("f", arg6);
+              });
+
+        Given("7 args: (.) (.) (.) (.) (.) (.) (.)",
+              (String arg1, String arg2, String arg3, String arg4, String arg5, String arg6, String arg7) -> {
+                  assertEquals("a", arg1);
+                  assertEquals("b", arg2);
+                  assertEquals("c", arg3);
+                  assertEquals("d", arg4);
+                  assertEquals("e", arg5);
+                  assertEquals("f", arg6);
+              });
+
+        Given("8 args: (.) (.) (.) (.) (.) (.) (.) (.)",
+              (String arg1, String arg2, String arg3, String arg4, String arg5, String arg6, String arg7,
+               String arg8) -> {
+                  assertEquals("a", arg1);
+                  assertEquals("b", arg2);
+                  assertEquals("c", arg3);
+                  assertEquals("d", arg4);
+                  assertEquals("e", arg5);
+                  assertEquals("f", arg6);
+                  assertEquals("g", arg7);
+              });
+
+        Given("9 args: (.) (.) (.) (.) (.) (.) (.) (.) (.)",
+              (String arg1, String arg2, String arg3, String arg4, String arg5, String arg6, String arg7,
+               String arg8, String arg9) -> {
+                  assertEquals("a", arg1);
+                  assertEquals("b", arg2);
+                  assertEquals("c", arg3);
+                  assertEquals("d", arg4);
+                  assertEquals("e", arg5);
+                  assertEquals("f", arg6);
+                  assertEquals("g", arg7);
+                  assertEquals("h", arg8);
+              });
     }
 
     public static class Person {
