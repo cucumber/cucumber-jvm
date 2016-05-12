cf811be650d8f8d8ca1e09b84db3a40ff0c49637
niberius <zoltor@gmail.com>
2016-05-12 19:33:17 +0300

    URLOutputStreamTest - LinkedBlockingDeque import was put in alphabet order inside other imports

--- core/src/test/java/cucumber/runtime/io/URLOutputStreamTest.java
+++ core/src/test/java/cucumber/runtime/io/URLOutputStreamTest.java
@@ -32,10 +32,10 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
