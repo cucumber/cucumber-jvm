be982d877ef4c37ebb0f5e06951beedbd0c47142
Andy Biewer <andrew.biewer@ge.com>
2016-08-01 16:58:01 -0500

    Log the fatal exception

--- wire/src/main/java/cucumber/java/connectors/wire/WireProtocolHandler.java
+++ wire/src/main/java/cucumber/java/connectors/wire/WireProtocolHandler.java
@@ -1,12 +1,17 @@
 package cucumber.java.connectors.wire;
 
+import cucumber.java.CucumberJavaWire;
 import cucumber.java.CukeEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Wire protocol handler, delegating JSON encoding and decoding to a
  * codec object and running commands on a provided engine instance.
  */
 public class WireProtocolHandler implements ProtocolHandler {
+    private final Logger logger = LoggerFactory.getLogger(CucumberJavaWire.class);
+
     private WireMessageCodec codec;
     private CukeEngine engine;
 
@@ -23,6 +28,7 @@ public class WireProtocolHandler implements ProtocolHandler {
             WireResponse wireResponse = command.run(engine);
             response = codec.encode(wireResponse);
         } catch (Exception e) {
+            logger.error("Fatal error occurred", e);
             response = "[\"fail\"]";
         }
         // LOG response
