f08fd86dd91c181cb0ed37ebf71c11b0bfe788e8
Björn Rasmusson <B.Rasmusson@computer.org>
2016-10-10 20:32:25 +0200

    Use some CucumberExpressions in examples/java-calculator.

--- examples/java-calculator/src/test/java/cucumber/examples/java/calculator/RpnCalculatorStepdefs.java
+++ examples/java-calculator/src/test/java/cucumber/examples/java/calculator/RpnCalculatorStepdefs.java
@@ -19,7 +19,7 @@ public class RpnCalculatorStepdefs {
         calc = new RpnCalculator();
     }
 
-    @When("^I add (\\d+) and (\\d+)$")
+    @When("I add {arg1} and {arg2}")
     public void adding(int arg1, int arg2) {
         calc.push(arg1);
         calc.push(arg2);
