c6594f88ad0c1962197ed204bf2cfe04ea4cbff2
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-03-13 11:56:38 +0100

    fixed javadoc

--- metrics/src/main/java/cucumber/metrics/annotation/regulator/SpeedRegulators.java
+++ metrics/src/main/java/cucumber/metrics/annotation/regulator/SpeedRegulators.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Target;
 public @interface SpeedRegulators {
 
     /**
-     * Array used for multiple SpeedRegulator declarations.
+     * @return Array used for multiple SpeedRegulator declarations.
      */
     SpeedRegulator[] value();
 
