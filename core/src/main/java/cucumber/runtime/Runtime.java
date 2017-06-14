89fc7248e8d0a2ba5a7f8ce300b5519e9c6570da
king-tyler <king-tyler@users.noreply.github.com>
2017-06-13 19:48:01 -0500

    Change constructor parameter type to interface
    There is a Glue interface and RuntimeGlue class which implements it. It would be better that the Runtime constructors use the Glue interface as the optionalGlue parameter rather than the RuntimeGlue class. (See Effective Java #52).

--- core/src/main/java/cucumber/runtime/Runtime.java
+++ core/src/main/java/cucumber/runtime/Runtime.java
@@ -86,12 +86,12 @@ public class Runtime {
     }
 
     public Runtime(ResourceLoader resourceLoader, ClassLoader classLoader, Collection<? extends Backend> backends,
-                   RuntimeOptions runtimeOptions, RuntimeGlue optionalGlue) {
+                   RuntimeOptions runtimeOptions, Glue optionalGlue) {
         this(resourceLoader, classLoader, backends, runtimeOptions, TimeService.SYSTEM, optionalGlue);
     }
 
     public Runtime(ResourceLoader resourceLoader, ClassLoader classLoader, Collection<? extends Backend> backends,
-                   RuntimeOptions runtimeOptions, TimeService stopWatch, RuntimeGlue optionalGlue) {
+                   RuntimeOptions runtimeOptions, TimeService stopWatch, Glue optionalGlue) {
         if (backends.isEmpty()) {
             throw new CucumberException("No backends were found. Please make sure you have a backend module on your CLASSPATH.");
         }
