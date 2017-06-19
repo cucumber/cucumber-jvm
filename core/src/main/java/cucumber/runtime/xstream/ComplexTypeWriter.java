9fcf26a6167c90c3a7c36b8bd879d5b26c15c889
M.P. Korstanje <mpkorstanje@users.noreply.github.com>
2017-06-19 12:04:05 +0200

    Fixed complex type writer and added tests. (#1)
    * Fixed complex type writer and added tests.
    
    * [Core] Add explicit error message to ComplexTypeWriter
     When dealing with nested complex types, the ComplexTypeWriter would produce
     unbalanced and misaligned tables.
    
     This could be resolved by adding  an `@XStreamConverter` to the complex field
     but this was not obvious. By adding an explicit exception this is resolved.
    
     For ease of use this exception is not thrown when acomplex field is empty or
     not included in the table .

--- core/src/main/java/cucumber/runtime/xstream/ComplexTypeWriter.java
+++ core/src/main/java/cucumber/runtime/xstream/ComplexTypeWriter.java
@@ -1,20 +1,25 @@
 package cucumber.runtime.xstream;
 
-import cucumber.runtime.table.CamelCaseStringConverter;
 
+import cucumber.deps.com.thoughtworks.xstream.annotations.XStreamConverter;
+import cucumber.runtime.CucumberException;
+import cucumber.runtime.table.CamelCaseStringConverter;
+import cucumber.runtime.table.PascalCaseStringConverter;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 import static java.util.Arrays.asList;
 
 public class ComplexTypeWriter extends CellWriter {
     private final List<String> columnNames;
-    private Map<String, String> fields = new LinkedHashMap<String, String>();
-    private String currentKey;
-
-    private int nodeDepth = 0;
+    private final Map<String, String> fields = new LinkedHashMap<String, String>();
+    private final Stack<String> currentKey = new Stack<String>();
 
     public ComplexTypeWriter(List<String> columnNames) {
         this.columnNames = columnNames;
@@ -48,10 +53,10 @@ public class ComplexTypeWriter extends CellWriter {
 
     @Override
     public void startNode(String name) {
-        if (nodeDepth == 1) {
-            currentKey = name;
+        currentKey.push(name);
+        if (currentKey.size() == 2) {
+            fields.put(name, "");
         }
-        nodeDepth++;
     }
 
     @Override
@@ -60,13 +65,26 @@ public class ComplexTypeWriter extends CellWriter {
 
     @Override
     public void setValue(String value) {
-        fields.put(currentKey, value == null ? "" : value);
+        // Add all simple types at level 2. nodeDepth 1 is the root node.
+        if(currentKey.size() < 2){
+            return;
+        }
+
+        if (currentKey.size() == 2) {
+            fields.put(currentKey.peek(), value == null ? "" : value);
+            return;
+        }
+
+        final String clazz = currentKey.get(0);
+        final String field = currentKey.get(1);
+        if ((columnNames.isEmpty() || columnNames.contains(field))) {
+            throw createMissingConverterException(clazz, field);
+        }
     }
 
     @Override
     public void endNode() {
-        nodeDepth--;
-        currentKey = null;
+        currentKey.pop();
     }
 
     @Override
@@ -77,5 +95,40 @@ public class ComplexTypeWriter extends CellWriter {
     @Override
     public void close() {
         throw new UnsupportedOperationException();
+    }
+
+    private static CucumberException createMissingConverterException(String clazz, String field) {
+        PascalCaseStringConverter converter = new PascalCaseStringConverter();
+        return new CucumberException(String.format(
+            "Don't know how to convert \"%s.%s\" into a table entry.\n" +
+                "Either exclude %s from the table by selecting the fields to include:\n" +
+                "\n" +
+                "DataTable.create(entries, \"Field\", \"Other Field\")\n" +
+                "\n" +
+                "Or try writing your own converter:\n" +
+                "\n" +
+                "@%s(%sConverter.class)\n" +
+                "%s %s;\n",
+            clazz,
+            field,
+            field,
+            XStreamConverter.class.getName(),
+            converter.map(field),
+            modifierAndTypeOfField(clazz, field),
+            field
+        ));
+    }
+
+    private static String modifierAndTypeOfField(String clazz, String fieldName) {
+        try {
+            Field field = Class.forName(clazz).getDeclaredField(fieldName);
+            String simpleTypeName = field.getType().getSimpleName();
+            String modifiers = Modifier.toString(field.getModifiers());
+            return modifiers + " " + simpleTypeName;
+        } catch (NoSuchFieldException e) {
+            return "private Object";
+        } catch (ClassNotFoundException e) {
+            return "private Object";
+        }
     }
 }
