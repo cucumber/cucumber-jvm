04467c4a864e15a9c5e3d300af8894b91686e54d
rmannibucau <rmannibucau@apache.org>
2017-07-10 12:02:43 +0200

    rebasing

--- junit/src/main/java/cucumber/api/junit/CucumberRule.java
+++ junit/src/main/java/cucumber/api/junit/CucumberRule.java
@@ -1,13 +1,14 @@
 package cucumber.api.junit;
 
+import cucumber.runner.EventBus;
 import cucumber.runtime.ClassFinder;
 import cucumber.runtime.Runtime;
 import cucumber.runtime.RuntimeOptions;
-import cucumber.runtime.RuntimeOptionsFactory;
 import cucumber.runtime.io.MultiLoader;
 import cucumber.runtime.io.ResourceLoader;
 import cucumber.runtime.io.ResourceLoaderClassFinder;
 import cucumber.runtime.junit.FeatureRunner;
+import cucumber.runtime.junit.JUnitOptions;
 import cucumber.runtime.junit.JUnitReporter;
 import cucumber.runtime.model.CucumberFeature;
 import org.junit.rules.TestRule;
@@ -20,39 +21,16 @@ import org.junit.runners.model.Statement;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Collection;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 
 public class CucumberRule implements TestRule {
-    private final RuntimeOptions runtimeOptions;
-
-    public CucumberRule() {
-        this(new RuntimeOptionsFactory(CucumberRule.class /* no conf */).create());
-    }
-
-    public CucumberRule(final RuntimeOptions runtimeOptions) {
-        this.runtimeOptions = runtimeOptions;
-    }
-
-    public CucumberRule(final String defaultPackage, final String... args) {
-        final List<String> options = new ArrayList<String>(args == null ? Collections.<String>emptyList() : asList(args));
-        if (defaultPackage != null) {
-            if (options.isEmpty()) {
-                options.add(MultiLoader.CLASSPATH_SCHEME + defaultPackage.replace('.', '/'));
-            }
-            if (!hasOption(options, "--glue")) {
-                options.add("--glue");
-                options.add(defaultPackage);
-            }
-            if (!hasOption(options, "--plugin")) {
-                options.add("--plugin");
-                options.add("pretty");
-            }
-        }
-        this.runtimeOptions = new RuntimeOptions(options);
-    }
+    private final List<String> runtimeOptions = new ArrayList<String>();
+    private final List<String> junitOptions = new ArrayList<String>();
+    private boolean featureAdded = false;
+    private final Collection<String> potentialFeatures = new ArrayList<String>();
 
     private boolean hasOption(List<String> options, String optName) {
         boolean hasPlugin = false;
@@ -65,8 +43,96 @@ public class CucumberRule implements TestRule {
         return hasPlugin;
     }
 
-    public CucumberRule(final String[] args) {
-        this(null, args);
+    //
+    // shared options
+    //
+
+    public CucumberRule withHelp() {
+        junitOptions.add("--help");
+        runtimeOptions.add("--help");
+        return this;
+    }
+
+    //
+    // junit options
+    //
+
+    public CucumberRule filenameCompatibleNames(final boolean value) {
+        junitOptions.add(optionPrefix(value) + "filename-compatible-names");
+        return this;
+    }
+
+    public CucumberRule stepNotifications(final boolean value) {
+        junitOptions.add(optionPrefix(value) + "step-notifications");
+        return this;
+    }
+
+    //
+    // runtime options
+    //
+
+    public CucumberRule feature(final String value) {
+        runtimeOptions.add(value);
+        featureAdded = true;
+        return this;
+    }
+
+    public CucumberRule glue(final Package value) {
+        runtimeOptions.addAll(asList("--glue", value.getName()));
+        potentialFeatures.add(MultiLoader.CLASSPATH_SCHEME + value.getName().replace('.', '/'));
+        return this;
+    }
+
+    public CucumberRule glue(final String value) {
+        runtimeOptions.addAll(asList("--glue", value));
+        potentialFeatures.add(MultiLoader.CLASSPATH_SCHEME + value.replace('.', '/'));
+        return this;
+    }
+
+    public CucumberRule tag(final String value) {
+        runtimeOptions.addAll(asList("--tag", value));
+        return this;
+    }
+
+    public CucumberRule plugin(final String value) {
+        runtimeOptions.addAll(asList("--plugin", value));
+        return this;
+    }
+
+    public CucumberRule snippets(final String value) {
+        runtimeOptions.addAll(asList("--snippets", value));
+        return this;
+    }
+
+    public CucumberRule name(final String value) {
+        runtimeOptions.addAll(asList("--name", value));
+        return this;
+    }
+
+    public CucumberRule dryRun(final boolean value) {
+        runtimeOptions.add(optionPrefix(value) + "dry-run");
+        return this;
+    }
+
+    public CucumberRule strict(final boolean value) {
+        runtimeOptions.add(optionPrefix(value) + "strict");
+        return this;
+    }
+
+    public CucumberRule monochrome(final boolean value) {
+        runtimeOptions.add(optionPrefix(value) + "monochrome");
+        return this;
+    }
+
+    // passthrough
+    public CucumberRule options(final String... values) {
+        runtimeOptions.addAll(asList(values));
+        featureAdded = true; // we pass in "expert" mode and ignore the feature "guess" logic
+        return this;
+    }
+
+    private String optionPrefix(final boolean value) {
+        return "--" + (!value ? "no-" : "");
     }
 
     @Override
@@ -74,27 +140,39 @@ public class CucumberRule implements TestRule {
         return new Statement() {
             @Override
             public void evaluate() throws Throwable {
+                synchronized (CucumberRule.this) {
+                    if (!hasOption(runtimeOptions, "--plugin")) {
+                        runtimeOptions.add("--plugin");
+                        runtimeOptions.add("pretty");
+                    }
+                    if (!featureAdded) {
+                        runtimeOptions.addAll(potentialFeatures);
+                        potentialFeatures.clear();
+                        featureAdded = true;
+                    }
+                }
+
                 final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
                 final ResourceLoader resourceLoader = new MultiLoader(classLoader);
                 final ClassFinder classFinder = new ResourceLoaderClassFinder(resourceLoader, classLoader);
+                final RuntimeOptions runtimeOptions = new RuntimeOptions(CucumberRule.this.runtimeOptions);
                 final Runtime runtime = new Runtime(resourceLoader, classFinder, classLoader, runtimeOptions);
-                final List<CucumberFeature> cucumberFeatures = runtimeOptions.cucumberFeatures(resourceLoader);
-                final JUnitReporter jUnitReporter = new JUnitReporter(runtimeOptions.reporter(classLoader), runtimeOptions.formatter(classLoader), runtimeOptions.isStrict());
+                final EventBus bus = runtime.getEventBus();
+                final List<CucumberFeature> cucumberFeatures = runtimeOptions.cucumberFeatures(resourceLoader, bus);
+                final JUnitReporter jUnitReporter = new JUnitReporter(bus, runtimeOptions.isStrict(), new JUnitOptions(junitOptions));
                 try {
                     for (final CucumberFeature feature : cucumberFeatures) {
                         new FeatureRunner(feature, runtime, jUnitReporter).run(new RunNotifier());
                     }
                     base.evaluate();
                 } finally {
-                    jUnitReporter.done();
-                    jUnitReporter.close();
                     runtime.printSummary();
                 }
             }
         };
     }
 
-    public static class ForceRunner extends BlockJUnit4ClassRunner {
+    public static class NoTestRunner extends BlockJUnit4ClassRunner {
         private static final FrameworkMethod FAKE_METHOD = new FrameworkMethod(placeholderMethod()) {
             @Override
             public Object invokeExplosively(Object target, Object... params) throws Throwable {
@@ -110,7 +188,7 @@ public class CucumberRule implements TestRule {
             }
         }
 
-        public ForceRunner(final Class<?> klass) throws InitializationError {
+        public NoTestRunner(final Class<?> klass) throws InitializationError {
             super(klass);
         }
 
--- junit/src/test/java/cucumber/runtime/junit/RuleTest.java
+++ junit/src/test/java/cucumber/runtime/junit/RuleTest.java
@@ -4,8 +4,8 @@ import cucumber.api.junit.CucumberRule;
 import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 
-@RunWith(CucumberRule.ForceRunner.class)
+@RunWith(CucumberRule.NoTestRunner.class) // just cause with a test junit runner would fail so this one allows to run such tests
 public class RuleTest {
     @ClassRule
-    public static final CucumberRule CUCUMBER = new CucumberRule(RuleTest.class.getPackage().getName());
+    public static final CucumberRule CUCUMBER = new CucumberRule().glue(RuleTest.class.getPackage());
 }
