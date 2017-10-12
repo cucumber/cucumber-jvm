fbbd4280e74803c0b16ae4ef2f42c99804277013
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-10-12 14:42:59 +0100

    Move cucumber-clojure to https://github.com/cucumber/cucumber-jvm-clojure

--- clojure/src/main/java/cucumber/runtime/clojure/Dummy.java
+++
@@ -1,5 +0,0 @@
-package cucumber.runtime.clojure;
-
-// Nothing to see here, just a workaround for https://github.com/cucumber/cucumber-jvm/issues/270
-public class Dummy {
-}
--- clojure/src/test/java/cucumber/runtime/clojure/RunCukesTest.java
+++
@@ -1,8 +0,0 @@
-package cucumber.runtime.clojure;
-
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-public class RunCukesTest {
-}
