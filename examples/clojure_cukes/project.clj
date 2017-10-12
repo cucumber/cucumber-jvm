b1126aa40ba84e81649da4d5261cbef803fa06fe
M.P. Korstanje <rien.korstanje@gmail.com>
2017-10-12 17:31:06 +0200

    Move cucumber-clojure-examples to https://github.com/cucumber/cucumber-jvm-clojure

--- examples/clojure_cukes/project.clj
+++
@@ -1,9 +0,0 @@
-(defproject clojure_cukes "1.0.X"
-  :description "A demo of Cucumber with Clojure and Leiningen"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[lein-cucumber "1.0.2"]]
-  :cucumber-feature-paths ["test/features/"]
-  :profiles
-  {:dev
-   {:dependencies [[lein-cucumber "1.0.2"]]}}
-)
--- examples/clojure_cukes/test/clojure_cukes/test/core.clj
+++
@@ -1,8 +0,0 @@
-(ns clojure-cukes.test.core
-  (:use [clojure-cukes.core])
-  (:use [clojure.test])
-  (:use [leiningen.cucumber])
-  (:import [cucumber.api.cli Main]))
-
-(deftest run-cukes
-  (. cucumber.api.cli.Main (main (into-array ["--format" "pretty" "--glue" "test" "test/features"]))))
