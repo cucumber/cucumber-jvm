e692fad72c0c23cd1a6a6d244a7ae025ba841109
M.P. Korstanje <rien.korstanje@gmail.com>
2017-10-14 20:07:30 +0200

    Add junit-jupiter example

---
+++ examples/java-calculator-jupiter/src/test/java/cucumber/examples/java/calculator/RunCukesTest.java
@@ -0,0 +1,19 @@
+package cucumber.examples.java.calculator;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.jupiter.CucumberExtension;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.stream.Stream;
+
+@ExtendWith(CucumberExtension.class)
+@CucumberOptions(plugin = "json:target/cucumber-report.json")
+public class RunCukesTest {
+
+    @TestFactory
+    public Stream<DynamicTest> runAllCucumberScenarios(Stream<DynamicTest> scenarios) {
+        return scenarios;
+    }
+}
