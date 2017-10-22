feb014ef6ee464d716c1d2eb1a3739674f2e0ebf
maritvandijk <mlvandijk@gmail.com>
2017-10-22 08:43:18 +0200

    Updates to reflect removal of several jvm languages

--- doc/genapi.sh
+++ doc/genapi.sh
@@ -16,15 +16,3 @@ pushd target/checkout/
   mvn javadoc:aggregate
   cp -R target/site/apidocs/ ../../../api.cucumber.io/cucumber-jvm/${1}/javadoc/
 popd
-
-# ScalaDoc
-pushd target/checkout/scala/
-  mvn scala:doc
-  cp -R scala_2.12/target/site/scaladocs/ ../../../../api.cucumber.io/cucumber-jvm/${1}/scaladoc/
-popd
-
-# Yardoc (Ruby)
-pushd target/checkout/jruby/
-  rake yard
-  cp -R doc/ ../../../../api.cucumber.io/cucumber-jvm/${1}/yardoc/
-popd
