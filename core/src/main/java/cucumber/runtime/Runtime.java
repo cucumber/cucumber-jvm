3a43350b65b5008db72f366efa0be7685056d111
Dmitrii Demin <ddemin@users.noreply.github.com>
2017-11-27 01:36:32 +0300

    Fix #1283
    Fix issue with XStreamConverters auto-registration:
    https://github.com/cucumber/cucumber-jvm/issues/1283

--- core/src/main/java/cucumber/runtime/Runtime.java
+++ core/src/main/java/cucumber/runtime/Runtime.java
@@ -59,7 +59,7 @@ public class Runtime {
         this.classLoader = classLoader;
         this.runtimeOptions = runtimeOptions;
         final Glue glue;
-        glue = optionalGlue == null ? new RuntimeGlue(new LocalizedXStreams(classLoader)) : optionalGlue;
+        glue = optionalGlue == null ? new RuntimeGlue(new LocalizedXStreams(classLoader, runtimeOptions.getConverters())) : optionalGlue;
         this.stats = new Stats(runtimeOptions.isMonochrome());
         this.bus = new EventBus(stopWatch);
         this.runner = new Runner(glue, bus, backends, runtimeOptions);
