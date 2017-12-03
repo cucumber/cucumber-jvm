bb3f95ba27a603fc4617ec8097d52c2398500a84
M.P. Korstanje <rien.korstanje@gmail.com>
2017-12-03 21:53:58 +0100

    Add test case that includes tags to JSON Formatter test.

--- core/src/test/java/cucumber/runtime/formatter/JSONFormatterTest.java
+++ core/src/test/java/cucumber/runtime/formatter/JSONFormatterTest.java
@@ -393,6 +393,89 @@ public class JSONFormatterTest {
     }
 
     @Test
+    public void should_format_feature_and_scenario_with_tags() throws Throwable {
+        CucumberFeature feature = TestHelper.feature("path/test.feature", "" +
+            "@Party @Banana\n" +
+            "Feature: Banana party\n" +
+            "  @Monkey\n" +
+            "  Scenario: Monkey eats more bananas\n" +
+            "    Then the monkey eats more bananas\n");
+        Map<String, Result> stepsToResult = new HashMap<String, Result>();
+        stepsToResult.put("the monkey eats more bananas", result("passed"));
+        Map<String, String> stepsToLocation = new HashMap<String, String>();
+        stepsToLocation.put("the monkey eats more bananas", "StepDefs.monkey_eats_more_bananas()");
+        Long stepDuration = milliSeconds(1);
+
+        String formatterOutput = runFeatureWithJSONPrettyFormatter(feature, stepsToResult, stepsToLocation, stepDuration);
+
+        String expected = "" +
+            "[\n" +
+            "  {\n" +
+            "    \"line\": 2,\n" +
+            "    \"elements\": [\n" +
+            "      {\n" +
+            "        \"line\": 4,\n" +
+            "        \"name\": \"Monkey eats more bananas\",\n" +
+            "        \"description\": \"\",\n" +
+            "        \"id\": \"banana-party;monkey-eats-more-bananas\",\n" +
+            "        \"type\": \"scenario\",\n" +
+            "        \"keyword\": \"Scenario\",\n" +
+            "        \"steps\": [\n" +
+            "          {\n" +
+            "            \"result\": {\n" +
+            "              \"duration\": 1000000,\n" +
+            "              \"status\": \"passed\"\n" +
+            "            },\n" +
+            "            \"line\": 5,\n" +
+            "            \"name\": \"the monkey eats more bananas\",\n" +
+            "            \"match\": {\n" +
+            "              \"location\": \"StepDefs.monkey_eats_more_bananas()\"\n" +
+            "            },\n" +
+            "            \"keyword\": \"Then \"\n" +
+            "          }\n" +
+            "        ],\n" +
+            "        \"tags\": [\n" +
+            "          {\n" +
+            "            \"name\": \"@Party\"\n" +
+            "          },\n" +
+            "          {\n" +
+            "            \"name\": \"@Banana\"\n" +
+            "          },\n" +
+            "          {\n" +
+            "            \"name\": \"@Monkey\"\n" +
+            "          }\n" +
+            "        ]\n" +
+            "      }\n" +
+            "    ],\n" +
+            "    \"name\": \"Banana party\",\n" +
+            "    \"description\": \"\",\n" +
+            "    \"id\": \"banana-party\",\n" +
+            "    \"keyword\": \"Feature\",\n" +
+            "    \"uri\": \"path/test.feature\",\n" +
+            "    \"tags\": [\n" +
+            "      {\n" +
+            "        \"name\": \"@Party\",\n" +
+            "        \"type\": \"Tag\",\n" +
+            "        \"location\": {\n" +
+            "          \"line\": 1,\n" +
+            "          \"column\": 1\n" +
+            "        }\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"name\": \"@Banana\",\n" +
+            "        \"type\": \"Tag\",\n" +
+            "        \"location\": {\n" +
+            "          \"line\": 1,\n" +
+            "          \"column\": 8\n" +
+            "        }\n" +
+            "      }\n" +
+            "    ]\n" +
+            "  }\n" +
+            "]";
+        assertPrettyJsonEquals(expected, formatterOutput);
+    }
+
+    @Test
     public void should_format_scenario_with_hooks() throws Throwable {
         CucumberFeature feature = TestHelper.feature("path/test.feature", "" +
                 "Feature: Banana party\n" +
