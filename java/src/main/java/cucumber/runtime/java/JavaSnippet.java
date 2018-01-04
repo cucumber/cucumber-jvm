5772e6497ccdec5cdc8548fbfd7ca35d9ccf8d9d
Marit van Dijk <mlvandijk@gmail.com>
2018-01-04 16:46:48 +0100

    Remove 'throws Exception' from JavaSnippet (#1308)

--- java/src/main/java/cucumber/runtime/java/JavaSnippet.java
+++ java/src/main/java/cucumber/runtime/java/JavaSnippet.java
@@ -10,7 +10,7 @@ class JavaSnippet extends AbstractJavaSnippet {
     @Override
     public String template() {
         return "@{0}(\"{1}\")\n" +
-                "public void {2}({3}) throws Exception '{'\n" +
+                "public void {2}({3}) '{'\n" +
                 "    // {4}\n" +
                 "{5}    throw new PendingException();\n" +
                 "'}'\n";
