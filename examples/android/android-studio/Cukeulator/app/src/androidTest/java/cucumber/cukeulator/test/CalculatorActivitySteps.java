8d1445fab65a61895f7619da2107e75affc0dc29
Christian Gnüchtel <christian@gnuechtel.com>
2018-01-20 18:36:55 +0100

    Update Cukeulator example project to work with newest Android build tools
    - Update to Android Studio 3.0.1, SDK 26+ and Gradle 4.1
    - Replace Instrumentation class by CucumberRunner
    - CucumberRunner uses AndroidJUnitRunner (JUnit 4+)
    - CalculationSteps class uses ActivityTestRule instead of deprecated ActivityInstrumentationTestCase2
    - Fix permissions to write reports on internal storage

--- examples/android/android-studio/Cukeulator/app/src/androidTest/java/cucumber/cukeulator/test/CalculatorActivitySteps.java
+++ examples/android/android-studio/Cukeulator/app/src/androidTest/java/cucumber/cukeulator/test/CalculatorActivitySteps.java
@@ -1,27 +1,30 @@
 package cucumber.cukeulator.test;
 
-import android.test.ActivityInstrumentationTestCase2;
+import android.app.Activity;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+
 import cucumber.api.CucumberOptions;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.cukeulator.CalculatorActivity;
 import cucumber.cukeulator.R;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertNotNull;
 
 /**
- * We extend ActivityInstrumentationTestCase2 in order to have access to methods like getActivity
- * and getInstrumentation. Depending on what methods we are going to need, we can put our
- * step definitions inside classes extending any of the following Android test classes:
- * <p/>
- * ActivityInstrumentationTestCase2
- * InstrumentationTestCase
- * AndroidTestCase
- * <p/>
+ * We use {@link ActivityTestRule} in order to have access to methods like getActivity
+ * and getInstrumentation.
+ * </p>
  * The CucumberOptions annotation is mandatory for exactly one of the classes in the test project.
  * Only the first annotated class that is found will be used, others are ignored. If no class is
  * annotated, an exception is thrown.
@@ -29,12 +32,45 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
  * The options need to at least specify features = "features". Features must be placed inside
  * assets/features/ of the test project (or a subdirectory thereof).
  */
-@CucumberOptions(features = "features")
-public class CalculatorActivitySteps extends ActivityInstrumentationTestCase2<CalculatorActivity> {
+@CucumberOptions
+public class CalculatorActivitySteps {
 
     public CalculatorActivitySteps(SomeDependency dependency) {
-        super(CalculatorActivity.class);
         assertNotNull(dependency);
+    }
+
+    /**
+    * Since {@link CucumberRunner} and {@link cucumber.api.android.CucumberInstrumentationCore} have the control over the
+    * test lifecycle, activity test rules must not be launched automatically. Automatic launching of test rules is only
+    * feasible for JUnit tests. Fortunately, we are able to launch the activity in Cucumber's {@link Before} method.
+    */
+    @Rule
+    ActivityTestRule rule = new ActivityTestRule<>(CalculatorActivity.class, false, false);
+
+    /**
+     * We launch the activity in Cucumber's {@link Before} hook.
+     * See the notes above for the reasons why we are doing this.
+     * @throws Exception any possible Exception
+     */
+    @Before
+    public void launchActivity() throws Exception {
+        rule.launchActivity(null);
+    }
+
+    /**
+     * All the clean up of application's data and state after each scenario must happen here
+     */
+    @After
+    public void finishActivity() throws Exception {
+        getActivity().finish();
+    }
+
+    /**
+     * Gets the activity from our test rule.
+     * @return the activity
+     */
+    private Activity getActivity() {
+        return rule.getActivity();
     }
 
     @Given("^I have a CalculatorActivity$")
