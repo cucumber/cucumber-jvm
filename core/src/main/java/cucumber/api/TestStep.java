574201457243e45ba956fd017fbf7b4889bd4611
M.P. Korstanje <rien.korstanje@gmail.com>
2018-03-12 18:45:26 +0100

    Add javadoc to TestStep.get*Argument

--- core/src/main/java/cucumber/api/TestStep.java
+++ core/src/main/java/cucumber/api/TestStep.java
@@ -22,7 +22,23 @@ public interface TestStep extends Step {
     gherkin.pickles.PickleStep getPickleStep();
 
 
+    /**
+     * Returns the arguments provided to the step definition.
+     *
+     * For example the step definition <code>Given (.*) pickles</code>
+     * when matched with <code>Given 15 pickles</code> will receive
+     * as argument <code>"15"</code>
+     *
+     * @return argument provided to the step definition
+     */
     List<cucumber.api.Argument> getDefinitionArgument();
+
+    /**
+     * Returns arguments provided to the Gherkin step. E.g:
+     * a data table or doc string.
+     *
+     * @return arguments provided to the gherkin step.
+     */
 
     List<gherkin.pickles.Argument> getStepArgument();
 
