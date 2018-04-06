b22572795f06e7f56f4888bed3d65821a8c31217
Rachel J. King <37672865+rjking99@users.noreply.github.com>
2018-04-06 17:54:45 +0000

    Fix OSGi implementation to work with tycho-surefire

--- osgi/src/main/java/cucumber/java/runtime/osgi/OsgiClassFinder.java
+++ osgi/src/main/java/cucumber/java/runtime/osgi/OsgiClassFinder.java
@@ -8,6 +8,7 @@ import java.util.Enumeration;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.wiring.BundleRevision;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,7 +17,9 @@ import cucumber.runtime.ClassFinder;
 public class OsgiClassFinder implements ClassFinder {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OsgiClassFinder.class);
-
+    private static final String TARGET_CLASSES = "target/classes/";
+	private static final String DOT_CLASS = ".class";
+	
     private final BundleContext bundleContext;
 
     public OsgiClassFinder(BundleContext bundleContext) {
@@ -25,38 +28,66 @@ public class OsgiClassFinder implements ClassFinder {
 
     @Override
     public <T> Collection<Class<? extends T>> getDescendants(Class<T> parentType, String packageName) {
-        if (LOGGER.isDebugEnabled())
-            LOGGER.debug("Looking for sub classes of " + parentType.getName() + " in '" + packageName + "' package");
+       
+    	final String searchPath = packageName.replace('.', '/');
 
-        final String searchPath = packageName.replace('.', '/');
-
-        final ArrayList<Class<? extends T>> result = new ArrayList<Class<? extends T>>();;
+        final ArrayList<Class<? extends T>> result = new ArrayList<>();
+        
         for (Bundle bundle : bundleContext.getBundles()) {
-            try {
-                result.addAll(findClassesInBundle(bundle, searchPath, parentType));
-            } catch (Exception e) {
-                LOGGER.error("Failed to inspect bundle " + bundle.getSymbolicName() + ": " + e.getMessage(), e);
-            }
+            
+        	/** 
+        	 * Fragments don't have a ClassLoader, so don't try to load classes from them.
+        	 * Their classes will be loaded inside their hosts. 
+        	 */
+        	if ((bundle.adapt(BundleRevision.class).getTypes() & BundleRevision.TYPE_FRAGMENT) == 0) {
+        	
+	        	try {
+	        		if (LOGGER.isDebugEnabled())
+	                    LOGGER.debug("Looking for sub classes of " + parentType.getName() + " in '" + packageName + "' package");
+	        		
+	                result.addAll(findClassesInBundle(bundle, "", searchPath, parentType));
+	                
+	                if (LOGGER.isDebugEnabled())
+	                    LOGGER.debug("Looking for fragment classes of " + parentType.getName() + " in '" + searchPath + "' path");
+	                
+	                result.addAll(findClassesInBundle(bundle, TARGET_CLASSES, TARGET_CLASSES+searchPath, parentType));
+	            } 
+	        	catch (Exception e) {
+	                LOGGER.error("Failed to inspect bundle " + bundle.getSymbolicName() + ": " + e.getMessage(), e);
+	            }
+        	}
         }
+        
         result.trimToSize();
+        
+        if (LOGGER.isDebugEnabled())
+			result.stream().forEach(c -> LOGGER.debug("Found class " + c.getName()));
+        
         return result;
     }
 
-    @Override
+    @SuppressWarnings("unchecked")
+	@Override
     public <T> Class<? extends T> loadClass(String className) throws ClassNotFoundException {
-        for (Bundle bundle : bundleContext.getBundles()) {
-            return (Class<? extends T>) bundle.loadClass(className);
+    	Class<? extends T> loadedClass = null;
+    	for (Bundle bundle : bundleContext.getBundles()) {
+            if ((loadedClass = (Class<? extends T>) bundle.loadClass(className)) != null) {
+            	break;
+            }
         }
+    	if (loadedClass != null) {
+    		return loadedClass;
+    	}
         throw new ClassNotFoundException("Couldn't load class from bundles: " + className);
     }
 
-    private <T> Collection<Class<? extends T>> findClassesInBundle(Bundle bundle, String searchPath, Class<T> parentType) {
-        final Collection<Class<? extends T>> result = new ArrayList<Class<? extends T>>();
+    protected <T> Collection<Class<? extends T>> findClassesInBundle(Bundle bundle, String prefix, String searchPath, Class<T> parentType) {
+        final Collection<Class<? extends T>> result = new ArrayList<>();
         final Enumeration<URL> resources = bundle.findEntries(searchPath, "*.class", true);
         if (resources == null)
             return Collections.emptyList();
         for (URL url : Collections.list(resources)) {
-            final String className = pathToClassName(url.getPath());
+            final String className = pathToClassName(prefix, url.getPath());
             try {
                 final Class<? extends T> castClass = loadClassFromBundle(parentType, bundle, className);
                 if (castClass != null)
@@ -68,7 +99,7 @@ public class OsgiClassFinder implements ClassFinder {
         return result;
     }
 
-    private <T> Class<? extends T> loadClassFromBundle(Class<T> parentType, Bundle bundle, String className) throws ClassNotFoundException {
+    protected <T> Class<? extends T> loadClassFromBundle(Class<T> parentType, Bundle bundle, String className) throws ClassNotFoundException {
         final Class<?> clazz = bundle.loadClass(className);
         if (clazz != null && !parentType.equals(clazz) && parentType.isAssignableFrom(clazz)) {
             return clazz.asSubclass(parentType);
@@ -76,7 +107,7 @@ public class OsgiClassFinder implements ClassFinder {
         return null;
     }
 
-    private static String pathToClassName(final String path) {
-        return path.substring(1, path.length() - 6).replace('/', '.');
+    protected static String pathToClassName(final String prefix, final String path) {
+        return path.substring(prefix.length()+1, path.length() - DOT_CLASS.length()).replace('/', '.');
     }
 }
