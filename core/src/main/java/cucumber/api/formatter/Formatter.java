93ff064ba46c5e6a8855e8dfe92471a1f8114a6e
Kiel Boatman <kiel.boatman@yahoo.co.uk>
2018-04-20 11:16:10 +0100

    update Formatter docs - inform that implementations should be threadsafe

--- core/src/main/java/cucumber/api/formatter/Formatter.java
+++ core/src/main/java/cucumber/api/formatter/Formatter.java
@@ -7,6 +7,8 @@ import cucumber.api.event.EventListener;
  * This is the interface you should implement if you want your own custom
  * formatter.
  *
+ * <b>NOTE: Implementations should be threadsafe to support '--threads' parameter</b>
+ *
  * @see EventListener
  * @see Plugin
  */
