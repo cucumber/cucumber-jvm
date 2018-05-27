73abc62bfc54a1d89f028594bf3ae734fac965c9
M.P. Korstanje <rien.korstanje@gmail.com>
2018-05-27 21:13:44 +0200

    Add public constructor to NullSummaryPrinter

--- core/src/main/java/cucumber/runtime/formatter/NullSummaryPrinter.java
+++ core/src/main/java/cucumber/runtime/formatter/NullSummaryPrinter.java
@@ -4,4 +4,9 @@ import cucumber.api.SummaryPrinter;
 
 class NullSummaryPrinter implements SummaryPrinter {
 
+    @SuppressWarnings("WeakerAccess") // Used by PluginFactory
+    public NullSummaryPrinter(){
+
+    }
+
 }
