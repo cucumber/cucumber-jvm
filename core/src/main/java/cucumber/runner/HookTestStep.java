8543111512de14ac3177c51e048eca95f341b71a
M.P. Korstanje <rien.korstanje@gmail.com>
2018-06-11 02:18:45 +0200

    Remove deprecated TestStep methods

--- core/src/main/java/cucumber/runner/HookTestStep.java
+++ core/src/main/java/cucumber/runner/HookTestStep.java
@@ -1,11 +1,7 @@
 package cucumber.runner;
 
-import cucumber.api.Argument;
 import cucumber.api.HookType;
 import cucumber.runtime.HookDefinitionMatch;
-import gherkin.pickles.PickleStep;
-
-import java.util.List;
 
 class HookTestStep extends TestStep implements cucumber.api.HookTestStep {
     private final HookType hookType;
@@ -18,54 +14,6 @@ class HookTestStep extends TestStep implements cucumber.api.HookTestStep {
     @Override
     public HookType getHookType() {
         return hookType;
-    }
-
-    @Override
-    @Deprecated
-    public boolean isHook() {
-        return true;
-    }
-
-    @Override
-    @Deprecated
-    public String getPattern() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    @Deprecated
-    public PickleStep getPickleStep() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    @Deprecated
-    public List<Argument> getDefinitionArgument() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    @Deprecated
-    public List<gherkin.pickles.Argument> getStepArgument() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    @Deprecated
-    public int getStepLine() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    @Deprecated
-    public String getStepLocation() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    @Deprecated
-    public String getStepText() {
-        throw new UnsupportedOperationException();
     }
 
 }
