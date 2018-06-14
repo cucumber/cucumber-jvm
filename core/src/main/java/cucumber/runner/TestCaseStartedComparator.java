b0841ee0a022ce1cb4540e44c9952205be217a3b
Kiel Boatman <kiel.boatman@yahoo.co.uk>
2018-06-14 12:59:28 +0100

    fix typo made before pushing

--- core/src/main/java/cucumber/runner/TestCaseStartedComparator.java
+++ core/src/main/java/cucumber/runner/TestCaseStartedComparator.java
@@ -7,7 +7,7 @@ import java.util.Comparator;
 public class TestCaseStartedComparator implements Comparator<TestCaseStarted> {
     @Override
     public int compare(final TestCaseStarted case1, final TestCaseStarted case2) {
-        final int uriCompare = case1.testCase.getUri().compareTo(case1.testCase.getUri());
+        final int uriCompare = case1.testCase.getUri().compareTo(case2.testCase.getUri());
         if (uriCompare != 0) {
             return uriCompare;
         }
