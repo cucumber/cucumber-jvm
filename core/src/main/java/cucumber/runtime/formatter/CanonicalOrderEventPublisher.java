b6490d0e7c42ab193160b2f6507406f5fc01052d
M.P. Korstanje <rien.korstanje@gmail.com>
2018-06-19 22:46:09 +0200

    Polish

--- core/src/main/java/cucumber/runtime/formatter/CanonicalOrderEventPublisher.java
+++ core/src/main/java/cucumber/runtime/formatter/CanonicalOrderEventPublisher.java
@@ -4,13 +4,13 @@ import cucumber.api.event.Event;
 import cucumber.api.event.TestRunFinished;
 import cucumber.runner.AbstractEventPublisher;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 class CanonicalOrderEventPublisher extends AbstractEventPublisher {
 
-    private final List<Event> queue = new ArrayList<Event>();
+    private final List<Event> queue = new LinkedList<>();
 
     public void handle(final Event event) {
         queue.add(event);
