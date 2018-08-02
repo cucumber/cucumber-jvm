8f2123ff8e8198477d1cca2b133acdcfca0c11ed
subhamproject <subhammandal01@gmail.com>
2018-08-02 11:18:55 +0530

    Update runtests.sh

--- SBI/runtests.sh
+++ SBI/runtests.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 mvn test
+RESULT=$?
+
+[ ! -d jenkins-test-results ] && mkdir jenkins-test-results
+
+clover=$(find . -name clover.xml)
+if [ -n "$clover" ]
+then
+    cp -p $clover jenkins-test-results/
+fi
