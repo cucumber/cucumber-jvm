b9d6c59c4c88f7c6d0ccbe6f6f1f0cd17dede16f
subhamproject <subhammandal01@gmail.com>
2018-08-02 11:36:43 +0530

    Update runtests.sh

--- SBI/runtests.sh
+++ SBI/runtests.sh
@@ -2,11 +2,11 @@
 mvn test
 RESULT=$?
 
-[ ! -d jenkins-cucumber-results ] && mkdir jenkins-cucumber-results
+#[ ! -d jenkins-cucumber-results ] && mkdir jenkins-cucumber-results
 
-cucumber=$(find . -name cucumber-report*.json -type f)
-if [ -n "$cucumber" ]
-then
-    cp -p $cucumber jenkins-cucumber-results/
-fi
+#cucumber=$(find . -name cucumber-report*.json -type f)
+#if [ -n "$cucumber" ]
+#then
+   # cp -p $cucumber jenkins-cucumber-results/
+#fi
 exit $RESULT
