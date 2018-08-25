6a4d329521a0bc5755d72d9b375e043bedf97382
mpkorstanje <logarithmicwhale@gmail.com>
2018-08-25 12:13:56 +0200

    Mock steps based on pickles in test helper

--- core/src/test/java/cucumber/runner/TestHelper.java
+++ core/src/test/java/cucumber/runner/TestHelper.java
@@ -8,6 +8,7 @@ import cucumber.api.event.ConcurrentEventListener;
 import cucumber.api.event.EventListener;
 import cucumber.runtime.BackendSupplier;
 import cucumber.runtime.FeatureSupplier;
+import cucumber.runtime.Glue;
 import cucumber.runtime.HookDefinition;
 import cucumber.runtime.Runtime;
 import cucumber.runtime.StepDefinition;
@@ -18,7 +19,13 @@ import gherkin.AstBuilder;
 import gherkin.Parser;
 import gherkin.TokenMatcher;
 import gherkin.ast.GherkinDocument;
+import gherkin.pickles.Compiler;
+import gherkin.pickles.Pickle;
+import gherkin.pickles.PickleStep;
+import gherkin.pickles.PickleString;
+import gherkin.pickles.PickleTable;
 import gherkin.pickles.PickleTag;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.stepexpression.TypeRegistry;
 import junit.framework.AssertionFailedError;
 import org.mockito.ArgumentMatchers;
@@ -26,16 +33,20 @@ import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
 import java.io.PrintWriter;
+import java.lang.reflect.Type;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
-import java.util.Set;
 
+import static cucumber.api.Result.Type.FAILED;
+import static cucumber.api.Result.Type.PASSED;
+import static cucumber.api.Result.Type.PENDING;
+import static cucumber.api.Result.Type.SKIPPED;
+import static cucumber.api.Result.Type.UNDEFINED;
+import static java.util.Locale.ENGLISH;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
@@ -64,6 +75,176 @@ public class TestHelper {
     private TestHelper() {
     }
 
+    private static final class TestHelperBackendSupplier extends TestBackendSupplier {
+
+        private final List<CucumberFeature> features;
+        private final Map<String, Result> stepsToResult;
+        private final Map<String, String> stepsToLocation;
+        private final List<SimpleEntry<String, Result>> hooks;
+        private final List<String> hookLocations;
+        private final List<Answer<Object>> hookActions;
+
+        private TestHelperBackendSupplier(List<CucumberFeature> features, Map<String, Result> stepsToResult, Map<String, String> stepsToLocation, List<SimpleEntry<String, Result>> hooks, List<String> hookLocations, List<Answer<Object>> hookActions) {
+            this.features = features;
+            this.stepsToResult = stepsToResult;
+            this.stepsToLocation = stepsToLocation;
+            this.hooks = hooks;
+            this.hookLocations = hookLocations;
+            this.hookActions = hookActions;
+        }
+
+
+        @Override
+        public void loadGlue(Glue glue, List<String> gluePaths) {
+            try {
+                mockSteps(glue, features, stepsToResult, stepsToLocation);
+                mockHooks(glue, hooks, hookLocations, hookActions);
+            } catch (Throwable throwable) {
+                throw new RuntimeException(throwable);
+            }
+        }
+
+        private static void mockSteps(Glue glue, List<CucumberFeature> features,
+                                      Map<String, Result> stepsToResult,
+                                      final Map<String, String> stepsToLocation) {
+            Compiler compiler = new Compiler();
+            TypeRegistry typeRegistry = new TypeRegistry(ENGLISH);
+
+            List<PickleStep> steps = new ArrayList<>();
+            for (CucumberFeature feature : features) {
+                for (Pickle pickle : compiler.compile(feature.getGherkinFeature())) {
+                    for (PickleStep step : pickle.getSteps()) {
+                        if (!containsStep(steps, step)) {
+                            steps.add(step);
+                        }
+                    }
+                }
+            }
+
+            for (final PickleStep step : steps) {
+                final Result stepResult = getResultWithDefaultPassed(stepsToResult, step.getText());
+                if (stepResult.is(UNDEFINED)) {
+                    continue;
+                }
+
+                Type[] types = mapArgumentToTypes(step);
+                StepDefinition stepDefinition = new StubStepDefinition(step.getText(), typeRegistry, types) {
+
+                    @Override
+                    public void execute(String language, Object[] args) throws Throwable {
+                        super.execute(language, args);
+                        if (stepResult.is(PENDING)) {
+                            throw new PendingException();
+                        } else if (stepResult.is(FAILED)) {
+                            throw stepResult.getError();
+                        } else if (stepResult.is(SKIPPED) && (stepResult.getError() != null)) {
+                            throw stepResult.getError();
+                        } else if (!stepResult.is(PASSED) && !stepResult.is(SKIPPED)) {
+                            fail("Cannot mock step to the result: " + stepResult.getStatus());
+                        }
+                    }
+
+                    @Override
+                    public String getLocation(boolean detail) {
+                        return stepsToLocation.get(step.getText());
+                    }
+                };
+
+                glue.addStepDefinition(stepDefinition);
+            }
+        }
+
+
+        private static Result getResultWithDefaultPassed(Map<String, Result> stepsToResult, String step) {
+            return stepsToResult.containsKey(step) ? stepsToResult.get(step) : new Result(PASSED, 0L, null);
+        }
+
+
+        private static boolean containsStep(List<PickleStep> steps, PickleStep step) {
+            for (PickleStep definedSteps : steps) {
+                if (definedSteps.getText().equals(step.getText())
+                    && definedSteps.getArgument().size() == step.getArgument().size()
+                ) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static Type[] mapArgumentToTypes(PickleStep step) {
+            Type[] types = new Type[0];
+            if (step.getArgument().isEmpty()) {
+                return types;
+            } else if (step.getArgument().get(0) instanceof PickleString) {
+                types = new Type[]{String.class};
+            } else if (step.getArgument().get(0) instanceof PickleTable) {
+                types = new Type[]{DataTable.class};
+            }
+            return types;
+        }
+
+        private static void mockHooks(cucumber.runtime.Glue glue, final List<SimpleEntry<String, Result>> hooks,
+                                      final List<String> hookLocations,
+                                      final List<Answer<Object>> hookActions) throws Throwable {
+            List<HookDefinition> beforeHooks = new ArrayList<>();
+            List<HookDefinition> afterHooks = new ArrayList<>();
+            List<HookDefinition> beforeStepHooks = new ArrayList<>();
+            List<HookDefinition> afterStepHooks = new ArrayList<>();
+            for (int i = 0; i < hooks.size(); ++i) {
+                String hookLocation = hookLocations.size() > i ? hookLocations.get(i) : null;
+                Answer<Object> hookAction = hookActions.size() > i ? hookActions.get(i) : null;
+                mockHook(hooks.get(i), hookLocation, hookAction, beforeHooks, afterHooks, beforeStepHooks, afterStepHooks);
+            }
+            for (HookDefinition hook : beforeHooks) {
+                glue.addBeforeHook(hook);
+            }
+            for (HookDefinition hook : afterHooks) {
+                glue.addAfterHook(hook);
+            }
+            for (HookDefinition hook : beforeStepHooks) {
+                glue.addBeforeStepHook(hook);
+            }
+            for (HookDefinition hook : afterStepHooks) {
+                glue.addAfterStepHook(hook);
+            }
+        }
+
+        private static void mockHook(final SimpleEntry<String, Result> hookEntry,
+                                     final String hookLocation,
+                                     final Answer<Object> action,
+                                     final List<HookDefinition> beforeHooks,
+                                     final List<HookDefinition> afterHooks,
+                                     final List<HookDefinition> beforeStepHooks,
+                                     final List<HookDefinition> afterStepHooks) throws Throwable {
+            HookDefinition hook = mock(HookDefinition.class);
+            when(hook.matches(ArgumentMatchers.<PickleTag>anyCollection())).thenReturn(true);
+            if (hookLocation != null) {
+                when(hook.getLocation(anyBoolean())).thenReturn(hookLocation);
+            }
+            if (action != null) {
+                doAnswer(action).when(hook).execute((Scenario) any());
+            }
+            if (hookEntry.getValue().is(FAILED)) {
+                doThrow(hookEntry.getValue().getError()).when(hook).execute((cucumber.api.Scenario) any());
+            } else if (hookEntry.getValue().is(PENDING)) {
+                doThrow(new PendingException()).when(hook).execute((cucumber.api.Scenario) any());
+            }
+            if ("before".equals(hookEntry.getKey())) {
+                beforeHooks.add(hook);
+            } else if ("after".equals(hookEntry.getKey())) {
+                afterHooks.add(hook);
+            } else if ("afterstep".equals(hookEntry.getKey())) {
+                afterStepHooks.add(hook);
+            } else if ("beforestep".equals(hookEntry.getKey())) {
+                beforeStepHooks.add(hook);
+            } else {
+                fail("Only before, after and afterstep hooks are allowed, hook type found was: " + hookEntry.getKey());
+            }
+        }
+
+    }
+
     public void run() {
 
         final StringBuilder args = new StringBuilder();
@@ -75,17 +256,15 @@ public class TestHelper {
         final ClasspathResourceLoader resourceLoader = new ClasspathResourceLoader(classLoader);
 
 
-        final BackendSupplier backendSupplier = new TestBackendSupplier() {
-            @Override
-            public void loadGlue(cucumber.runtime.Glue glue, List<String> gluePaths) {
-                try {
-                    TestHelper.mockSteps(glue, stepsToResult, stepsToLocation);
-                    TestHelper.mockHooks(glue, hooks, hookLocations, hookActions);
-                } catch (Throwable throwable) {
-                    throw new RuntimeException(throwable);
-                }
-            }
-        };
+        final BackendSupplier backendSupplier = new TestHelperBackendSupplier(
+            features,
+            stepsToResult,
+            stepsToLocation,
+            hooks,
+            hookLocations,
+            hookActions
+        );
+
         final FeatureSupplier featureSupplier = features.isEmpty()
             ? null // assume feature paths passed in as args instead
             : new FeatureSupplier() {
@@ -282,87 +461,6 @@ public class TestHelper {
         };
     }
 
-    private static void mockSteps(cucumber.runtime.Glue glue, Map<String, Result> stepsToResult, Map<String, String> stepsToLocation) {
-        for (String stepText : mergeStepSets(stepsToResult, stepsToLocation)) {
-            final Result stepResult = getResultWithDefaultPassed(stepsToResult, stepText);
-            if (stepResult.is(Result.Type.UNDEFINED)) {
-                continue;
-            }
-
-            StepDefinition step = new StubStepDefinition(stepText, new TypeRegistry(Locale.ENGLISH)) {
-
-                @Override
-                public void execute(String language, Object[] args) throws Throwable {
-                    super.execute(language, args);
-                    if (stepResult.is(Result.Type.PENDING)) {
-                        throw new PendingException();
-                    } else if (stepResult.is(Result.Type.FAILED)) {
-                        throw stepResult.getError();
-                    } else if (stepResult.is(Result.Type.SKIPPED) && stepResult.getError() != null) {
-                        throw stepResult.getError();
-                    } else if (!stepResult.is(Result.Type.PASSED) && !stepResult.is(Result.Type.SKIPPED)) {
-                        fail("Cannot mock step to the result: " + stepResult.getStatus());
-                    }
-
-                }
-            };
-
-            glue.addStepDefinition(step);
-        }
-    }
-
-    private static void mockHooks(cucumber.runtime.Glue glue, final List<SimpleEntry<String, Result>> hooks, final List<String> hookLocations,
-                                  final List<Answer<Object>> hookActions) throws Throwable {
-        List<HookDefinition> beforeHooks = new ArrayList<>();
-        List<HookDefinition> afterHooks = new ArrayList<>();
-        List<HookDefinition> beforeStepHooks = new ArrayList<>();
-        List<HookDefinition> afterStepHooks = new ArrayList<>();
-        for (int i = 0; i < hooks.size(); ++i) {
-            String hookLocation = hookLocations.size() > i ? hookLocations.get(i) : null;
-            Answer<Object> hookAction = hookActions.size() > i ? hookActions.get(i) : null;
-            TestHelper.mockHook(hooks.get(i), hookLocation, hookAction, beforeHooks, afterHooks, beforeStepHooks, afterStepHooks);
-        }
-        for (HookDefinition hook : beforeHooks) {
-            glue.addBeforeHook(hook);
-        }
-        for (HookDefinition hook : afterHooks) {
-            glue.addAfterHook(hook);
-        }
-        for (HookDefinition hook : beforeStepHooks) {
-            glue.addBeforeStepHook(hook);
-        }
-        for (HookDefinition hook : afterStepHooks) {
-            glue.addAfterStepHook(hook);
-        }
-    }
-
-    private static void mockHook(final SimpleEntry<String, Result> hookEntry, final String hookLocation, final Answer<Object> action,
-                                 final List<HookDefinition> beforeHooks, final List<HookDefinition> afterHooks, final List<HookDefinition> beforeStepHooks, final List<HookDefinition> afterStepHooks) throws Throwable {
-        HookDefinition hook = mock(HookDefinition.class);
-        when(hook.matches(ArgumentMatchers.<PickleTag>anyCollection())).thenReturn(true);
-        if (hookLocation != null) {
-            when(hook.getLocation(anyBoolean())).thenReturn(hookLocation);
-        }
-        if (action != null) {
-            doAnswer(action).when(hook).execute((Scenario) any());
-        }
-        if (hookEntry.getValue().is(Result.Type.FAILED)) {
-            doThrow(hookEntry.getValue().getError()).when(hook).execute((cucumber.api.Scenario) any());
-        } else if (hookEntry.getValue().is(Result.Type.PENDING)) {
-            doThrow(new PendingException()).when(hook).execute((cucumber.api.Scenario) any());
-        }
-        if ("before".equals(hookEntry.getKey())) {
-            beforeHooks.add(hook);
-        } else if ("after".equals(hookEntry.getKey())) {
-            afterHooks.add(hook);
-        } else if ("afterstep".equals(hookEntry.getKey())) {
-            afterStepHooks.add(hook);
-        } else if ("beforestep".equals(hookEntry.getKey())) {
-            beforeStepHooks.add(hook);
-        } else {
-            fail("Only before, after and afterstep hooks are allowed, hook type found was: " + hookEntry.getKey());
-        }
-    }
 
     private static AssertionFailedError mockAssertionFailedError() {
         AssertionFailedError error = mock(AssertionFailedError.class);
@@ -375,6 +473,7 @@ public class TestHelper {
             }
         };
         doAnswer(printStackTraceHandler).when(error).printStackTrace((PrintWriter) any());
+        when(error.getStackTrace()).thenReturn(new StackTraceElement[0]);
         return error;
     }
 
@@ -394,16 +493,6 @@ public class TestHelper {
 
     public static SimpleEntry<String, Result> hookEntry(String type, Result result) {
         return new SimpleEntry<>(type, result);
-    }
-
-    private static Set<String> mergeStepSets(Map<String, Result> stepsToResult, Map<String, String> stepsToLocation) {
-        Set<String> steps = new HashSet<>(stepsToResult.keySet());
-        steps.addAll(stepsToLocation.keySet());
-        return steps;
-    }
-
-    private static Result getResultWithDefaultPassed(Map<String, Result> stepsToResult, String step) {
-        return stepsToResult.containsKey(step) ? stepsToResult.get(step) : new Result(Result.Type.PASSED, 0L, null);
     }
 
 }
--- core/src/test/java/cucumber/runtime/formatter/TimelineFormatterTest.java
+++ core/src/test/java/cucumber/runtime/formatter/TimelineFormatterTest.java
@@ -41,8 +41,8 @@ public class TimelineFormatterTest {
     private static final long STEP_DURATION_MS = 1000;
 
     private final Gson gson = new GsonBuilder().create();
-    private final Map<String, Result> stepsToResult = new HashMap<String, Result>();
-    private final Map<String, String> stepsToLocation = new HashMap<String, String>();
+    private final Map<String, Result> stepsToResult = new HashMap<>();
+    private final Map<String, String> stepsToLocation = new HashMap<>();
 
     private final CucumberFeature failingFeature = feature("some\\windows\\path\\failing.feature", "" +
         "Feature: Failing Feature\n" +
@@ -91,15 +91,8 @@ public class TimelineFormatterTest {
         reportDir = TempDir.createTempDirectory();
         reportJsFile = new File(reportDir, REPORT_JS);
 
-        stepsToResult.put("bg_1", result("passed"));
-        stepsToResult.put("bg_2", result("passed"));
-        stepsToResult.put("bg_3", result("passed"));
-        stepsToResult.put("step_01", result("passed"));
-        stepsToResult.put("step_02", result("passed"));
         stepsToResult.put("step_03", result("failed"));
-        stepsToResult.put("step_10", result("passed"));
-        stepsToResult.put("step_20", result("passed"));
-        stepsToResult.put("step_30", result("passed"));
+        stepsToResult.put("step_50", result("undefined"));
 
         stepsToLocation.put("bg_1", "path/step_definitions.java:3");
         stepsToLocation.put("bg_2", "path/step_definitions.java:4");
