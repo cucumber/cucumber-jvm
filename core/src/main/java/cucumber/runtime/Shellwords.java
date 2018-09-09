d7da122588fe6a95a751d7faf771cbb0e7bf022c
John Patrick <nhoj.patrick@gmail.com>
2018-09-09 23:03:23 +0100

    Shellwords increase tests from 4 to 14 postly whitespace checks

--- core/src/main/java/cucumber/runtime/Shellwords.java
+++ core/src/main/java/cucumber/runtime/Shellwords.java
@@ -6,25 +6,30 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Shellwords {
+
     private static final Pattern SHELLWORDS_PATTERN = Pattern.compile("[^\\s'\"]+|[']([^']*)[']|[\"]([^\"]*)[\"]");
 
     private Shellwords() {
     }
 
     public static List<String> parse(String cmdline) {
-        List<String> matchList = new ArrayList<String>();
+        List<String> matchList = new ArrayList<>();
         Matcher shellwordsMatcher = SHELLWORDS_PATTERN.matcher(cmdline);
         while (shellwordsMatcher.find()) {
             if (shellwordsMatcher.group(1) != null) {
                 matchList.add(shellwordsMatcher.group(1));
             } else {
-                String shellword = shellwordsMatcher.group();
-                if (shellword.startsWith("\"")
-                        && shellword.endsWith("\"")
-                        && shellword.length() > 2) {
-                    shellword = shellword.substring(1, shellword.length() - 1);
+                String token = shellwordsMatcher.group();
+                boolean singleQuoted = token.startsWith("'")
+                    && token.endsWith("'");
+                boolean doubleQuoted = token.startsWith("\"")
+                    && token.endsWith("\"");
+                boolean quoted = singleQuoted || doubleQuoted;
+                if (quoted
+                        && token.length() > 2) {
+                    token = token.substring(1, token.length() - 1);
                 }
-                matchList.add(shellword);
+                matchList.add(token);
             }
         }
         return matchList;
--- core/src/test/java/cucumber/runtime/ShellwordsTest.java
+++ core/src/test/java/cucumber/runtime/ShellwordsTest.java
@@ -1,30 +1,131 @@
 package cucumber.runtime;
 
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 import static java.util.Arrays.asList;
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 
+@RunWith(Parameterized.class)
 public class ShellwordsTest {
 
-    @Test
-    public void parses_single_quoted_strings() {
-        assertEquals(asList("--name", "The Fox"), Shellwords.parse("--name 'The Fox'"));
+    private final List<String> expected;
+    private final String input;
+
+    public ShellwordsTest(final String input, final List<String> expected) {
+        this.input = input;
+        this.expected = expected;
     }
 
     @Test
-    public void parses_double_quoted_strings() {
-        assertEquals(asList("--name", "The Fox"), Shellwords.parse("--name \"The Fox\""));
+    public void parseTest() {
+        assertThat(Shellwords.parse(this.input), is(equalTo(this.expected)));
     }
 
-    @Test
-    public void parses_both_single_and_double_quoted_strings() {
-        assertEquals(asList("--name", "The Fox", "--fur", "Brown White"), Shellwords.parse("--name \"The Fox\" --fur 'Brown White'"));
+    @Parameterized.Parameters(name = "{index}: input={0}, expected={1}")
+    public static Collection<Object[]> data() {
+
+        return Arrays.asList(
+
+            // template
+            // new Object[]{
+            //    input String,
+            //    expected List<String>
+            // },
+
+            // empty string
+            new Object[]{
+                "",
+                new ArrayList<>()
+            },
+
+            // whitespace string
+            new Object[]{
+                "    ",
+                new ArrayList<>()
+            },
+
+            // ---name only
+            new Object[]{
+                "--name",
+                asList("--name")
+            },
+
+            // ---name whitespace padded
+            new Object[]{
+                "     --name     ",
+                asList("--name")
+            },
+
+            // ---name --fur
+            new Object[]{
+                "--name --fur",
+                asList("--name", "--fur")
+            },
+
+            // ---name --fur whitespace padding
+            new Object[]{
+                "     --name     --fur     ",
+                asList("--name", "--fur")
+            },
+
+            // parses_single_quoted_strings
+            new Object[]{
+                "--name 'The Fox'",
+                asList("--name", "The Fox")
+            },
+
+            // parses_single_quoted_strings whitespace padding
+            new Object[]{
+                "     --name     'The Fox'     ",
+                asList("--name", "The Fox")
+            },
+
+            // parses_double_quoted_strings
+            new Object[]{
+                "--name \"The Fox\"",
+                asList("--name", "The Fox")
+            },
+
+            // parses_double_quoted_strings whitespace padding
+            new Object[]{
+                "     --name      \"The Fox\"     ",
+                asList("--name", "The Fox")
+            },
+
+            // double_quote start continue until double_quote end, and ignore single_quote early termination
+            new Object[]{
+                "--name \"The ' Fox\"",
+                asList("--name", "The ' Fox")
+            },
+
+            // single_quote start continue until single_quote end, and ignore double_quote early termination
+            new Object[]{
+                "--name 'The \" Fox'",
+                asList("--name", "The \" Fox")
+            },
+
+            // parses_both_single_and_double_quoted_strings
+            new Object[]{
+                "--name \"The Fox\" --fur 'Brown White'",
+                asList("--name", "The Fox", "--fur", "Brown White")
+            },
+
+            // can_quote_both_single_and_double_quotes
+            new Object[]{
+                "\"'\" '\"'",
+                asList("'", "\"")
+            }
+
+        );
     }
 
-    @Test
-    public void can_quote_both_single_and_double_quotes() {
-        assertEquals(asList("'", "\""), Shellwords.parse("\"'\" '\"'"));
-    }
 }
