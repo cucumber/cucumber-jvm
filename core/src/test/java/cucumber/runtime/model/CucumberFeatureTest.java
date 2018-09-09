bd66bb4a1e3f753ca1b717be9171de3136e302d3
John Patrick <nhoj.patrick@gmail.com>
2018-09-09 23:28:05 +0100

    Replace deprecated org.mockito.Matchers (#1455)

--- core/src/test/java/cucumber/runtime/model/CucumberFeatureTest.java
+++ core/src/test/java/cucumber/runtime/model/CucumberFeatureTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
