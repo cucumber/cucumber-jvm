702ecda890ba1090c9c1e4fe6464a5bdc64712f9
mpkorstanje <logarithmicwhale@gmail.com>
2018-10-04 21:10:17 +0200

    [Java8] Remove debug statements

--- java8/src/main/java/cucumber/runtime/java8/ParameterInfo.java
+++ java8/src/main/java/cucumber/runtime/java8/ParameterInfo.java
@@ -1,6 +1,5 @@
 package cucumber.runtime.java8;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,11 +10,6 @@ class ParameterInfo {
     static List<ParameterInfo> fromTypes(Type[] genericParameterTypes) {
         List<ParameterInfo> result = new ArrayList<>();
         for (Type genericParameterType : genericParameterTypes) {
-            for (Annotation annotation : genericParameterType.getClass().getAnnotations()) {
-                System.out.println(annotation.toString());
-            }
-
-
             result.add(new ParameterInfo(genericParameterType));
         }
         return result;
