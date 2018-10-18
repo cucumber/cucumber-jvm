c1beb21b4490bb54e692f46c8afc4fe3f9b8eff8
James Bennett <james.bennett@bjss.com>
2018-10-18 19:50:51 +0100

    [Guice] Replace CucumberModules.SCENARIO with threadsafe factory method
    Fixes an issue where the same SequentialScenarioScope is shared
    across multiple threads when running in parallel. This causes
    IllegalStateExceptions to be thrown.
    
    CucumberModules.SCENARIO and CucumberScopes.SCENARIO have been deprecated and replaced with a factory
    methods - CucumberModules.createScenarioModule() and CucumberScopes.createScenario() respectively.
    
    Fixes: #1485

--- guice/src/test/java/cucumber/runtime/java/guice/integration/YourInjectorSource.java
+++ guice/src/test/java/cucumber/runtime/java/guice/integration/YourInjectorSource.java
@@ -4,12 +4,14 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Stage;
 import cucumber.api.guice.CucumberModules;
+import cucumber.api.guice.CucumberScopes;
 import cucumber.runtime.java.guice.InjectorSource;
+import cucumber.runtime.java.guice.ScenarioScope;
 
 public class YourInjectorSource implements InjectorSource {
 
     @Override
     public Injector getInjector() {
-        return Guice.createInjector(Stage.PRODUCTION, CucumberModules.SCENARIO, new YourModule());
+        return Guice.createInjector(Stage.PRODUCTION, CucumberModules.createScenarioModule(), new YourModule());
     }
 }
