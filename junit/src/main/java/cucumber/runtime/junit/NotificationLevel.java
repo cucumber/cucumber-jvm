ad18a54d21e086acac7f4aa94dae635271976244
mpkorstanje <logarithmicwhale@gmail.com>
2018-10-18 19:45:16 +0200

    [Core] Use Locale.ROOT when transforming case of identifiers
    Several plugins use lowercased string representations of enum
    values. Doing this without an explicit locale results in the
    default locale being used. This may lead to surprising errors.
    
    Fixes: https://github.com/cucumber/cucumber-java-skeleton/issues/33

--- junit/src/main/java/cucumber/runtime/junit/NotificationLevel.java
+++ junit/src/main/java/cucumber/runtime/junit/NotificationLevel.java
@@ -1,10 +1,12 @@
 package cucumber.runtime.junit;
 
+import static java.util.Locale.ROOT;
+
 public enum NotificationLevel {
     SCENARIO,
     STEP;
 
     String lowerCaseName() {
-        return name().toLowerCase();
+        return name().toLowerCase(ROOT);
     }
 }
