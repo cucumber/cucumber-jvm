430cbe03fd2ddfeb631827720e5e2b636f6a2688
Michiel Leegwater <mleegwt@users.noreply.github.com>
2019-03-16 21:21:17 +0100

    Added Javadoc.

--- core/src/main/java/io/cucumber/core/api/TypeRegistryConfigurer.java
+++ core/src/main/java/io/cucumber/core/api/TypeRegistryConfigurer.java
@@ -2,9 +2,18 @@ package io.cucumber.core.api;
 
 import java.util.Locale;
 
+/**
+ * The type registry configurer allows to configure a new type registry and the locale.
+ */
 public interface TypeRegistryConfigurer {
-
+    /**
+     * @return The locale to use.
+     */
     Locale locale();
 
+    /**
+     * Configures the type registry.
+     * @param typeRegistry The new type registry.
+     */
     void configureTypeRegistry(TypeRegistry typeRegistry);
 }
