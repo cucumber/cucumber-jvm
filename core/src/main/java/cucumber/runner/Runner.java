f67b44f75f84f554932b8290c6e176cfb38b0f29
mpkorstanje <logarithmicwhale@gmail.com>
2019-05-24 17:25:04 +0200

    Fix steps not being disposed of in runner

--- core/src/main/java/cucumber/runner/Runner.java
+++ core/src/main/java/cucumber/runner/Runner.java
@@ -6,12 +6,12 @@ import cucumber.api.event.SnippetsSuggestedEvent;
 import cucumber.runtime.Backend;
 import cucumber.runtime.HookDefinition;
 import cucumber.util.FixJava;
-import io.cucumber.core.logging.Logger;
-import io.cucumber.core.logging.LoggerFactory;
-import io.cucumber.core.options.RunnerOptions;
 import gherkin.events.PickleEvent;
 import gherkin.pickles.PickleStep;
 import gherkin.pickles.PickleTag;
+import io.cucumber.core.logging.Logger;
+import io.cucumber.core.logging.LoggerFactory;
+import io.cucumber.core.options.RunnerOptions;
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -135,5 +135,6 @@ public final class Runner {
         for (Backend backend : backends) {
             backend.disposeWorld();
         }
+        glue.removeScenarioScopedGlue();
     }
 }
