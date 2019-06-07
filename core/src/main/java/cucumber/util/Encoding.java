550ef4bf02bf583ea831176a6a2da645f6f5cecc
grasshopper <cool.infy.951@gmail.com>
2019-06-07 15:23:22 +0530

    [Core] Parse UTF-8-BOM Feature File (#1654)
    Fixed failure to parse UTF-8-BOM encoded feature file.
    
    Fixes: #1639

--- core/src/main/java/cucumber/util/Encoding.java
+++ core/src/main/java/cucumber/util/Encoding.java
@@ -16,9 +16,14 @@ public class Encoding {
     private static final Pattern COMMENT_OR_EMPTY_LINE_PATTERN = Pattern.compile("^\\s*#|^\\s*$");
     private static final Pattern ENCODING_PATTERN = Pattern.compile("^\\s*#\\s*encoding\\s*:\\s*([0-9a-zA-Z\\-]+)", Pattern.CASE_INSENSITIVE);
     public static final String DEFAULT_ENCODING = "UTF-8";
+    private static final String UTF_8_BOM = "\uFEFF";
 
     public static String readFile(Resource resource) throws RuntimeException, IOException {
         String source = FixJava.readReader(new InputStreamReader(resource.getInputStream(), DEFAULT_ENCODING));
+     // Remove UTF8 BOM encoded in first bytes
+     	if (source.startsWith(UTF_8_BOM)) {
+     		source = source.replaceFirst(UTF_8_BOM, "");
+     	}
         String enc = encoding(source);
         if(!enc.equals(DEFAULT_ENCODING)) {
             source = FixJava.readReader(new InputStreamReader(resource.getInputStream(), enc));
