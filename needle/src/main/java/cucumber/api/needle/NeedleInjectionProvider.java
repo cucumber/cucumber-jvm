3f1e458e8077b9e70704473e93d936433254ef5b
mpkorstanje <logarithmicwhale@gmail.com>
2019-06-17 00:50:56 +0200

    [Build] Bump Cucumber version

--- needle/src/main/java/cucumber/api/needle/NeedleInjectionProvider.java
+++ needle/src/main/java/cucumber/api/needle/NeedleInjectionProvider.java
@@ -1,7 +1,5 @@
 package cucumber.api.needle;
 
-import de.akquinet.jbosscc.needle.injection.InjectionProvider;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -9,7 +7,7 @@ import java.lang.annotation.Target;
 
 /**
  * Annotation to mark InjectionProviders in the cucumber glue or cucumber steps.
- * Should be placed on fields of type {@link InjectionProvider} or an array of those.
+ * Should be placed on fields of type {@link de.akquinet.jbosscc.needle.injection.InjectionProvider} or an array of those.
  *
  * @deprecated use {@code NeedleInjectionProvider} instead
  */
