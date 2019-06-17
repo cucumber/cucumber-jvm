79d2c78fb3d5cdc51cccfdb552d48f3bf2da1dce
mpkorstanje <logarithmicwhale@gmail.com>
2019-06-17 22:45:28 +0200

    [Java] Extract annotation generation to groovy script

---
+++ java/src/main/groovy/annotation.java.gsp
@@ -0,0 +1,61 @@
+package cucumber.api.java.${lang};
+
+import cucumber.runtime.java.StepDefAnnotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * To execute steps in a feature file the steps must be
+ * connected to executable code. This can be done by annotating
+ * a method with a cucumber or regular expression.
+ * <p>
+ * The parameters extracted from the step by the expression
+ * along with the data table or doc string argument are provided as
+ * arguments to the method.
+ * <p>
+ * The types of the parameters are determined by the cucumber or
+ * regular expression.
+ * <p>
+ * The type of the data table or doc string argument is determined
+ * by the argument name value. When none is provided cucumber will
+ * attempt to transform the data table or doc string to the type
+ * of the last argument.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@StepDefAnnotation
+@Documented
+public @interface ${kw} {
+    /**
+     * A cucumber or regular expression.
+     *
+     * @return a cucumber or regular expression
+     */
+    String value();
+
+    /**
+     * Duration in milliseconds this step is allowed to run. Cucumber
+     * will mark the step as failed when exceeded.
+     *
+     * When the maximum  duration is exceeded the thread will
+     * receive an in interrupt. Note: if the interrupt is ignored
+     * cucumber will wait for the this hook to finish.
+     *
+     * @return timeout in milliseconds. 0 (default) means no restriction.
+     */
+    long timeout() default 0;
+
+    /**
+     * Allows the use of multiple '${kw}'s on a single method.
+     */
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @interface ${kw}s {
+        ${kw}[] value();
+    }
+}
---
+++ java/src/main/groovy/generate-annotations.groovy
@@ -0,0 +1,62 @@
+import gherkin.GherkinDialectProvider
+import groovy.text.SimpleTemplateEngine
+
+import java.text.Normalizer
+
+SimpleTemplateEngine engine = new SimpleTemplateEngine()
+def templateSource = new File(project.baseDir, "src/main/groovy/annotation.java.gsp").getText()
+
+static def normalize(s) {
+    if (System.getProperty("java.version").startsWith("1.6")) {
+        return s
+    } else {
+        return Normalizer.normalize(s, Normalizer.Form.NFC)
+    }
+}
+
+def localeFor(lang) {
+    languageAndCountry = lang.split("-")
+    if (languageAndCountry.length == 1) {
+        return new Locale(lang)
+    } else {
+        return new Locale(languageAndCountry[0], languageAndCountry[1])
+    }
+}
+
+// TODO: Need to add i18n.getName() and i18n.getNative() for better names.
+def package_info_java = """\
+/**
+ * \${locale.getDisplayLanguage()}
+ */
+package io.cucumber.java.api.\${normalized_language}; 
+"""
+
+
+def unsupported = ["em"] // The generated files for Emoij do not compile.
+def dialectProvider = new GherkinDialectProvider()
+
+GherkinDialectProvider.DIALECTS.keySet().each { language ->
+    def dialect = dialectProvider.getDialect(language, null)
+    def normalized_language = dialect.language.replaceAll("[\\s-]", "_").toLowerCase()
+    if (!unsupported.contains(normalized_language)) {
+        dialect.stepKeywords.findAll { !it.contains('*') && !it.matches("^\\d.*") }.unique().each { kw ->
+            def normalized_kw = normalize(kw.replaceAll("[\\s',!\u00AD]", ""))
+            def binding = ["lang": normalized_language, "kw": normalized_kw]
+            def template = engine.createTemplate(templateSource).make(binding)
+            def file = new File(project.baseDir, "target/generated-sources/i18n/java/cucumber/api/java/${normalized_language}/${normalized_kw}.java")
+            if (!file.exists()) {
+                // Haitian has two translations that only differ by case - Sipozeke and SipozeKe
+                // Some file systems are unable to distinguish between them and overwrite the other one :-(
+                file.parentFile.mkdirs()
+                file.write(template.toString(), "UTF-8")
+            }
+        }
+
+        // package-info.java
+        def locale = localeFor(dialect.language)
+        def binding = [ "locale": locale, "normalized_language": normalized_language ]
+        def html = engine.createTemplate(package_info_java).make(binding).toString()
+        def file = new File(project.baseDir, "target/generated-sources/i18n/java/cucumber/api/java/${normalized_language}/package-info.java")
+        file.write(html, "UTF-8")
+    }
+}
