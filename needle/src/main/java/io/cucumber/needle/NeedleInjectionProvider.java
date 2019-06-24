3520cf63894e540a06db957c63c0333fcbe37a6f
M.P. Korstanje <logarithmicwhale@gmail.com>
2019-06-24 18:53:39 +0200

    [CDI2] Mark public API

--- needle/src/main/java/io/cucumber/needle/NeedleInjectionProvider.java
+++ needle/src/main/java/io/cucumber/needle/NeedleInjectionProvider.java
@@ -1,6 +1,7 @@
 package io.cucumber.needle;
 
 import de.akquinet.jbosscc.needle.injection.InjectionProvider;
+import org.apiguardian.api.API;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -13,5 +14,6 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
+@API(status = API.Status.STABLE)
 public @interface NeedleInjectionProvider {
 }
