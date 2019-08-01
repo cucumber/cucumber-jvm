13d88b186003ffcf62954602411b99995074f65c
M.P. Korstanje <logarithmicwhale@gmail.com>
2019-08-01 23:26:08 +0200

    [Core] Convert empty pickle table cells to null
    Fixes: https://github.com/cucumber/cucumber-jvm/issues/1617

--- core/src/main/java/io/cucumber/core/stepexpression/PickleTableConverter.java
+++ core/src/main/java/io/cucumber/core/stepexpression/PickleTableConverter.java
@@ -14,11 +14,12 @@ final class PickleTableConverter {
     }
 
     static List<List<String>> toTable(PickleTable pickleTable) {
-        List<List<String>> table = new ArrayList<List<String>>();
+        List<List<String>> table = new ArrayList<>();
         for (PickleRow pickleRow : pickleTable.getRows()) {
-            List<String> row = new ArrayList<String>();
+            List<String> row = new ArrayList<>();
             for (PickleCell pickleCell : pickleRow.getCells()) {
-                row.add(pickleCell.getValue());
+                String value = pickleCell.getValue();
+                row.add(value.isEmpty() ? null : value);
             }
             table.add(row);
         }
--- core/src/main/java/io/cucumber/core/stepexpression/RawTableTransformer.java
+++ core/src/main/java/io/cucumber/core/stepexpression/RawTableTransformer.java
@@ -2,6 +2,7 @@ package io.cucumber.core.stepexpression;
 
 import java.util.List;
 
+@FunctionalInterface
 interface RawTableTransformer<T> {
     T transform(List<List<String>> raw);
 }
