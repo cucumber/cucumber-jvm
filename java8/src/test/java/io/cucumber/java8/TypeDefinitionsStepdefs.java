9251ef040c62acd56bb19a361bce982bc6453c34
Anton Deryabin <ander.2142@gmail.com>
2019-09-23 19:05:31 +0300

    more tests

--- java8/src/test/java/io/cucumber/java8/TypeDefinitionsStepdefs.java
+++ java8/src/test/java/io/cucumber/java8/TypeDefinitionsStepdefs.java
@@ -3,11 +3,14 @@ package io.cucumber.java8;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import io.cucumber.datatable.DataTable;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class TypeDefinitionsStepdefs implements En{
     public TypeDefinitionsStepdefs() {
-        Given("docstring, defined with lambda",
+        Given("docstring, defined by lambda",
             (StringBuilder builder) -> assertThat(builder.getClass(), equalTo(StringBuilder.class)));
         DocStringType("doc", (String docString) -> new StringBuilder(docString));
 
@@ -15,10 +18,18 @@ public class TypeDefinitionsStepdefs implements En{
             return new Author(entry.get("name"), entry.get("surname"), entry.get("famousBook"));
         });
 
-        Given("data table, defined with lambda", (Author author) -> {
+        Given("single entry data table, defined by lambda", (Author author) -> {
             assertThat(author.name, equalTo("Fedor"));
             assertThat(author.surname, equalTo("Dostoevsky"));
             assertThat(author.famousBook, equalTo("Crime and Punishment"));
+        });
+
+        Given("data table, defined by lambda", (DataTable dataTable) -> {
+            List<Author> authors = dataTable.asList(Author.class);
+            Author dostoevsky = new Author("Fedor","Dostoevsky", "Crime and Punishment");
+            Author tolstoy = new Author("Lev", "Tolstoy", "War and Peace");
+            assertThat(authors.get(0), equalTo(dostoevsky));
+            assertThat(authors.get(1), equalTo(tolstoy));
         });
 
         Given("{stringbuilder} parameter, defined by lambda", (StringBuilder builder) -> {
@@ -38,6 +49,21 @@ public class TypeDefinitionsStepdefs implements En{
             this.name = name;
             this.surname = surname;
             this.famousBook = famousBook;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Author author = (Author) o;
+            return Objects.equals(name, author.name) &&
+                Objects.equals(surname, author.surname) &&
+                Objects.equals(famousBook, author.famousBook);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, surname, famousBook);
         }
     }
 }
--- java8/src/test/resources/io/cucumber/java8/lambda-type-definitions.feature
+++ java8/src/test/resources/io/cucumber/java8/lambda-type-definitions.feature
@@ -1,15 +1,21 @@
 Feature: Lambda type definition
 
   Scenario: define docstring type by lambda
-    Given docstring, defined with lambda
+    Given docstring, defined by lambda
     """doc
     really long docstring
     """
 
-  Scenario: define data table type by lambda
-    Given data table, defined with lambda
+  Scenario: define single entry data table type by lambda
+    Given single entry data table, defined by lambda
       |name  | surname       | famousBook          |
       |Fedor | Dostoevsky    |Crime and Punishment |
+
+  Scenario: define  data table type by lambda
+    Given data table, defined by lambda
+      |name  | surname       | famousBook          |
+      |Fedor | Dostoevsky    |Crime and Punishment |
+      |Lev   | Tolstoy       |War and Peace        |
 
   Scenario: define parameter type by lambda
     Given stringbuilder parameter, defined by lambda
