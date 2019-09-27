de71b1533aadbae54656015952d0ccaac2c7f10a
Tim te Beek <tim.te.beek@jdriven.com>
2019-09-27 22:18:49 +0200

    Add ParameterTypes with 4-9 arguments

--- java8/src/main/java/io/cucumber/java8/ParameterDefinitionBody.java
+++ java8/src/main/java/io/cucumber/java8/ParameterDefinitionBody.java
@@ -16,4 +16,28 @@ public interface ParameterDefinitionBody<R> {
     interface A3<R> extends ParameterDefinitionBody<R> {
         R accept(String p1, String p2, String p3) throws Throwable;
     }
+    @FunctionalInterface
+    interface A4<R> extends ParameterDefinitionBody<R> {
+        R accept(String p1, String p2, String p3, String p4) throws Throwable;
+    }
+    @FunctionalInterface
+    interface A5<R> extends ParameterDefinitionBody<R> {
+        R accept(String p1, String p2, String p3, String p4, String p5) throws Throwable;
+    }
+    @FunctionalInterface
+    interface A6<R> extends ParameterDefinitionBody<R> {
+        R accept(String p1, String p2, String p3, String p4, String p5, String p6) throws Throwable;
+    }
+    @FunctionalInterface
+    interface A7<R> extends ParameterDefinitionBody<R> {
+        R accept(String p1, String p2, String p3, String p4, String p5, String p6, String p7) throws Throwable;
+    }
+    @FunctionalInterface
+    interface A8<R> extends ParameterDefinitionBody<R> {
+        R accept(String p1, String p2, String p3, String p4, String p5, String p6, String p7, String p8) throws Throwable;
+    }
+    @FunctionalInterface
+    interface A9<R> extends ParameterDefinitionBody<R> {
+        R accept(String p1, String p2, String p3, String p4, String p5, String p6, String p7, String p8, String p9) throws Throwable;
+    }
 }
