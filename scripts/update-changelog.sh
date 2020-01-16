f3aa938277d6f7fb03f9f5fc06eb0f8e396f0cbe
M.P. Korstanje <rien.korstanje@gmail.com>
2020-01-16 11:44:49 +0100

    [Build] Support release candidate versions in update changelog

--- scripts/update-changelog.sh
+++ scripts/update-changelog.sh
@@ -45,7 +45,7 @@ line_number_colon_unreleased_link=$(echo "${changelog}" | grep -n "\[Unreleased\
 line_number=$(echo "${line_number_colon_unreleased_link}" | cut -d: -f1)
 unreleased_link=$(echo "${line_number_colon_unreleased_link}" | cut -d' ' -f2)
 
-if [[ "${unreleased_link}" =~ \/v([0-9]+\.[0-9]+\.[0-9]+) ]]; then
+if [[ "${unreleased_link}" =~ \/v([0-9]+\.[0-9]+\.[0-9]+(-RC[0-9]+)?) ]]; then
   last_version="${BASH_REMATCH[1]}"
   changelog=$(echo "${changelog}" | sed "s/v${last_version}\.\.\./v${new_version}.../")
 else
