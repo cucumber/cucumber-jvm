523acac8d275b01cba1af4f9cf4ad80451881a99
M.P. Korstanje <rien.korstanje@gmail.com>
2020-01-16 17:07:39 +0100

    [Archetype] Add Maven Archetype module to project
    The maven archetype lives in its own project[1] which makes it
    cumbersome to keep it up-to-date with on going releases. As
    each release of Cucumber-JVM results in a release of the archetype
    it is sensible to keep both project together.
    
    The module uses resource filtering[2] to replace the `{project.version}`
    in the archetypes `pom.xml`. This ensures no manual version updates
    are needed.
    
    1. https://github.com/cucumber/cucumber-archetype
    2. http://maven.apache.org/plugins/maven-resources-plugin

---
+++ archetype/src/main/resources/archetype-resources/src/test/java/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package ${package};
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunCucumberTest {
+}
---
+++ archetype/src/main/resources/archetype-resources/src/test/java/StepDefinitions.java
@@ -0,0 +1,11 @@
+package ${package};
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.*;
+
+public class StepDefinitions {
+
+}
