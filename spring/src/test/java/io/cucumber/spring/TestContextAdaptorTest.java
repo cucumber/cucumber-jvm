3b56c93de92b5469ae1981862e7000c7ecf9fca6
M.P. Korstanje <mpkorstanje@users.noreply.github.com>
2020-02-28 17:23:00 +0100

    [Spring] Do not reuse generic application context between scenarios (#1905)
    When neither a glue class with a spring context configuration could be found
    nor a `cucumber.xml` file exists `cucumber-spring` will fall back to a
    `GenericApplicationContext` to facilitate dependency injection.
    
    The current implementation tries to reuse this application context between
    scenarios. This requires refreshing the context which can only be done once.
    So it's not possible to reuse the `GenericApplicationContext` and a new one
    should be created for each scenario.

--- spring/src/test/java/io/cucumber/spring/TestContextAdaptorTest.java
+++ spring/src/test/java/io/cucumber/spring/TestContextAdaptorTest.java
@@ -3,8 +3,15 @@ package io.cucumber.spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.GenericApplicationContext;
 
+import static io.cucumber.spring.TestContextAdaptor.createApplicationContextAdaptor;
+import static io.cucumber.spring.TestContextAdaptor.createClassPathXmlApplicationContextAdaptor;
+import static io.cucumber.spring.TestContextAdaptor.createGenericApplicationContextAdaptor;
 import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TestContextAdaptorTest {
 
@@ -17,13 +24,37 @@ class TestContextAdaptorTest {
         GenericApplicationContext context = new GenericApplicationContext();
         context.setAllowBeanDefinitionOverriding(false);
 
-        TestContextAdaptor adaptor1 = TestContextAdaptor.createApplicationContextAdaptor(context, asList(GlueClass.class, GlueClass.class));
+        TestContextAdaptor adaptor1 = createApplicationContextAdaptor(context, asList(GlueClass.class, GlueClass.class));
         adaptor1.start();
-        GlueCodeContext.getInstance().start();
         GlueClass instance1 = adaptor1.getInstance(GlueClass.class);
         assertNotNull(instance1);
         adaptor1.stop();
-        GlueCodeContext.getInstance().stop();
+    }
+
+    @Test
+    void xmlApplicationContextCanBeReUsedBetweenScenarios() {
+        String[] configLocations = {"cucumber.xml"};
+        TestContextAdaptor adaptor1 = createClassPathXmlApplicationContextAdaptor(configLocations, asList(GlueClass.class, GlueClass.class));
+        adaptor1.start();
+        GlueClass instance1 = adaptor1.getInstance(GlueClass.class);
+        assertNotNull(instance1);
+        adaptor1.stop();
+
+        adaptor1.start();
+        GlueClass instance2 = adaptor1.getInstance(GlueClass.class);
+        assertNotNull(instance2);
+        assertNotSame(instance1, instance2);
+        adaptor1.stop();
+    }
+
+    @Test
+    void configurableApplicationContextCanNotBeReused() {
+        TestContextAdaptor adaptor = createGenericApplicationContextAdaptor(asList(GlueClass.class, GlueClass.class));
+        adaptor.start();
+        adaptor.stop();
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class, adaptor::start);
+        assertThat(exception.getMessage(), containsString("GenericApplicationContext does not support multiple refresh attempts"));
     }
 
 }
