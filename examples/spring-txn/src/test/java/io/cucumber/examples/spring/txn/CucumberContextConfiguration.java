151fab20523a73286906c08461d84760bc4f0b0d
Anton Deryabin <ander.2142@gmail.com>
2020-03-04 16:29:49 +0300

    cucumber-spring backend implementation

--- examples/spring-txn/src/test/java/io/cucumber/examples/spring/txn/CucumberContextConfiguration.java
+++ examples/spring-txn/src/test/java/io/cucumber/examples/spring/txn/CucumberContextConfiguration.java
@@ -1,16 +1,12 @@
 package io.cucumber.examples.spring.txn;
 
-import io.cucumber.java.Before;
+import io.cucumber.spring.CucumberSpringTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@CucumberSpringTest
 @SpringBootTest
 @AutoConfigureMockMvc
 public class CucumberContextConfiguration  {
 
-    @Before
-    public void setup_cucumber_spring_context(){
-        // Dummy method so cucumber will recognize this class as glue
-        // and use its context configuration.
-    }
 }
---
+++ spring/src/main/java/io/cucumber/spring/CucumberSpringTest.java
@@ -0,0 +1,14 @@
+package io.cucumber.spring;
+
+import org.apiguardian.api.API;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@API(status = API.Status.STABLE)
+public @interface CucumberSpringTest {
+}
