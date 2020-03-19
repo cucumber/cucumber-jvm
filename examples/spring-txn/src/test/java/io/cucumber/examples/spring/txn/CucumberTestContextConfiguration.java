a854aac4d407838503c4a6b9f5a3fdc1f038001c
M.P. Korstanje <rien.korstanje@gmail.com>
2020-03-19 17:55:08 +0100

    Duplicate names

--- examples/spring-txn/src/test/java/io/cucumber/examples/spring/txn/CucumberTestContextConfiguration.java
+++ examples/spring-txn/src/test/java/io/cucumber/examples/spring/txn/CucumberTestContextConfiguration.java
@@ -1,11 +1,11 @@
 package io.cucumber.examples.spring.txn;
 
+import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@io.cucumber.spring.CucumberContextConfiguration
+@CucumberContextConfiguration
 @SpringBootTest
 @AutoConfigureMockMvc
-public class CucumberContextConfiguration  {
-
+public class CucumberTestContextConfiguration {
 }
