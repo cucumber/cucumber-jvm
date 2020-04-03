ca2638e044702ee451656f6162f9a010fd436cd9
Tim te Beek <tim.te.beek@jdriven.com>
2020-04-03 20:25:39 +0200

    Use Jupiter Assertions over JUnit4 Assert

--- spring/src/test/java/io/cucumber/spring/contextcaching/SomeTest.java
+++ spring/src/test/java/io/cucumber/spring/contextcaching/SomeTest.java
@@ -1,7 +1,7 @@
 package io.cucumber.spring.contextcaching;
 
 import org.hamcrest.MatcherAssert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -12,12 +12,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {ContextConfig.class})
-public class SomeTest {
+class SomeTest {
     @Autowired
     ContextCounter contextCounter;
 
     @Test
-    public void contextCountIsOne() {
+    void contextCountIsOne() {
         assertThat(contextCounter.getContextCount(), is(1));
     }
 
