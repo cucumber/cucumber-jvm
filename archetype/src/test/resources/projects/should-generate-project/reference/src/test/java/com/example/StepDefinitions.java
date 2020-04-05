250649c6afad6df66871f576e6024f6cf997760a
Tim te Beek <tim.te.beek@jdriven.com>
2020-04-05 11:05:04 +0200

    Revert archetype changes

--- archetype/src/test/resources/projects/should-generate-project/reference/src/test/java/com/example/StepDefinitions.java
+++ archetype/src/test/resources/projects/should-generate-project/reference/src/test/java/com/example/StepDefinitions.java
@@ -4,7 +4,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 public class StepDefinitions {
 
