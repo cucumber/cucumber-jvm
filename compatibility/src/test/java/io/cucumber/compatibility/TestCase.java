8500096a60d36a95066320b578c0eb3d523b8924
M.P. Korstanje <rien.korstanje@gmail.com>
2020-04-30 16:14:47 +0200

    Minimize diff

--- compatibility/src/test/java/io/cucumber/compatibility/TestCase.java
+++ compatibility/src/test/java/io/cucumber/compatibility/TestCase.java
@@ -10,6 +10,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import static java.util.Comparator.comparing;
