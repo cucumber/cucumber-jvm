0136079aa07e5bb14e6d1bdc9afb2d09d5da7570
M.P. Korstanje <rien.korstanje@gmail.com>
2020-04-30 18:57:07 +0200

    [Java] Switch `useRegexpMatchAsStrongTypeHint` default to false
    Regular expressions providing strong type hints turn out to be rather annoying
    when mixing cucumber expressions and regular expressions. Especially when the
    patterns involved are short and common.
    
    See: #1914, cucumber/cucumber#918
    
    Closes: #1915

--- java/src/main/java/io/cucumber/java/ParameterType.java
+++ java/src/main/java/io/cucumber/java/ParameterType.java
@@ -79,16 +79,12 @@ public @interface ParameterType {
     /**
      * Indicates whether or not this parameter provides a strong type hint when considering a
      * regular expression match. If so, the type hint provided by the method arguments will be
-     * ignored. If not, when both type hints are in agreement, this parameter types transformer
+     * ignored. If not, when both type hints are in agreement, this parameter type's transformer
      * will be used. Otherwise parameter transformation for a regular expression match will be
      * handled by {@link DefaultParameterTransformer}.
-     *
-     * Note: This value currently defaults to true but will default to false in
-     * the next major release.
      *
      * @return true if this parameter type provides a type hint when considering a regular
      * expression match
      */
-    @API(status = API.Status.EXPERIMENTAL)
-    boolean useRegexpMatchAsStrongTypeHint() default true;
+    boolean useRegexpMatchAsStrongTypeHint() default false;
 }
