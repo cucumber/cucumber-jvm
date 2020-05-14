1b7b1182cfcdd9ff9e320157ddd700c896d2a1ee
M.P. Korstanje <rien.korstanje@gmail.com>
2020-05-14 14:20:33 +0200

    [Core] Execute features files without pickles
    A feature without pickles is a valid feature file and should not throw
    any exceptions when executed.
    
    An empty feature file is also a valid feature file but we should throw
    an exception when explicitly asked to execute it. It was probably a
    mistake.

--- gherkin-messages/src/test/java/io/cucumber/core/gherkin/messages/FeatureParserTest.java
+++ gherkin-messages/src/test/java/io/cucumber/core/gherkin/messages/FeatureParserTest.java
@@ -23,18 +23,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FeatureParserTest {
 
     private final GherkinMessagesFeatureParser parser = new GherkinMessagesFeatureParser();
 
     @Test
-    void feature_file_without_pickles_is_parsed_but_produces_no_feature() throws IOException {
+    void feature_file_without_pickles_is_parsed_produces_empty_feature() throws IOException {
         URI uri = URI.create("classpath:com/example.feature");
         String source = new String(
             readAllBytes(Paths.get("src/test/resources/io/cucumber/core/gherkin/messages/no-pickles.feature")));
         Optional<Feature> feature = parser.parse(uri, source, UUID::randomUUID);
-        assertFalse(feature.isPresent());
+        assertTrue(feature.isPresent());
+        assertEquals(0, feature.get().getPickles().size());
     }
 
     @Test
---
+++ testng/src/test/resources/io/cucumber/testng/empty-feature.feature
@@ -0,0 +1,2 @@
+Feature: A feature without any scenarios
+
