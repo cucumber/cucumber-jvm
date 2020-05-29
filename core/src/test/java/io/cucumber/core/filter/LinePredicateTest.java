cucumber/cucumber-jvm

commit cee8eb2435c57f2c926ed2425dfcfe0853f4772a

[Core] Extract Gherkin compatibility layer (#1804)
Gherkin 6 introduced the `Rule` keyword and a new AST structure. This poses
several problems.

1. Cucumber-JVM is closely tied to the Pickle structure of Gherkin 5.
2. The HTML and JSON formatters use the Gherkin 5 parser.
3. The JSON formatter is the defacto output standard for third party tools.
4. There is no schema for the JSON formatters output.

To phase out the JSON formatter we'll need an alternative. This alternative
is the `message` formatter. This plugin will write the output of Cucumbers
execution to protobuf or ndjson file using the schema defined in
`cucumber-messages`.

Because `cucumber-messages` for Gherkin can only be generated by Gherkin 8
we need a way to run both Gherkin 5 and Gherkin 8 next to each other. By
extracting a compatibility layer we can use both Gherkin 5 and Gherkin 8.

core/src/main/java/io/cucumber/core/filter/LinePredicate.java
@@ -1,13 +1,13 @@
 package io.cucumber.core.filter;
 
-import io.cucumber.core.feature.CucumberPickle;
+import io.cucumber.core.gherkin.Pickle;
 
 import java.net.URI;
 import java.util.Collection;
 import java.util.Map;
 import java.util.function.Predicate;
 
-final class LinePredicate implements Predicate<CucumberPickle> {
+final class LinePredicate implements Predicate<Pickle> {
     private final Map<URI, ? extends Collection<Integer>> lineFilters;
 
     LinePredicate(Map<URI, ? extends Collection<Integer>> lineFilters) {
@@ -15,13 +15,13 @@ final class LinePredicate implements Predicate<CucumberPickle> {
     }
 
     @Override
-    public boolean test(CucumberPickle pickle) {
+    public boolean test(Pickle pickle) {
         URI picklePath = pickle.getUri();
         if (!lineFilters.containsKey(picklePath)) {
             return true;
         }
         for (Integer line : lineFilters.get(picklePath)) {
-            if (line == pickle.getLocation().getLine() || line == pickle.getScenarioLocation().getLine()) {
+            if (line == pickle.getLocation().getLine() || line == pickle.getLocation().getLine()) {
                 return true;
             }
         }

core/src/test/java/io/cucumber/core/filter/LinePredicateTest.java
@@ -1,7 +1,7 @@
 package io.cucumber.core.filter;
 
-import io.cucumber.core.feature.CucumberFeature;
-import io.cucumber.core.feature.CucumberPickle;
+import io.cucumber.core.gherkin.Feature;
+import io.cucumber.core.gherkin.Pickle;
 import io.cucumber.core.feature.TestFeatureParser;
 import org.junit.jupiter.api.Test;
 
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LinePredicateTest {
 
-    private final CucumberFeature feature = TestFeatureParser.parse(
+    private final Feature feature = TestFeatureParser.parse(
         "file:path/file.feature",
         "" +
             "Feature: Test feature\n" +
@@ -27,7 +27,7 @@ class LinePredicateTest {
             "       | cucumber | \n" +
             "       | gherkin  | \n"
     );
-    private final CucumberPickle pickle = feature.getPickles().get(0);
+    private final Pickle pickle = feature.getPickles().get(0);
 
     @Test
     void matches_pickles_from_files_not_in_the_predicate_map() {

commit 642fa9226d593e23b10ca8cbbd6021f941181150

[Core] Fix line filter for scenario outlines
Broken by #1804. While covered by unit tests, the unit tests were broken by
a change in the way feature identifiers were parsed. When parsing a feature
path relative paths are expanded to full file uris. This resulted in the uri
of the pickle not matching the uri provided by the test.

Fixes: #1980

core/src/main/java/io/cucumber/core/filter/LinePredicate.java
@@ -5,6 +5,7 @@ import io.cucumber.core.gherkin.Pickle;
 import java.net.URI;
 import java.util.Collection;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 final class LinePredicate implements Predicate<Pickle> {
@@ -22,7 +23,8 @@ final class LinePredicate implements Predicate<Pickle> {
             return true;
         }
         for (Integer line : lineFilters.get(picklePath)) {
-            if (line == pickle.getLocation().getLine() || line == pickle.getLocation().getLine()) {
+            if (Objects.equals(line, pickle.getLocation().getLine())
+                    || Objects.equals(line, pickle.getScenarioLocation().getLine())) {
                 return true;
             }
         }

gherkin-messages/src/main/java/io/cucumber/core/gherkin/messages/GherkinMessagesPickle.java
@@ -5,6 +5,7 @@ import io.cucumber.core.gherkin.Step;
 import io.cucumber.core.gherkin.StepType;
 import io.cucumber.gherkin.GherkinDialect;
 import io.cucumber.messages.Messages;
+import io.cucumber.messages.Messages.GherkinDocument.Feature.Scenario;
 import io.cucumber.messages.Messages.Pickle.PickleStep;
 import io.cucumber.messages.Messages.Pickle.PickleTag;
 import io.cucumber.plugin.event.Location;
@@ -84,7 +85,9 @@ final class GherkinMessagesPickle implements Pickle {
 
     @Override
     public Location getScenarioLocation() {
-        Messages.Location location = cucumberQuery.getGherkinScenario(pickle.getAstNodeIds(0)).getLocation();
+        String sourceId = pickle.getAstNodeIds(0);
+        Scenario scenario = cucumberQuery.getGherkinScenario(sourceId);
+        Messages.Location location = scenario.getLocation();
         return GherkinMessagesLocation.from(location);
     }
 

core/src/test/java/io/cucumber/core/filter/LinePredicateTest.java
@@ -5,10 +5,10 @@ import io.cucumber.core.gherkin.Feature;
 import io.cucumber.core.gherkin.Pickle;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.net.URI;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static java.util.Collections.singletonMap;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -16,8 +16,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LinePredicateTest {
 
+    public static final URI featurePath = URI.create("classpath:path/file.feature");
     private final Feature feature = TestFeatureParser.parse(
-        "file:path/file.feature",
+        featurePath,
         "" +
                 "Feature: Test feature\n" +
                 "  Scenario Outline: Test scenario\n" +
@@ -34,15 +35,23 @@ class LinePredicateTest {
         // results in only line predicates only for another_path/file.feature,
         // but all pickles from path/file.feature shall also be executed.
         LinePredicate predicate = new LinePredicate(singletonMap(
-            URI.create("file:another_path/file.feature"),
+            URI.create("classpath:another_path/file.feature"),
             singletonList(8)));
         assertTrue(predicate.test(pickle));
     }
 
     @Test
+    void does_not_matches_pickles_for_no_lines_in_predicate() {
+        LinePredicate predicate = new LinePredicate(singletonMap(
+            featurePath,
+            emptyList()));
+        assertFalse(predicate.test(pickle));
+    }
+
+    @Test
     void matches_pickles_for_any_line_in_predicate() {
         LinePredicate predicate = new LinePredicate(singletonMap(
-            URI.create("file:path/file.feature"),
+            featurePath,
             asList(2, 4)));
         assertTrue(predicate.test(pickle));
     }
@@ -50,7 +59,7 @@ class LinePredicateTest {
     @Test
     void matches_pickles_on_scenario_location_of_the_pickle() {
         LinePredicate predicate = new LinePredicate(singletonMap(
-            URI.create("file:path/file.feature"),
+            featurePath,
             singletonList(2)));
         assertTrue(predicate.test(pickle));
     }
@@ -58,7 +67,7 @@ class LinePredicateTest {
     @Test
     void matches_pickles_on_example_location_of_the_pickle() {
         LinePredicate predicate = new LinePredicate(singletonMap(
-            URI.create("file:path/file.feature"),
+            featurePath,
             singletonList(6)));
         assertTrue(predicate.test(pickle));
     }
@@ -66,7 +75,7 @@ class LinePredicateTest {
     @Test
     void does_not_matches_pickles_not_on_any_line_of_the_predicate() {
         LinePredicate predicate = new LinePredicate(singletonMap(
-            new File("path/file.feature").toURI(),
+            featurePath,
             singletonList(4)));
         assertFalse(predicate.test(pickle));
     }