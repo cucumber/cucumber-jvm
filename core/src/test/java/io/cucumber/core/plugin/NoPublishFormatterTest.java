e75318c9c955260c7012d33ab69bb57f31236243
Aslak Hellesøy <aslak.hellesoy@smartbear.com>
2020-08-14 15:37:41 +0100

    Remove 'plugin' from publish-related env vars/system props.

--- core/src/test/java/io/cucumber/core/plugin/NoPublishFormatterTest.java
+++ core/src/test/java/io/cucumber/core/plugin/NoPublishFormatterTest.java
@@ -22,12 +22,12 @@ class NoPublishFormatterTest {
                 "│ Share your Cucumber Report with your team at https://reports.cucumber.io      │\n" +
                 "│                                                                               │\n" +
                 "│ Code:                   @CucumberOptions(publish = true)                      │\n" +
-                "│ Environment variable:   CUCUMBER_PLUGIN_PUBLISH_ENABLED=true                  │\n" +
-                "│ System property:        -Dcucumber.plugin.publish.enabled=true                │\n" +
+                "│ Environment variable:   CUCUMBER_PUBLISH_ENABLED=true                         │\n" +
+                "│ System property:        -Dcucumber.publish.enabled=true                       │\n" +
                 "│                                                                               │\n" +
                 "│ More information at https://reports.cucumber.io/docs/cucumber-jvm             │\n" +
                 "│                                                                               │\n" +
-                "│ To disable this message, add cucumber.plugin.publish.quiet=true to            │\n" +
+                "│ To disable this message, add cucumber.publish.quiet=true to                   │\n" +
                 "│ src/test/resources/cucumber.properties or                                     │\n" +
                 "│ src/test/resources/junit-platform.properties (cucumber-junit-platform-engine) │\n" +
                 "└───────────────────────────────────────────────────────────────────────────────┘\n"));
