6838ed68d81558654cebf00f173e64348c85bf57
M.P. Korstanje <rien.korstanje@gmail.com>
2021-02-26 18:14:37 +0100

    Split and refactor tests

--- cdi2/src/test/java/io/cucumber/cdi2/Cdi2FactoryTest.java
+++ cdi2/src/test/java/io/cucumber/cdi2/Cdi2FactoryTest.java
@@ -1,6 +1,7 @@
 package io.cucumber.cdi2;
 
 import io.cucumber.core.backend.ObjectFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -9,35 +10,53 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 class Cdi2FactoryTest {
 
-    @Test
-    void shouldGiveUsNewInstancesForEachScenario() {
+    final ObjectFactory factory = new Cdi2Factory();
 
-        final ObjectFactory factory = new Cdi2Factory();
+    @Test
+    void shouldCreateNewInstancesForEachScenario() {
         factory.addClass(BellyStepDefinitions.class);
-        factory.addClass(CdiBellyStepDefinitions.class);
 
         // Scenario 1
         factory.start();
-        final BellyStepDefinitions o1 = factory.getInstance(BellyStepDefinitions.class);
-        final CdiBellyStepDefinitions cdiStep = factory.getInstance(CdiBellyStepDefinitions.class);
-        assertAll(
-            // assert that it is is a CDI proxy
-            () -> assertThat(cdiStep.getClass(), not(is(CdiBellyStepDefinitions.class))),
-            () -> assertThat(cdiStep.getClass().getSuperclass(), is(CdiBellyStepDefinitions.class)));
+        BellyStepDefinitions a1 = factory.getInstance(BellyStepDefinitions.class);
+        BellyStepDefinitions a2 = factory.getInstance(BellyStepDefinitions.class);
+         assertThat(a1, is(equalTo(a2)));
         factory.stop();
 
         // Scenario 2
         factory.start();
-        final BellyStepDefinitions o2 = factory.getInstance(BellyStepDefinitions.class);
+        BellyStepDefinitions b1 = factory.getInstance(BellyStepDefinitions.class);
         factory.stop();
 
         assertAll(
-            () -> assertThat(o1, is(notNullValue())),
-            () -> assertThat(o1, is(not(equalTo(o2)))),
-            () -> assertThat(o2, is(not(equalTo(o1)))));
+            () -> assertThat(a1, is(notNullValue())),
+            () -> assertThat(a1, is(not(equalTo(b1)))),
+            () -> assertThat(b1, is(not(equalTo(a1)))));
+    }
+
+    @Test
+    void shouldCreateApplicationScopedInstance() {
+        factory.addClass(CdiBellyStepDefinitions.class);
+        factory.start();
+        CdiBellyStepDefinitions cdiStep = factory.getInstance(CdiBellyStepDefinitions.class);
+        assertAll(
+                // assert that it is is a CDI proxy
+                () -> assertThat(cdiStep.getClass(), not(is(CdiBellyStepDefinitions.class))),
+                () -> assertThat(cdiStep.getClass().getSuperclass(), is(CdiBellyStepDefinitions.class)));
+        factory.stop();
+    }
+
+    @Test
+    void shouldCreateUnmanagedInstance() {
+        factory.addClass(UnmanagedBellyStepDefinitions.class);
+        factory.start();
+        assertNotNull(factory.getInstance(UnmanagedBellyStepDefinitions.class));
+        factory.stop();
     }
 
 }
