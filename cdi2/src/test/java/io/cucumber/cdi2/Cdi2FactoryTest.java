5aed18f40eb62492339da31b8063aee52ea0d1e9
Daniel Beland <daniel.beland@ca.thalesgroup.com>
2021-03-07 09:49:46 -0500

    revert processing of injection points

--- cdi2/src/test/java/io/cucumber/cdi2/Cdi2FactoryTest.java
+++ cdi2/src/test/java/io/cucumber/cdi2/Cdi2FactoryTest.java
@@ -10,10 +10,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Vetoed;
 import javax.inject.Inject;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -127,44 +123,4 @@ class Cdi2FactoryTest {
         factory.stop();
     }
 
-    static class SubParameterizedBean<K, V> {
-
-    }
-
-    static class ParameterizedBean<K, V> {
-
-        @Inject
-        SubParameterizedBean<K, V> injected;
-
-    }
-
-    static class ParameterizedStepDefinitions {
-
-        @Inject
-        ParameterizedBean<Map<String, List<String>>, String> injectedParams1;
-
-        @Inject
-        ParameterizedBean<Set<Boolean>, String> injectedParams2;
-
-        @Inject
-        @SuppressWarnings("rawtypes")
-        ParameterizedBean injectedRaw;
-
-    }
-
-    @ParameterizedTest
-    @ValueSource(booleans = { true, false })
-    void canInjectParameterizedBeans(boolean ignoreLocalBeansXml) {
-        IgnoreLocalBeansXmlClassLoader.setClassLoader(ignoreLocalBeansXml);
-        factory.addClass(ParameterizedStepDefinitions.class);
-        factory.start();
-        ParameterizedStepDefinitions stepDefinitions = factory.getInstance(ParameterizedStepDefinitions.class);
-        assertThat(stepDefinitions.injectedParams1, is(notNullValue()));
-        assertThat(stepDefinitions.injectedParams1.injected, is(notNullValue()));
-        assertThat(stepDefinitions.injectedParams2, is(notNullValue()));
-        assertThat(stepDefinitions.injectedParams2.injected, is(notNullValue()));
-        assertThat(stepDefinitions.injectedRaw, is(notNullValue()));
-        assertThat(stepDefinitions.injectedRaw.injected, is(notNullValue()));
-        factory.stop();
-    }
 }
--- jakarta-cdi/src/test/java/io/cucumber/jakarta/cdi/example/RunCucumberTest.java
+++ jakarta-cdi/src/test/java/io/cucumber/jakarta/cdi/example/RunCucumberTest.java
@@ -1,32 +1,8 @@
 package io.cucumber.jakarta.cdi.example;
 
-import io.cucumber.jakarta.cdi.IgnoreLocalBeansXmlClassLoader;
 import io.cucumber.junit.platform.engine.Cucumber;
-import io.cucumber.plugin.ConcurrentEventListener;
-import io.cucumber.plugin.event.EventPublisher;
-import io.cucumber.plugin.event.TestRunFinished;
-import io.cucumber.plugin.event.TestSourceRead;
 
 @Cucumber
-public class RunCucumberTest implements ConcurrentEventListener {
-
-    private static final String WITHOUT_LOCAL_BEANS_XML = "withoutLocalBeansXml";
-
-    @Override
-    public void setEventPublisher(EventPublisher eventPublisher) {
-
-        eventPublisher.registerHandlerFor(TestSourceRead.class, event -> {
-            if (event.getUri().toString().contains(WITHOUT_LOCAL_BEANS_XML)) {
-                IgnoreLocalBeansXmlClassLoader.setClassLoader(true);
-            } else {
-                IgnoreLocalBeansXmlClassLoader.restoreClassLoader();
-            }
-        });
-
-        eventPublisher.registerHandlerFor(TestRunFinished.class, event -> {
-            IgnoreLocalBeansXmlClassLoader.restoreClassLoader();
-        });
-
-    }
+public class RunCucumberTest {
 
 }
