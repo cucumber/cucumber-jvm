a0913724851edf832bb7aee9d2e3ffd500894b7c
M.P. Korstanje <rien.korstanje@gmail.com>
2021-03-26 17:42:06 +0100

    Reduce logspam

--- core/src/test/java/io/cucumber/core/resource/ClasspathScannerTest.java
+++ core/src/test/java/io/cucumber/core/resource/ClasspathScannerTest.java
@@ -56,7 +56,8 @@ class ClasspathScannerTest {
 
     @Test
     void scanForSubClassesInPackage() {
-        List<Class<? extends ExampleInterface>> classes = scanner.scanForSubClassesInPackage("io.cucumber.core.resource.test",
+        List<Class<? extends ExampleInterface>> classes = scanner.scanForSubClassesInPackage(
+            "io.cucumber.core.resource.test",
             ExampleInterface.class);
 
         assertThat(classes, contains(ExampleClass.class));
