ade49c12a582d8a1115c5e2c1fae16d68a208e43
David Goss <david@davidgoss.co>
2021-06-16 12:53:45 +0100

    HtmlFormatter

--- core/src/main/java/io/cucumber/core/plugin/HtmlFormatter.java
+++ core/src/main/java/io/cucumber/core/plugin/HtmlFormatter.java
@@ -25,7 +25,7 @@ public final class HtmlFormatter implements ConcurrentEventListener {
     private void write(Envelope event) {
         // Workaround to reduce the size of the report
         // See: https://github.com/cucumber/cucumber/issues/1232
-        if (event.hasStepDefinition() || event.hasHook() || event.hasParameterType()) {
+        if (event.getStepDefinition() != null || event.getHook() != null || event.getParameterType() != null) {
             return;
         }
 
@@ -37,7 +37,7 @@ public final class HtmlFormatter implements ConcurrentEventListener {
 
         // TODO: Plugins should implement the closable interface
         // and be closed by Cucumber
-        if (event.hasTestRunFinished()) {
+        if (event.getTestRunFinished() != null) {
             try {
                 writer.close();
             } catch (IOException e) {
