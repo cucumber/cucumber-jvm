7ab1552d0b3ba9a35fb5156f9dc7913991588ab5
M.P. Korstanje <rien.korstanje@gmail.com>
2021-07-23 20:08:13 +0200

    Fix TimelineFormatter

--- core/src/main/java/io/cucumber/core/plugin/TimelineFormatter.java
+++ core/src/main/java/io/cucumber/core/plugin/TimelineFormatter.java
@@ -1,7 +1,10 @@
 package io.cucumber.core.plugin;
 
 import io.cucumber.core.exception.CucumberException;
-import io.cucumber.messages.JSON;
+import io.cucumber.messages.internal.com.fasterxml.jackson.annotation.JsonInclude.Include;
+import io.cucumber.messages.internal.com.fasterxml.jackson.core.JsonGenerator.Feature;
+import io.cucumber.messages.internal.com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.messages.internal.com.fasterxml.jackson.databind.SerializationFeature;
 import io.cucumber.plugin.ConcurrentEventListener;
 import io.cucumber.plugin.event.EventPublisher;
 import io.cucumber.plugin.event.Location;
@@ -47,17 +50,23 @@ public final class TimelineFormatter implements ConcurrentEventListener {
     };
 
     private final Map<String, TestData> allTests = new HashMap<>();
-    private final Map<Long, GroupData> allGroups = new HashMap<>();
+    private final Map<Long, GroupData> threadGroups = new HashMap<>();
+
     private final File reportDir;
     private final UTF8OutputStreamWriter reportJs;
     private final Map<URI, Collection<Node>> parsedTestSources = new HashMap<>();
 
+    private final ObjectMapper objectMapper = new ObjectMapper()
+            .setSerializationInclusion(Include.NON_NULL)
+            .enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING)
+            .disable(Feature.AUTO_CLOSE_TARGET);
+
     @SuppressWarnings("unused") // Used by PluginFactory
-    public TimelineFormatter(final File reportDir) throws FileNotFoundException {
+    public TimelineFormatter(File reportDir) throws FileNotFoundException {
         reportDir.mkdirs();
         if (!reportDir.isDirectory()) {
             throw new CucumberException(String.format("The %s needs an existing directory. Not a directory: %s",
-                getClass().getName(), reportDir.getAbsolutePath()));
+                    getClass().getName(), reportDir.getAbsolutePath()));
         }
 
         this.reportDir = reportDir;
@@ -65,7 +74,7 @@ public final class TimelineFormatter implements ConcurrentEventListener {
     }
 
     @Override
-    public void setEventPublisher(final EventPublisher publisher) {
+    public void setEventPublisher(EventPublisher publisher) {
         publisher.registerHandlerFor(TestSourceParsed.class, this::handleTestSourceParsed);
         publisher.registerHandlerFor(TestCaseStarted.class, this::handleTestCaseStarted);
         publisher.registerHandlerFor(TestCaseFinished.class, this::handleTestCaseFinished);
@@ -76,22 +85,56 @@ public final class TimelineFormatter implements ConcurrentEventListener {
         parsedTestSources.put(event.getUri(), event.getNodes());
     }
 
-    private void handleTestCaseStarted(final TestCaseStarted event) {
-        Thread currentThread = Thread.currentThread();
-        final Long threadId = currentThread.getId();
-        final TestData test = new TestData(event, threadId);
-        allTests.put(getId(event), test);
-        if (!allGroups.containsKey(threadId)) {
-            allGroups.put(threadId, new GroupData(currentThread));
+    private void handleTestCaseStarted(TestCaseStarted event) {
+        Thread thread = Thread.currentThread();
+        threadGroups.computeIfAbsent(thread.getId(), threadId -> {
+            GroupData group = new GroupData();
+            group.setContent(thread.toString());
+            group.setId(threadId);
+            return group;
+        });
+
+        TestCase testCase = event.getTestCase();
+        TestData data = new TestData();
+        data.setId(getId(event));
+        data.setFeature(findRootNodeName(testCase));
+        data.setScenario(testCase.getName());
+        data.setStart(event.getInstant().toEpochMilli());
+        data.setTags(buildTagsValue(testCase));
+        data.setGroup(thread.getId());
+        allTests.put(data.getId(), data);
+    }
+
+    private String buildTagsValue(TestCase testCase) {
+        StringBuilder tags = new StringBuilder();
+        for (String tag : testCase.getTags()) {
+            tags.append(tag.toLowerCase()).append(",");
         }
+        return tags.toString();
     }
 
-    private void handleTestCaseFinished(final TestCaseFinished event) {
-        final String id = getId(event);
-        allTests.get(id).end(event);
+    private void handleTestCaseFinished(TestCaseFinished event) {
+        TestData data = allTests.get(getId(event));
+        data.setEnd(event.getInstant().toEpochMilli());
+        data.setClassName(event.getResult().getStatus().name().toLowerCase(ROOT));
     }
 
-    private void finishReport(final TestRunFinished event) {
+    private String findRootNodeName(TestCase testCase) {
+        Location location = testCase.getLocation();
+        Predicate<Node> withLocation = candidate -> candidate.getLocation().equals(location);
+        return parsedTestSources.get(testCase.getUri())
+                .stream()
+                .map(node -> node.findPathTo(withLocation))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst()
+                .map(nodes -> nodes.get(0))
+                .flatMap(Node::getName)
+                .orElse("Unknown");
+    }
+
+    private void finishReport(TestRunFinished event) {
+
         try {
             reportJs.append("$(document).ready(function() {");
             reportJs.append("\n");
@@ -99,7 +142,7 @@ public final class TimelineFormatter implements ConcurrentEventListener {
             reportJs.append("\n");
             // Need to sort groups by id, so can guarantee output of order in
             // rendered timeline
-            appendAsJsonToJs(reportJs, "timelineGroups", new TreeMap<>(allGroups).values());
+            appendAsJsonToJs(reportJs, "timelineGroups", new TreeMap<>(threadGroups).values());
             reportJs.append("\n");
             reportJs.append("});");
             reportJs.close();
@@ -109,15 +152,15 @@ public final class TimelineFormatter implements ConcurrentEventListener {
         }
     }
 
-    private String getId(final TestCaseEvent testCaseEvent) {
+    private static String getId(TestCaseEvent testCaseEvent) {
         return testCaseEvent.getTestCase().getId().toString();
     }
 
     private void appendAsJsonToJs(
-            final UTF8OutputStreamWriter out, final String pushTo, final Collection<?> content
+            UTF8OutputStreamWriter out, String pushTo, Collection<?> content
     ) throws IOException {
         out.append("CucumberHTML.").append(pushTo).append(".pushArray(");
-        JSON.writeValue(out, content);
+        objectMapper.writeValue(out, content);
         out.append(");");
     }
 
@@ -125,19 +168,19 @@ public final class TimelineFormatter implements ConcurrentEventListener {
         if (reportDir == null) {
             return;
         }
-        final File outputDir = new File(reportDir.getPath());
+        File outputDir = new File(reportDir.getPath());
         for (String textAsset : TEXT_ASSETS) {
-            final InputStream textAssetStream = getClass().getResourceAsStream(textAsset);
+            InputStream textAssetStream = getClass().getResourceAsStream(textAsset);
             if (textAssetStream == null) {
                 throw new CucumberException("Couldn't find " + textAsset);
             }
-            final String fileName = new File(textAsset).getName();
+            String fileName = new File(textAsset).getName();
             copyFile(textAssetStream, new File(outputDir, fileName));
             closeQuietly(textAssetStream);
         }
     }
 
-    private static void copyFile(final InputStream source, final File dest) throws CucumberException {
+    private static void copyFile(InputStream source, File dest) throws CucumberException {
         OutputStream os = null;
         try {
             os = new FileOutputStream(dest);
@@ -165,64 +208,111 @@ public final class TimelineFormatter implements ConcurrentEventListener {
 
     static class GroupData {
 
-        final long id;
-        final String content;
+        private long id;
+        private String content;
 
-        GroupData(Thread thread) {
-            id = thread.getId();
-            content = thread.toString();
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public String getContent() {
+            return content;
         }
 
     }
 
-    class TestData {
+    static class TestData {
 
-        final String id;
-        final String feature;
-        final String scenario;
-        final long startTime;
-        final long group;
-        final String content = ""; // Replaced in JS file
-        final String tags;
-        long end;
-        String className;
+        private String id;
+        private String feature;
+        private String scenario;
+        private long start;
+        private long group;
+        private String content = ""; // Replaced in JS file
+        private String tags;
+        private long end;
+        private String className;
 
-        TestData(final TestCaseStarted started, final Long group) {
-            this.id = TimelineFormatter.this.getId(started);
-            final TestCase testCase = started.getTestCase();
-            this.feature = findRootNodeName(testCase);
-            this.scenario = testCase.getName();
-            this.startTime = started.getInstant().toEpochMilli();
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public void setFeature(String feature) {
+            this.feature = feature;
+        }
+
+        public void setScenario(String scenario) {
+            this.scenario = scenario;
+        }
+
+        public void setStart(long start) {
+            this.start = start;
+        }
+
+        public void setGroup(long group) {
             this.group = group;
-            this.tags = buildTagsValue(testCase);
         }
 
-        private String findRootNodeName(TestCase testCase) {
-            Location location = testCase.getLocation();
-            Predicate<Node> withLocation = candidate -> candidate.getLocation().equals(location);
-            return parsedTestSources.get(testCase.getUri())
-                    .stream()
-                    .map(node -> node.findPathTo(withLocation))
-                    .filter(Optional::isPresent)
-                    .map(Optional::get)
-                    .findFirst()
-                    .map(nodes -> nodes.get(0))
-                    .flatMap(Node::getName)
-                    .orElse("Unknown");
+        public void setContent(String content) {
+            this.content = content;
         }
 
-        private String buildTagsValue(final TestCase testCase) {
-            final StringBuilder tags = new StringBuilder();
-            for (final String tag : testCase.getTags()) {
-                tags.append(tag.toLowerCase()).append(",");
-            }
-            return tags.toString();
+        public void setTags(String tags) {
+            this.tags = tags;
         }
 
-        void end(final TestCaseFinished event) {
-            this.end = event.getInstant().toEpochMilli();
-            this.className = event.getResult().getStatus().name().toLowerCase(ROOT);
+        public void setEnd(long end) {
+            this.end = end;
         }
+
+        public void setClassName(String className) {
+            this.className = className;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getFeature() {
+            return feature;
+        }
+
+        public String getScenario() {
+            return scenario;
+        }
+
+        public long getStart() {
+            return start;
+        }
+
+        public long getGroup() {
+            return group;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public String getTags() {
+            return tags;
+        }
+
+        public long getEnd() {
+            return end;
+        }
+
+        public String getClassName() {
+            return className;
+        }
+
     }
 
 }
