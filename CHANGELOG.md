633d26139c4b3c1662a722f3bd54da50f4f9c4a4
M.P. Korstanje <mpkorstanje@users.noreply.github.com>
2021-08-10 23:20:15 +0200

    [Core] Synchronize event bus before use (#2358)
    We can not exclude the possibility that an event is published to the event bus
    after test execution has completed. For example because the test framework may
    have decided that a test case has timed out and is now terminating test
    execution.
    
    As such events from the pending testcase on the synchronized bus may race with
    the events from the unsynchronized bus. This in turn may break the `LinkedList`
    in the `CanonicalOrderEventPublisher`.
    
    Fixes: #2288 (mabye??)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,6 +16,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 ### Removed
 
 ### Fixed
+* [Core] Synchronize event bus before use ([#2358](https://github.com/cucumber/cucumber-jvm/pull/2358)) M.P. Korstanje) 
 
 ## [6.11.0] (2021-08-05)
 
--- junit/src/main/java/io/cucumber/junit/Cucumber.java
+++ junit/src/main/java/io/cucumber/junit/Cucumber.java
@@ -21,6 +21,7 @@ import io.cucumber.core.runtime.FeaturePathFeatureSupplier;
 import io.cucumber.core.runtime.ObjectFactoryServiceLoader;
 import io.cucumber.core.runtime.ObjectFactorySupplier;
 import io.cucumber.core.runtime.ScanningTypeRegistryConfigurerSupplier;
+import io.cucumber.core.runtime.SynchronizedEventBus;
 import io.cucumber.core.runtime.ThreadLocalObjectFactorySupplier;
 import io.cucumber.core.runtime.ThreadLocalRunnerSupplier;
 import io.cucumber.core.runtime.TimeServiceEventBus;
@@ -44,6 +45,7 @@ import java.util.UUID;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
+import static io.cucumber.core.runtime.SynchronizedEventBus.synchronize;
 import static io.cucumber.junit.FileNameCompatibleNames.uniqueSuffix;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
@@ -148,7 +150,7 @@ public final class Cucumber extends ParentRunner<ParentRunner<?>> {
                 .parse(CucumberProperties.fromSystemProperties())
                 .build(junitEnvironmentOptions);
 
-        this.bus = new TimeServiceEventBus(Clock.systemUTC(), UUID::randomUUID);
+        this.bus = synchronize(new TimeServiceEventBus(Clock.systemUTC(), UUID::randomUUID));
 
         // Parse the features early. Don't proceed when there are lexer errors
         FeatureParser parser = new FeatureParser(bus::generateId);
