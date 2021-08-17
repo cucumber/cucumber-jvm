d41e9d1a90dfd9bfc24b48ac3910c8ca891652cd
M.P. Korstanje <rien.korstanje@gmail.com>
2021-08-17 19:50:04 +0200

    [BOM] Add Bill of Materials

--- examples/calculator-java-junit4/src/test/java/io/cucumber/examples/calculator/RpnCalculatorSteps.java
+++ examples/calculator-java-junit4/src/test/java/io/cucumber/examples/calculator/RpnCalculatorSteps.java
@@ -11,7 +11,7 @@ import io.cucumber.java.en.When;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class RpnCalculatorSteps {
 
