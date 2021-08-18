564fb8e6c038171f2b8e30e40947054e907c092d
M.P. Korstanje <rien.korstanje@gmail.com>
2021-08-18 13:36:16 +0200

    Use JUnit Platform in archetype

--- archetype/src/main/resources/archetype-resources/src/test/java/RunCucumberTest.java
+++ archetype/src/main/resources/archetype-resources/src/test/java/RunCucumberTest.java
@@ -1,11 +1,15 @@
 package ${package};
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("${packageInPathFormat}")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
-
 }
