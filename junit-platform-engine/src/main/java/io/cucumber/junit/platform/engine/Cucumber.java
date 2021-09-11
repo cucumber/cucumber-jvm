f0d09c00008ae9b057f2d423b7874b7b4e4d0372
M.P. Korstanje <rien.korstanje@gmail.com>
2021-09-11 20:35:13 +0200

    [JUnit Platform] Fix javadoc formatting

--- junit-platform-engine/src/main/java/io/cucumber/junit/platform/engine/Cucumber.java
+++ junit-platform-engine/src/main/java/io/cucumber/junit/platform/engine/Cucumber.java
@@ -22,22 +22,27 @@ import java.lang.annotation.Target;
  * recommended way for IDEs and other tooling use the selectors implemented by
  * Cucumber to discover feature files.
  * <p>
- * DEPRECATED: Please use the JUnit Platform Suite to run Cucumber in
- * combination with Surefire or Gradle. E.g: <code><pre>{@code
- *  package com.example;
+ * 
+ * @deprecated Please use the JUnit Platform Suite to run Cucumber in
+ *             combination with Surefire or Gradle. E.g: <code><pre>{@code
+ *package com.example;
  *
- *  import org.junit.platform.suite.api.ConfigurationParameter;
- *  import org.junit.platform.suite.api.SelectClasspathResource;
- *  import org.junit.platform.suite.api.Suite;
+ *import org.junit.platform.suite.api.ConfigurationParameter;
+ *import org.junit.platform.suite.api.SelectClasspathResource;
+ *import org.junit.platform.suite.api.Suite;
  *
- * import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
- *  &#64;Suite
- *  &#64;SelectClasspathResource("com/example")
- *  &#64;ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example")
- *  public class RunCucumberTest {
- *  }
- * }</pre></code> @see CucumberTestEngine @deprecated Please use the JUnit
- * Platform Suite.
+ *import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+ *
+ *&#64;Suite
+ *&#64;SelectClasspathResource("com/example")
+ *&#64;ConfigurationParameter(
+ *   key = GLUE_PROPERTY_NAME,
+ *   value = "com.example"
+ *)
+ *public class RunCucumberTest {
+ *}
+ *}</pre></code>
+ * @see        CucumberTestEngine
  */
 @API(status = Status.DEPRECATED)
 @Retention(RetentionPolicy.RUNTIME)
