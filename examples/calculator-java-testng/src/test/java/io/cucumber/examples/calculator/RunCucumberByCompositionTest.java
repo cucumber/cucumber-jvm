f0c851e6c8ff68e9bdd20d22b112b0d95d1057eb
mpkorstanje <logarithmicwhale@gmail.com>
2021-10-12 12:54:50 +0200

    [Examples] Use properties from testng.xml

--- examples/calculator-java-testng/src/test/java/io/cucumber/examples/calculator/RunCucumberByCompositionTest.java
+++ examples/calculator-java-testng/src/test/java/io/cucumber/examples/calculator/RunCucumberByCompositionTest.java
@@ -1,13 +1,16 @@
 package io.cucumber.examples.calculator;
 
 import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.CucumberPropertiesProvider;
 import io.cucumber.testng.FeatureWrapper;
 import io.cucumber.testng.PickleWrapper;
 import io.cucumber.testng.TestNGCucumberRunner;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.xml.XmlTest;
 
 /**
  * An example of using TestNG when the test class does not inherit from
@@ -20,8 +23,10 @@ public class RunCucumberByCompositionTest extends RunCucumberByCompositionBase {
     private TestNGCucumberRunner testNGCucumberRunner;
 
     @BeforeClass(alwaysRun = true)
-    public void setUpClass() {
-        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    public void setUpClass(ITestContext context) {
+        XmlTest currentXmlTest = context.getCurrentXmlTest();
+        CucumberPropertiesProvider properties = currentXmlTest::getParameter;
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass(), properties);
     }
 
     @Test(groups = "cucumber", description = "Runs Cucumber Scenarios", dataProvider = "scenarios")
