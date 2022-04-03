21a8ea5a7b9bb8ff9cc6701fdddb7a0a5a40f068
M.P. Korstanje <rien.korstanje@gmail.com>
2022-04-03 18:01:51 +0200

    WIP Use null safe-messages

--- core/src/main/java/io/cucumber/core/runner/TestStepResultStatus.java
+++ core/src/main/java/io/cucumber/core/runner/TestStepResultStatus.java
@@ -2,20 +2,23 @@ package io.cucumber.core.runner;
 
 import io.cucumber.plugin.event.Status;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 class TestStepResultStatus {
 
-    private static final Map<Status, io.cucumber.messages.types.TestStepResultStatus> STATUS = new HashMap<>() {
-        {
-            put(Status.FAILED, io.cucumber.messages.types.TestStepResultStatus.FAILED);
-            put(Status.PASSED, io.cucumber.messages.types.TestStepResultStatus.PASSED);
-            put(Status.UNDEFINED, io.cucumber.messages.types.TestStepResultStatus.UNDEFINED);
-            put(Status.PENDING, io.cucumber.messages.types.TestStepResultStatus.PENDING);
-            put(Status.SKIPPED, io.cucumber.messages.types.TestStepResultStatus.SKIPPED);
-            put(Status.AMBIGUOUS, io.cucumber.messages.types.TestStepResultStatus.AMBIGUOUS);
-        }
+    private static final Map<Status, io.cucumber.messages.types.TestStepResultStatus> STATUS;
+
+    static {
+        Map<Status, io.cucumber.messages.types.TestStepResultStatus> status = new HashMap<>();
+        status.put(Status.FAILED, io.cucumber.messages.types.TestStepResultStatus.FAILED);
+        status.put(Status.PASSED, io.cucumber.messages.types.TestStepResultStatus.PASSED);
+        status.put(Status.UNDEFINED, io.cucumber.messages.types.TestStepResultStatus.UNDEFINED);
+        status.put(Status.PENDING, io.cucumber.messages.types.TestStepResultStatus.PENDING);
+        status.put(Status.SKIPPED, io.cucumber.messages.types.TestStepResultStatus.SKIPPED);
+        status.put(Status.AMBIGUOUS, io.cucumber.messages.types.TestStepResultStatus.AMBIGUOUS);
+        STATUS = Collections.unmodifiableMap(status);
     };
 
     private TestStepResultStatus() {
