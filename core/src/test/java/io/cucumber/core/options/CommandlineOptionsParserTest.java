20f1f9dbe4ff317acd77bf276e61f84521561f16
M.P. Korstanje <rien.korstanje@gmail.com>
2022-04-19 00:17:31 +0200

    Tests

--- core/src/test/java/io/cucumber/core/options/CommandlineOptionsParserTest.java
+++ core/src/test/java/io/cucumber/core/options/CommandlineOptionsParserTest.java
@@ -112,8 +112,18 @@ class CommandlineOptionsParserTest {
     @Test
     void prints_usage_for_help() {
         parser.parse("--help");
-
         assertThat(output(), startsWith("Usage"));
+    }
+    @Test
+    void prints_supported_languages() {
+        parser.parse("--i18n", "help");
+        assertThat(output(), startsWith("af              Afrikaans                 Afrikaans"));
+    }
+
+    @Test
+    void prints_supported_keywords() {
+        parser.parse("--i18n", "en");
+        assertThat(output(), startsWith("      | feature          | \"Feature\", \"Business Need\", \"Ability\"   |"));
     }
 
     @Test
