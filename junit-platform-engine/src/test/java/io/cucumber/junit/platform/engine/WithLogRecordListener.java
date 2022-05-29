2337b8f018ee72e4341d243414cfb3404b2a6c99
Dom Corvasce <dcorvasce@zohomail.eu>
2022-05-29 10:47:51 +0200

    Store log record listener instance within extension context

--- junit-platform-engine/src/test/java/io/cucumber/junit/platform/engine/WithLogRecordListener.java
+++ junit-platform-engine/src/test/java/io/cucumber/junit/platform/engine/WithLogRecordListener.java
@@ -1,4 +1,4 @@
-package io.cucumber.junit.platform.engine;
+package io.cucumber.core.logging;
 
 import io.cucumber.core.logging.LogRecordListener;
 import io.cucumber.core.logging.LoggerFactory;
@@ -14,23 +14,46 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.Optional;
 
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 @ExtendWith({ WithLogRecordListener.Extension.class })
 public @interface WithLogRecordListener {
     class Extension implements BeforeEachCallback, AfterEachCallback, ParameterResolver {
-        protected LogRecordListener logRecordListener;
+        private ExtensionContext.Store getContextStore(ExtensionContext context) {
+            Optional<Class<?>> testClass = context.getTestClass();
+            Object namespaceId = ExtensionContext.Namespace.GLOBAL;
+
+            // Use test class name as namespace for the context store
+            // If the test class is not available, fall back to the GLOBAL
+            // namespace
+            if (testClass.isPresent()) {
+                namespaceId = testClass.get().getSimpleName();
+            }
+
+            ExtensionContext.Namespace namespace = ExtensionContext.Namespace.create(namespaceId);
+            ExtensionContext.Store store = context.getStore(namespace);
+
+            return store;
+        }
 
         @Override
         public void beforeEach(ExtensionContext context) throws Exception {
-            logRecordListener = new LogRecordListener();
+            LogRecordListener logRecordListener = new LogRecordListener();
             LoggerFactory.addListener(logRecordListener);
+
+            // Store the log record listener instance into a namespaced store
+            // to prevent issues when running tests in parallel
+            ExtensionContext.Store store = getContextStore(context);
+            store.put("logRecordListener", logRecordListener);
         }
 
         @Override
         public void afterEach(ExtensionContext context) throws Exception {
-            LoggerFactory.removeListener(logRecordListener);
+            ExtensionContext.Store store = getContextStore(context);
+            LoggerFactory.removeListener((LogRecordListener) store.get("logRecordListener"));
+            store.remove("logRecordListener");
         }
 
         @Override
@@ -42,7 +65,8 @@ public @interface WithLogRecordListener {
         @Override
         public Object resolveParameter(ParameterContext paramContext, ExtensionContext extContext)
                 throws ParameterResolutionException {
-            return logRecordListener;
+            ExtensionContext.Store store = getContextStore(extContext);
+            return store.get("logRecordListener");
         }
     }
 }
