6f0595f3d29e00f52bd310f45dc19482d6e074f9
Karel Hovorka <git@karel-hovorka.eu>
2022-11-10 19:38:45 +0100

    PickleOrder extensibility proposition.

--- cucumber-core/src/main/java/io/cucumber/core/options/PickleOrderParser.java
+++ cucumber-core/src/main/java/io/cucumber/core/options/PickleOrderParser.java
@@ -5,7 +5,9 @@ import io.cucumber.core.logging.LoggerFactory;
 import io.cucumber.core.order.PickleOrder;
 import io.cucumber.core.order.StandardPickleOrders;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Random;
+import java.util.ServiceLoader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,6 +16,11 @@ final class PickleOrderParser {
     private static final Logger log = LoggerFactory.getLogger(PickleOrderParser.class);
 
     private static final Pattern RANDOM_AND_SEED_PATTERN = Pattern.compile("random(?::(\\d+))?");
+
+    private static final String ID_PATTERN = "\\p{javaJavaIdentifierStart}\\p{javaJavaIdentifierPart}*";
+    private static final Pattern FULLY_QUALIFIED_CLASS_NAME = Pattern.compile(ID_PATTERN + "(\\." + ID_PATTERN + ")*");
+
+    private static final String ERROR_MESSAGE = "Invalid order. Must be either reverse, random, random:<long> or fully qualified name of a class implementing PickleOrder interface.";
 
     static PickleOrder parse(String argument) {
         if ("reverse".equals(argument)) {
@@ -25,10 +32,31 @@ final class PickleOrderParser {
         }
 
         Matcher matcher = RANDOM_AND_SEED_PATTERN.matcher(argument);
-        if (!matcher.matches()) {
-            throw new IllegalArgumentException("Invalid order. Must be either reverse, random or random:<long>");
+        if (matcher.matches()) {
+            return StandardPickleOrders.random(parseSeed(matcher));
         }
 
+        if (FULLY_QUALIFIED_CLASS_NAME.matcher(argument).matches()) {
+            try {
+                return tryCreateInstance(argument);
+            } catch (Exception e) {
+                throw new IllegalArgumentException(ERROR_MESSAGE, e);
+            }
+        }
+
+        throw new IllegalArgumentException(ERROR_MESSAGE);
+    }
+
+    private static PickleOrder tryLoadUsingServiceProvider(String argument) {
+        ServiceLoader<PickleOrder> loader = ServiceLoader.load(PickleOrder.class, PickleOrderParser.class.getClassLoader());
+        return loader.stream().filter( it -> it.getClass().getName().equals(argument)).findFirst().orElseThrow().get();
+    }
+
+    private static PickleOrder tryCreateInstance(String argument) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        return (PickleOrder) Class.forName(argument).getConstructor().newInstance();
+    }
+
+    private static long parseSeed(Matcher matcher) {
         final long seed;
         String seedString = matcher.group(1);
         if (seedString != null) {
@@ -37,7 +65,7 @@ final class PickleOrderParser {
             seed = Math.abs(new Random().nextLong());
             log.info(() -> "Using random scenario order. Seed: " + seed);
         }
-        return StandardPickleOrders.random(seed);
+        return seed;
     }
 
 }
