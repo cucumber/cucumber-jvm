99c846fd6288a1eb72705edb8cef1f248c04b19b
M.P. Korstanje <rien.korstanje@gmail.com>
2022-11-14 22:03:10 +0100

    Used nested suites to map features

--- cucumber-core/src/main/java/io/cucumber/core/plugin/JUnitFormatter2.java
+++ cucumber-core/src/main/java/io/cucumber/core/plugin/JUnitFormatter2.java
@@ -30,11 +30,17 @@ import java.time.Instant;
 import java.util.Comparator;
 import java.util.Deque;
 import java.util.EnumSet;
+import java.util.LinkedHashSet;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static io.cucumber.messages.TimeConversion.timestampToJavaInstant;
 import static io.cucumber.messages.types.TestStepResultStatus.PASSED;
@@ -82,11 +88,11 @@ public final class JUnitFormatter2 implements ConcurrentEventListener {
         private final OutputStreamWriter out;
         private final XmlReportData data = new XmlReportData();
 
-        public MessagesToJunitWriter(OutputStream out) {
+        MessagesToJunitWriter(OutputStream out) {
             this.out = new OutputStreamWriter(out, StandardCharsets.UTF_8);
         }
 
-        public void write(Envelope envelope) throws IOException {
+        void write(Envelope envelope) throws IOException {
             data.collect(envelope);
             envelope.getTestRunFinished().ifPresent(this::handleTestRunFinished);
         }
@@ -109,7 +115,7 @@ public final class JUnitFormatter2 implements ConcurrentEventListener {
 
         private static final long MILLIS_PER_SECOND = SECONDS.toMillis(1L);
         private final Comparator<TestStepResult> testStepResultComparator = nullsFirst(
-            comparing(o -> o.getStatus().ordinal()));
+                comparing(o -> o.getStatus().ordinal()));
         private Instant testRunStarted;
         private Instant testRunFinished;
         private final Deque<String> testCaseStartedIds = new ConcurrentLinkedDeque<>();
@@ -119,8 +125,9 @@ public final class JUnitFormatter2 implements ConcurrentEventListener {
         private final Map<String, String> testCaseStartedIdToTestCaseId = new ConcurrentHashMap<>();
         private final Map<String, String> testCaseIdToPickleId = new ConcurrentHashMap<>();
         private final Map<String, String> pickleIdToPickleName = new ConcurrentHashMap<>();
-        private final Map<String, String> pickleIdToScenarioAstNodeId = new ConcurrentHashMap<>();
-        private final Map<String, String> scenarioAstNodeIdToFeatureName = new ConcurrentHashMap<>();
+        private final Map<String, String> pickleIdToPickleUri = new ConcurrentHashMap<>();
+
+        private final Map<String, String> uriToFeatureName = new ConcurrentHashMap<>();
 
         void collect(Envelope envelope) {
             envelope.getTestRunStarted().ifPresent(this::testRunStarted);
@@ -128,8 +135,8 @@ public final class JUnitFormatter2 implements ConcurrentEventListener {
             envelope.getTestCaseStarted().ifPresent(this::testCaseStarted);
             envelope.getTestCaseFinished().ifPresent(this::testCaseFinished);
             envelope.getTestStepFinished().ifPresent(this::testStepFinished);
-            envelope.getGherkinDocument().ifPresent(this::source);
             envelope.getPickle().ifPresent(this::pickle);
+            envelope.getGherkinDocument().ifPresent(this::gherkinDocument);
             envelope.getTestCase().ifPresent(this::testCase);
         }
 
@@ -149,38 +156,23 @@ public final class JUnitFormatter2 implements ConcurrentEventListener {
 
         void testCaseFinished(TestCaseFinished event) {
             this.testCaseStartedIdToFinishedInstant.put(event.getTestCaseStartedId(),
-                timestampToJavaInstant(event.getTimestamp()));
+                    timestampToJavaInstant(event.getTimestamp()));
         }
 
         void testStepFinished(TestStepFinished event) {
             testCaseStartedIdToResult.compute(event.getTestCaseStartedId(),
-                (__, previousStatus) -> mostSevereResult(previousStatus, event.getTestStepResult()));
-        }
-
-        void source(GherkinDocument event) {
-            event.getFeature().ifPresent(feature -> {
-                feature.getChildren().forEach(featureChild -> {
-                    featureChild.getRule().ifPresent(rule -> {
-                        rule.getChildren().forEach(ruleChild -> {
-                            ruleChild.getScenario().ifPresent(scenario -> {
-                                scenarioAstNodeIdToFeatureName.put(scenario.getId(), feature.getName());
-                            });
-                        });
-                    });
-                    featureChild.getScenario().ifPresent(scenario -> {
-                        scenarioAstNodeIdToFeatureName.put(scenario.getId(), feature.getName());
-                    });
-                });
-            });
+                    (__, previousStatus) -> mostSevereResult(previousStatus, event.getTestStepResult()));
         }
 
         void pickle(Pickle pickle) {
             pickleIdToPickleName.put(pickle.getId(), pickle.getName());
-            // @formatter:off
-            pickle.getAstNodeIds().stream()
-                    .findFirst()
-                    .ifPresent(id -> pickleIdToScenarioAstNodeId.put(pickle.getId(), id));
-            // @formatter:on
+            pickleIdToPickleUri.put(pickle.getId(), pickle.getUri());
+        }
+
+        void gherkinDocument(GherkinDocument event) {
+            event.getUri()
+                    .ifPresent(uri -> event.getFeature()
+                            .ifPresent(feature -> uriToFeatureName.put(uri, feature.getName())));
         }
 
         void testCase(TestCase testCase) {
@@ -195,9 +187,31 @@ public final class JUnitFormatter2 implements ConcurrentEventListener {
             return durationInSeconds(testRunStarted, testRunFinished);
         }
 
+        double getSuiteDurationInSeconds(String uri) {
+            // @formatter:off
+            Optional<Instant> firstStartedInstant = testCaseStartedIds.stream()
+                    .filter(testCaseStartedIdOfFeature(uri))
+                    .map(testCaseStartedIdToStartedInstant::get)
+                    .sorted()
+                    .findFirst();
+            // @formatter:on
+            // @formatter:off
+            Optional<Instant> lastFinishedInstant = testCaseStartedIds.stream()
+                    .filter(testCaseStartedIdOfFeature(uri))
+                    .map(testCaseStartedIdToFinishedInstant::get)
+                    .sorted()
+                    .reduce((first, second) -> second);
+            // @formatter:on
+            // @formatter:off
+            return firstStartedInstant
+                    .flatMap(started -> lastFinishedInstant.map(finished -> durationInSeconds(started, finished)))
+                    .orElse(0.0);
+            // @formatter:on
+        }
+
         double getDurationInSeconds(String testCaseStartedId) {
             return durationInSeconds(testCaseStartedIdToStartedInstant.get(testCaseStartedId),
-                testCaseStartedIdToFinishedInstant.get(testCaseStartedId));
+                    testCaseStartedIdToFinishedInstant.get(testCaseStartedId));
         }
 
         private static double durationInSeconds(Instant testRunStarted1, Instant testRunFinished1) {
@@ -212,8 +226,27 @@ public final class JUnitFormatter2 implements ConcurrentEventListener {
             // @formatter:on
         }
 
-        int getTestCaseCount() {
-            return testCaseStartedIdToStartedInstant.size();
+        Map<TestStepResultStatus, Long> getTestCaseStatusCounts(String uri) {
+            // @formatter:off
+            return testCaseStartedIds.stream()
+                    .filter(testCaseStartedIdOfFeature(uri))
+                    .map(testCaseStartedIdToResult::get)
+                    .filter(Objects::nonNull)
+                    .map(TestStepResult::getStatus)
+                    .collect(groupingBy(identity(), counting()));
+            // @formatter:on
+        }
+
+        long getTestCaseCount() {
+            return testCaseStartedIds.size();
+        }
+
+        long getTestCaseCount(String uri) {
+            // @formatter:off
+            return testCaseStartedIds.stream()
+                    .filter(testCaseStartedIdOfFeature(uri))
+                    .count();
+            // @formatter:on
         }
 
         String getPickleName(String testCaseStartedId) {
@@ -222,35 +255,45 @@ public final class JUnitFormatter2 implements ConcurrentEventListener {
             return pickleIdToPickleName.get(pickleId);
         }
 
-        public String getFeatureName(String testCaseStartedId) {
-            String testCaseId = testCaseStartedIdToTestCaseId.get(testCaseStartedId);
-            String pickleId = testCaseIdToPickleId.get(testCaseId);
-            String astNodeId = pickleIdToScenarioAstNodeId.get(pickleId);
-            return scenarioAstNodeIdToFeatureName.get(astNodeId);
+        String getFeatureName(String uri) {
+            return uriToFeatureName.get(uri);
         }
 
-        public Deque<String> testCaseStartedIds() {
-            return testCaseStartedIds;
+        Iterable<String> testCaseStartedIds(String uri) {
+            return testCaseStartedIds.stream()
+                    .filter(testCaseStartedIdOfFeature(uri))
+                    .collect(Collectors.toList());
+        }
+
+        private Predicate<String> testCaseStartedIdOfFeature(String uri) {
+            return id -> {
+                String testCaseId = testCaseStartedIdToTestCaseId.get(id);
+                String pickleId = testCaseIdToPickleId.get(testCaseId);
+                String pickleUri = pickleIdToPickleUri.get(pickleId);
+                return uri.equals(pickleUri);
+            };
         }
 
         private static final io.cucumber.messages.types.Duration ZERO_DURATION = new io.cucumber.messages.types.Duration(
-            0L, 0L);
-        private static final TestStepResult SCENARIO_WITH_NO_STEPS = new TestStepResult(ZERO_DURATION, null, PASSED); // By
-                                                                                                                      // definition,
-                                                                                                                      // but
-                                                                                                                      // see
-                                                                                                                      // https://github.com/cucumber/gherkin/issues/11
-        public TestStepResult getTestCaseStatus(String testCaseStartedId) {
+                0L, 0L);
+
+        // By definition, but see https://github.com/cucumber/gherkin/issues/11
+        private static final TestStepResult SCENARIO_WITH_NO_STEPS = new TestStepResult(ZERO_DURATION, null, PASSED);
+
+        TestStepResult getTestCaseStatus(String testCaseStartedId) {
             return testCaseStartedIdToResult.getOrDefault(testCaseStartedId, SCENARIO_WITH_NO_STEPS);
         }
 
+        Set<String> getFeatureUris() {
+            return new LinkedHashSet<>(pickleIdToPickleUri.values());
+        }
     }
 
     private static class XmlReportWriter {
         private final NumberFormat numberFormat = NumberFormat.getInstance(Locale.US);
         private final XmlReportData data;
 
-        public XmlReportWriter(XmlReportData data) {
+        XmlReportWriter(XmlReportData data) {
             this.data = data;
         }
 
@@ -259,31 +302,55 @@ public final class JUnitFormatter2 implements ConcurrentEventListener {
             XMLStreamWriter writer = factory.createXMLStreamWriter(out);
             writer.writeStartDocument("UTF-8", "1.0");
             newLine(writer);
-            writeTestsuite(data, writer);
+            writeTestSuites(writer);
             writer.writeEndDocument();
             writer.flush();
         }
 
-        private void writeTestsuite(XmlReportData data, XMLStreamWriter writer) throws XMLStreamException {
-            writer.writeStartElement("testsuite");
-            writeSuiteAttributes(writer);
+        private void writeTestSuites(XMLStreamWriter writer) throws XMLStreamException {
+            writer.writeStartElement("testsuites");
+            writeSuitesAttributes(writer);
             newLine(writer);
 
-            for (String testCaseStartedId : data.testCaseStartedIds()) {
-                writeTestcase(writer, testCaseStartedId);
+            for (String uri : data.getFeatureUris()) {
+                writeTestsuite(writer, uri);
             }
 
             writer.writeEndElement();
             newLine(writer);
         }
 
-        private void writeSuiteAttributes(XMLStreamWriter writer) throws XMLStreamException {
+        private void writeSuitesAttributes(XMLStreamWriter writer) throws XMLStreamException {
             writer.writeAttribute("name", "Cucumber");
             writer.writeAttribute("time", numberFormat.format(data.getSuiteDurationInSeconds()));
 
             Map<TestStepResultStatus, Long> counts = data.getTestCaseStatusCounts();
 
             writer.writeAttribute("tests", String.valueOf(data.getTestCaseCount()));
+            writer.writeAttribute("failures", String.valueOf(countFailures(counts)));
+            writer.writeAttribute("errors", "0");
+        }
+
+        private void writeTestsuite(XMLStreamWriter writer, String uri) throws XMLStreamException {
+            writer.writeStartElement("testsuite");
+            writeSuiteAttributes(writer, uri);
+            newLine(writer);
+
+            for (String testCaseStartedId : data.testCaseStartedIds(uri)) {
+                writeTestcase(writer, uri, testCaseStartedId);
+            }
+
+            writer.writeEndElement();
+            newLine(writer);
+        }
+
+        private void writeSuiteAttributes(XMLStreamWriter writer, String uri) throws XMLStreamException {
+            writer.writeAttribute("name", data.getFeatureName(uri));
+            writer.writeAttribute("time", numberFormat.format(data.getSuiteDurationInSeconds(uri)));
+
+            Map<TestStepResultStatus, Long> counts = data.getTestCaseStatusCounts(uri);
+
+            writer.writeAttribute("tests", String.valueOf(data.getTestCaseCount(uri)));
             writer.writeAttribute("skipped", counts.getOrDefault(SKIPPED, 0L).toString());
             writer.writeAttribute("failures", String.valueOf(countFailures(counts)));
             writer.writeAttribute("errors", "0");
@@ -296,10 +363,10 @@ public final class JUnitFormatter2 implements ConcurrentEventListener {
             return notPassedNotSkipped.stream().mapToLong(s -> counts.getOrDefault(s, 0L)).sum();
         }
 
-        private void writeTestcase(XMLStreamWriter writer, String id) throws XMLStreamException {
+        private void writeTestcase(XMLStreamWriter writer, String uri, String id) throws XMLStreamException {
             writer.writeStartElement("testcase");
 
-            writer.writeAttribute("classname", data.getFeatureName(id));
+            writer.writeAttribute("classname", data.getFeatureName(uri));
             writer.writeAttribute("name", data.getPickleName(id));
             writer.writeAttribute("time", numberFormat.format(data.getDurationInSeconds(id)));
 
@@ -333,7 +400,9 @@ public final class JUnitFormatter2 implements ConcurrentEventListener {
             }
             newLine(writer);
         }
+
         private static final Pattern CDATA_TERMINATOR_SPLIT = Pattern.compile("(?<=]])(?=>)");
+
         private static void writeCDataSafely(XMLStreamWriter writer, String data) throws XMLStreamException {
             // https://stackoverflow.com/questions/223652/is-there-a-way-to-escape-a-cdata-end-token-in-xml
             for (String part : CDATA_TERMINATOR_SPLIT.split(data)) {
