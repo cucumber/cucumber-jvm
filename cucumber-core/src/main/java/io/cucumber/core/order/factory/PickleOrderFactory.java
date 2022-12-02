0a95ab5008d0394704d2f6913e5c1274bcae0519
M.P. Korstanje <rien.korstanje@gmail.com>
2022-12-02 12:16:05 +0100

    Spotless

--- cucumber-core/src/main/java/io/cucumber/core/order/factory/PickleOrderFactory.java
+++ cucumber-core/src/main/java/io/cucumber/core/order/factory/PickleOrderFactory.java
@@ -4,12 +4,16 @@ import io.cucumber.core.order.PickleOrder;
 import org.apiguardian.api.API;
 
 /**
- * Responsible for creating {@link PickleOrder}. Factories use {@link java.util.ServiceLoader}.
+ * Responsible for creating {@link PickleOrder}. Factories use
+ * {@link java.util.ServiceLoader}.
  */
 @API(status = API.Status.EXPERIMENTAL)
 public interface PickleOrderFactory {
 
-    /** The name is used as a lookup when providing the name of order from a command line. */
+    /**
+     * The name is used as a lookup when providing the name of order from a
+     * command line.
+     */
     String getName();
 
     PickleOrder create(String argument);
