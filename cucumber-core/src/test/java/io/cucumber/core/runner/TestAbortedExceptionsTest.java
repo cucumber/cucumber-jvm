a782d66d4f9ccd248b9529a29712f5028ea9f5e8
Julien Kronegg <jk@kronegg.ch>
2022-12-20 23:34:12 +0100

    Create TestAbortedExceptionsTest.java
    Added test cases for `TestAbortedExceptions` `Predicate` with one negative example and two positive examples.

---
+++ cucumber-core/src/test/java/io/cucumber/core/runner/TestAbortedExceptionsTest.java
@@ -0,0 +1,23 @@
+package io.cucumber.core.runner;
+
+import org.junit.jupiter.api.Test;
+import org.opentest4j.TestAbortedException;
+
+import java.util.function.Predicate;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestAbortedExceptionsTest {
+    static class TestAbortedExceptionSubClass extends TestAbortedException {
+    }
+
+    @Test
+    void testPredicate() {
+        Predicate<Throwable> isTestAbortedExceptionPredicate = TestAbortedExceptions.createIsTestAbortedExceptionPredicate6();
+        assertFalse(isTestAbortedExceptionPredicate.test(new RuntimeException()));
+        assertTrue(isTestAbortedExceptionPredicate.test(new TestAbortedException()));
+        assertTrue(isTestAbortedExceptionPredicate.test(new TestAbortedExceptionSubClass()));
+    }
+
+}
