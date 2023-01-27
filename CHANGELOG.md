287f718c49678b5cf12687668236b9a0989a9cb6
Thai Nguyen <62538035+ntthaibk@users.noreply.github.com>
2023-01-27 18:17:42 +0800

    [Spring] Instantiate TestContextManager synchronously (#2687)
    In brief, the problem is that invoking `new TestContextManager(someContextConfiguration)`
    for identical instances of someContextConfiguration is not actually
    thread-safe. Spring caches the configuration derived from `someContextConfiguration`
    and when AOT is used, will fail to apply idempotent updates configuration.
    
    Fixes: #2686
    
    Co-authored-by: M.P. Korstanje <rien.korstanje@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Fixed
+- [Spring] Instantiate `TestContextManager` synchronously ([#2686](https://github.com/cucumber/cucumber-jvm/pull/2686), [#2687](https://github.com/cucumber/cucumber-jvm/pull/2687) Thai Nguyen, M.P. Korstanje)
+
 ### Added
 - [Core] Warn when `cucumber.options` is used ([#2685](https://github.com/cucumber/cucumber-jvm/pull/2685) M.P. Korstanje)
 
--- cucumber-spring/src/main/java/io/cucumber/spring/TestContextAdaptor.java
+++ cucumber-spring/src/main/java/io/cucumber/spring/TestContextAdaptor.java
@@ -13,6 +13,7 @@ import java.lang.reflect.Method;
 import java.util.ArrayDeque;
 import java.util.Collection;
 import java.util.Deque;
+import java.util.function.Supplier;
 
 import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
 import static org.springframework.beans.factory.config.AutowireCapableBeanFactory.AUTOWIRE_CONSTRUCTOR;
@@ -20,34 +21,43 @@ import static org.springframework.beans.factory.config.AutowireCapableBeanFactor
 class TestContextAdaptor {
 
     private static final Object monitor = new Object();
-
     private final TestContextManager delegate;
     private final ConfigurableApplicationContext applicationContext;
-    private final Collection<Class<?>> glueClasses;
     private final Deque<Runnable> stopInvocations = new ArrayDeque<>();
     private Object delegateTestInstance;
 
-    TestContextAdaptor(
-            TestContextManager delegate,
+    static TestContextAdaptor create(
+            Supplier<TestContextManager> testContextManagerSupplier,
             Collection<Class<?>> glueClasses
     ) {
-        TestContext testContext = delegate.getTestContext();
-        ConfigurableApplicationContext applicationContext = (ConfigurableApplicationContext) testContext
-                .getApplicationContext();
-        this.delegate = delegate;
-        this.applicationContext = applicationContext;
-        this.glueClasses = glueClasses;
+        synchronized (monitor) {
+            // While under construction, the TestContextManager delegate will
+            // build a cached version of the application context configuration.
+            // Since Spring Boot 3 and in combination with AOT building this
+            // configuration is not idempotent (#2686).
+            TestContextManager delegate = testContextManagerSupplier.get();
+
+            TestContext testContext = delegate.getTestContext();
+            ConfigurableApplicationContext applicationContext = (ConfigurableApplicationContext) testContext
+                    .getApplicationContext();
+
+            // The TestContextManager delegate makes the application context
+            // available to other threads. Registering the glue however modifies
+            // the application context. To avoid concurrent modification issues
+            // (#1823, #1153, #1148, #1106) we do this serially.
+            registerGlueCodeScope(applicationContext);
+            registerStepClassBeanDefinitions(applicationContext.getBeanFactory(), glueClasses);
+
+            return new TestContextAdaptor(delegate);
+        }
     }
 
-    public final void start() {
-        // The TestContextManager delegate makes the application context
-        // available to other threads. Registering the glue however modifies the
-        // application context. To avoid concurrent modification issues (#1823,
-        // #1153, #1148, #1106) we do this serially.
-        synchronized (monitor) {
-            registerGlueCodeScope(applicationContext);
-            registerStepClassBeanDefinitions(applicationContext.getBeanFactory());
-        }
+    TestContextAdaptor(TestContextManager delegate) {
+        this.delegate = delegate;
+        this.applicationContext = (ConfigurableApplicationContext) delegate.getTestContext().getApplicationContext();
+    }
+
+    final void start() {
         stopInvocations.push(this::notifyTestContextManagerAboutAfterTestClass);
         notifyContextManagerAboutBeforeTestClass();
         stopInvocations.push(this::stopCucumberTestContext);
@@ -125,7 +135,7 @@ class TestContextAdaptor {
         }
     }
 
-    private void registerGlueCodeScope(ConfigurableApplicationContext context) {
+    private static void registerGlueCodeScope(ConfigurableApplicationContext context) {
         while (context != null) {
             ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
             // Scenario scope may have already been registered by another
@@ -146,14 +156,16 @@ class TestContextAdaptor {
         }
     }
 
-    private void registerStepClassBeanDefinitions(ConfigurableListableBeanFactory beanFactory) {
+    private static void registerStepClassBeanDefinitions(
+            ConfigurableListableBeanFactory beanFactory, Collection<Class<?>> glueClasses
+    ) {
         BeanDefinitionRegistry registry = (BeanDefinitionRegistry) beanFactory;
         for (Class<?> glue : glueClasses) {
             registerStepClassBeanDefinition(registry, glue);
         }
     }
 
-    private void registerStepClassBeanDefinition(BeanDefinitionRegistry registry, Class<?> glueClass) {
+    private static void registerStepClassBeanDefinition(BeanDefinitionRegistry registry, Class<?> glueClass) {
         String beanName = glueClass.getName();
         // Step definition may have already been
         // registered as a bean by another thread.
@@ -166,7 +178,7 @@ class TestContextAdaptor {
                 .getBeanDefinition());
     }
 
-    public final void stop() {
+    final void stop() {
         // Cucumber only supports 1 set of before/after semantics while JUnit
         // and Spring have 2 sets. So here we use a stack to ensure we don't
         // invoke only the matching after methods for each before methods.
