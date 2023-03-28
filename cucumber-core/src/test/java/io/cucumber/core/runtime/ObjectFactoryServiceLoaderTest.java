4d4ef8f70c3d9ee32cef1794e4d61555448945f3
Julien Kronegg <julien [at] kronegg.ch>
2023-03-28 11:12:13 +0200

    feat: moved doc to test classes

--- cucumber-core/src/test/java/io/cucumber/core/runtime/ObjectFactoryServiceLoaderTest.java
+++ cucumber-core/src/test/java/io/cucumber/core/runtime/ObjectFactoryServiceLoaderTest.java
@@ -20,7 +20,26 @@ import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
- * @see <a href="UuidGeneratorServiceLoaderTest.md">test-cases description</a>
+ * Testcases for `ObjectFactoryServiceLoader`
+ *
+ * <!-- @formatter:off -->
+ * | # | object-factory property | Available services                                    | Result                                                                           |
+ * |---|-------------------------|-------------------------------------------------------|----------------------------------------------------------------------------------|
+ * | 1 | undefined               | none                                                  | exception, no generators available                                               |
+ * | 2 | undefined               | DefaultObjectFactory                                  | DefaultObjectFactory used                                                        |
+ * | 3 | DefaultObjectFactory    | DefaultObjectFactory                                  | DefaultObjectFactory used                                                        |
+ * | 4 | undefined               | DefaultObjectFactory, OtherFactory                    | OtherFactory used                                                                |
+ * | 5 | DefaultObjectFactory    | DefaultObjectFactory, OtherFactory                    | DefaultObjectFactory used                                                        |
+ * | 6 | undefined               | DefaultObjectFactory, OtherFactory, YetAnotherFactory | exception, cucumber couldn't  decide multiple (non default) generators available |
+ * | 7 | OtherFactory            | DefaultObjectFactory, OtherFactory, YetAnotherFactory | OtherFactory used                                                                |
+ * | 8 | OtherFactory            | DefaultObjectFactory                                  | exception, class not found through SPI                                           |
+ * | 9 | undefined               | OtherFactory                                          | OtherFactory used                                                                |
+ * <!-- @formatter:on -->
+ *
+ * Essentially this means that
+ * * (2) Cucumber works by default
+ * * (4) When adding a custom implementation to the class path it is used automatically
+ * * When cucumber should not guess (5) or can not guess (7), the property is used to force a choice
  */
 class ObjectFactoryServiceLoaderTest {
 
--- cucumber-core/src/test/java/io/cucumber/core/runtime/UuidGeneratorServiceLoaderTest.java
+++ cucumber-core/src/test/java/io/cucumber/core/runtime/UuidGeneratorServiceLoaderTest.java
@@ -16,7 +16,25 @@ import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * @see <a href="UuidGeneratorServiceLoaderTest.md">test-cases description</a>
+ * # Testcases for `UuidGeneratorServiceLoader`
+ *
+ * <!-- @formatter:off -->
+ * | #   | uuid-generator property   | Available services                                                                  | Result                                                                           |
+ * |-----|---------------------------|-------------------------------------------------------------------------------------|----------------------------------------------------------------------------------|
+ * | 1   | undefined                 | none                                                                                | exception, no generators available                                               |
+ * | 2   | undefined                 | RandomUuidGenerator, IncrementingUuidGenerator                                      | RandomUuidGenerator used                                                         |
+ * | 3   | RandomUuidGenerator       | RandomUuidGenerator, IncrementingUuidGenerator                                      | RandomUuidGenerator used                                                         |
+ * | 4   | undefined                 | RandomUuidGenerator, IncrementingUuidGenerator, OtherGenerator                      | OtherGenerator used                                                              |
+ * | 5   | RandomUuidGenerator       | RandomUuidGenerator, IncrementingUuidGenerator, OtherGenerator                      | RandomUuidGenerator used                                                         |
+ * | 6   | undefined                 | RandomUuidGenerator, IncrementingUuidGenerator, OtherGenerator, YetAnotherGenerator | exception, cucumber couldn't  decide multiple (non default) generators available |
+ * | 7   | OtherGenerator            | RandomUuidGenerator, IncrementingUuidGenerator, OtherGenerator, YetAnotherGenerator | OtherGenerator used                                                              |
+ * | 8   | IncrementingUuidGenerator | RandomUuidGenerator, IncrementingUuidGenerator, OtherGenerator, YetAnotherGenerator | IncrementingUuidGenerator used                                                   |
+ * | 9   | IncrementingUuidGenerator | RandomUuidGenerator, IncrementingUuidGenerator                                      | IncrementingUuidGenerator used                                                   |
+ * | 10  | OtherGenerator            | none                                                                                | exception, generator OtherGenerator not available                                |
+ * | 11  | undefined                 | OtherGenerator                                                                      | OtherGenerator used                                                              |
+ * | 12  | undefined                 | IncrementingUuidGenerator, OtherGenerator                                           | OtherGenerator used                                                              |
+ * | 13  | undefined                 | IncrementingUuidGenerator                                                           | IncrementingUuidGenerator used                                                   |
+ * <!-- @formatter:on -->
  */
 class UuidGeneratorServiceLoaderTest {
 
