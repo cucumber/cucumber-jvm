681cc8bae66426b8d79d3991d9beb15410d60fb7
M.P. Korstanje <rien.korstanje@gmail.com>
2023-04-07 15:15:25 +0200

    Remove unneeded annotation

--- cucumber-picocontainer/src/test/java/io/cucumber/picocontainer/StepDefinitionsWithTransitiveDependencies.java
+++ cucumber-picocontainer/src/test/java/io/cucumber/picocontainer/StepDefinitionsWithTransitiveDependencies.java
@@ -1,18 +1,11 @@
 package io.cucumber.picocontainer;
 
-import io.cucumber.java.en.Given;
-
 public class StepDefinitionsWithTransitiveDependencies {
 
     final FirstDependency firstDependency;
 
     public StepDefinitionsWithTransitiveDependencies(FirstDependency firstDependency) {
         this.firstDependency = firstDependency;
-    }
-
-    @Given("step definitions with transitive dependencies")
-    public void stepDefinitionsWithTransitiveDependencies() {
-
     }
 
     public static class FirstDependency {
