7d4dbc25a219e2d1146d45d44869215479d21a50
vishesh10 <visheshbansal10@gmail.com>
2023-05-14 18:08:57 +0530

    Improve Javadoc on CucumberContextConfiguration (#2738)
    Co-authored-by: M.P. Korstanje <rien.korstanje@gmail.com>

--- cucumber-spring/src/main/java/io/cucumber/spring/CucumberContextConfiguration.java
+++ cucumber-spring/src/main/java/io/cucumber/spring/CucumberContextConfiguration.java
@@ -8,7 +8,29 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Configure an application context class that Cucumber should use
+ * This annotation is used on a configuration class to make the Cucumber aware
+ * of the test configuration. This is to be used in conjunction with
+ * {@code @ContextConfiguration}, {@code @ContextHierarchy} or
+ * {@code @BootstrapWith}. In case of SpringBoot, the configuration class can be
+ * annotated as follows:
+ * <p>
+ * 
+ * <pre>
+ * &#64;CucumberContextConfiguration
+ * &#64;SpringBootTest(classes = TestConfig.class)
+ * public class CucumberSpringConfiguration {
+ * }
+ * </pre>
+ * 
+ * Notes:
+ * <ul>
+ * <li>Only one glue class should be annotated with
+ * {@code @CucumberContextConfiguration} otherwise an exception will be
+ * thrown.</li>
+ * <li>Cucumber Spring uses Spring's {@code TestContextManager} framework
+ * internally. As a result a single Cucumber scenario will mostly behave like a
+ * JUnit test.</li>
+ * </ul>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
