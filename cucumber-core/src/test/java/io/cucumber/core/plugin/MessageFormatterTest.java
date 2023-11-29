d43e25e9f2606a5137ff5529473c17a63e57cbe3
bongale2 <bongale2@illinois.edu>
2023-11-28 21:39:24 -0600

    Fixed test failure by comparing maps instead of json strings

--- cucumber-core/src/test/java/io/cucumber/core/plugin/MessageFormatterTest.java
+++ cucumber-core/src/test/java/io/cucumber/core/plugin/MessageFormatterTest.java
@@ -1,5 +1,6 @@
 package io.cucumber.core.plugin;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.core.eventbus.EventBus;
 import io.cucumber.core.runtime.TimeServiceEventBus;
 import io.cucumber.messages.types.Envelope;
@@ -9,12 +10,18 @@ import io.cucumber.messages.types.Timestamp;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.time.Clock;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.in;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class MessageFormatterTest {
 
@@ -32,9 +39,21 @@ public class MessageFormatterTest {
         bus.send(Envelope.of(testRunFinished));
 
         String ndjson = new String(bytes.toByteArray(), UTF_8);
-        assertThat(ndjson, containsString("" +
-                "{\"testRunStarted\":{\"timestamp\":{\"seconds\":10,\"nanos\":0}}}\n" +
-                "{\"testRunFinished\":{\"success\":true,\"timestamp\":{\"seconds\":15,\"nanos\":0}}}\n"));
+        String expectedJson = "{\"testRunStarted\":{\"timestamp\":{\"seconds\":10,\"nanos\":0}},\"testRunFinished\":{\"success\":true,\"timestamp\":{\"seconds\":15,\"nanos\":0}}}";
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            Map<String, Object> expectedMap = objectMapper.readValue(expectedJson, Map.class);
+
+            String[] ndjsonObjects = ndjson.split("\\n");
+            Map<String, Object> ndjsonMap = new HashMap<>();
+            for (String ndjsonObject : ndjsonObjects) {
+                ndjsonMap.putAll(objectMapper.readValue(ndjsonObject, Map.class));
+            }
+
+            assertThat(expectedMap.entrySet(), everyItem(is(in(ndjsonMap.entrySet()))));
+        } catch (IOException e) {
+            fail("An exception occurred while processing JSON: " + e.getMessage());
+        }
     }
 
 }
