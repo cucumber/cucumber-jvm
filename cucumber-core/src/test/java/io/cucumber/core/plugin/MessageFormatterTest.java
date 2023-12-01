d6954d939c318ee57cef22050574710b108852fb
bongale2 <bongale2@illinois.edu>
2023-12-01 15:09:11 -0600

    Updated test fix. Used JSONAssert to compare json strings.

--- cucumber-core/src/test/java/io/cucumber/core/plugin/MessageFormatterTest.java
+++ cucumber-core/src/test/java/io/cucumber/core/plugin/MessageFormatterTest.java
@@ -1,32 +1,26 @@
 package io.cucumber.core.plugin;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.core.eventbus.EventBus;
 import io.cucumber.core.runtime.TimeServiceEventBus;
 import io.cucumber.messages.types.Envelope;
 import io.cucumber.messages.types.TestRunFinished;
 import io.cucumber.messages.types.TestRunStarted;
 import io.cucumber.messages.types.Timestamp;
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.time.Clock;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.everyItem;
-import static org.hamcrest.Matchers.in;
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class MessageFormatterTest {
 
     @Test
-    void test() {
+    void test() throws JSONException {
         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
         MessageFormatter formatter = new MessageFormatter(bytes);
         EventBus bus = new TimeServiceEventBus(Clock.systemUTC(), UUID::randomUUID);
@@ -39,20 +33,13 @@ public class MessageFormatterTest {
         bus.send(Envelope.of(testRunFinished));
 
         String ndjson = new String(bytes.toByteArray(), UTF_8);
-        String expectedJson = "{\"testRunStarted\":{\"timestamp\":{\"seconds\":10,\"nanos\":0}},\"testRunFinished\":{\"success\":true,\"timestamp\":{\"seconds\":15,\"nanos\":0}}}";
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            Map<String, Object> expectedMap = objectMapper.readValue(expectedJson, Map.class);
-
-            String[] ndjsonObjects = ndjson.split("\\n");
-            Map<String, Object> ndjsonMap = new HashMap<>();
-            for (String ndjsonObject : ndjsonObjects) {
-                ndjsonMap.putAll(objectMapper.readValue(ndjsonObject, Map.class));
-            }
-
-            assertThat(expectedMap.entrySet(), everyItem(is(in(ndjsonMap.entrySet()))));
-        } catch (IOException e) {
-            fail("An exception occurred while processing JSON: " + e.getMessage());
+        String[] ndjsonArray = ndjson.split("\\n");
+        String[] expectedArray = {
+                "{\"testRunStarted\":{\"timestamp\":{\"seconds\":10,\"nanos\":0}}}",
+                "{\"testRunFinished\":{\"success\":true,\"timestamp\":{\"seconds\":15,\"nanos\":0}}}"
+        };
+        for (int i = 0; i < ndjsonArray.length; i++) {
+            JSONAssert.assertEquals(expectedArray[i], ndjsonArray[i], JSONCompareMode.LENIENT);
         }
     }
 
