a6b13a3f4c9c12b5f7224f0be72a8c41ca2c593a
M.P. Korstanje <rien.korstanje@gmail.com>
2024-02-17 00:10:40 +0100

    [JUnit Platform] Fix UniqueId resolution
    A unique id of any feature element is prefixed by the unique
    id of the engine. This prefix, and only this prefix should be
    skipped. Simply finding the first feature segment is not correct.
    
    In practice, because we check if the unique id we try to resolve
    is prefixed by the engine descriptor, everything does work out
    fine. But this should help against malformed ids.

--- cucumber-junit-platform-engine/src/main/java/io/cucumber/junit/platform/engine/FeatureResolver.java
+++ cucumber-junit-platform-engine/src/main/java/io/cucumber/junit/platform/engine/FeatureResolver.java
@@ -214,16 +214,20 @@ final class FeatureResolver {
         Predicate<TestDescriptor> keepTestWithSelectedId = testDescriptor -> uniqueId
                 .equals(testDescriptor.getUniqueId());
 
+        List<UniqueId.Segment> resolvedSegments = engineDescriptor.getUniqueId().getSegments();
+
         uniqueId.getSegments()
                 .stream()
+                .skip(resolvedSegments.size())
+                .findFirst()
                 .filter(FeatureOrigin::isFeatureSegment)
                 .map(UniqueId.Segment::getValue)
                 .map(URI::create)
-                .flatMap(this::resolveUri)
-                .forEach(featureDescriptor -> {
+                .map(this::resolveUri)
+                .ifPresent(featureDescriptors -> featureDescriptors.forEach(featureDescriptor -> {
                     featureDescriptor.prune(keepTestWithSelectedId);
                     engineDescriptor.mergeFeature(featureDescriptor);
-                });
+                }));
     }
 
     private Stream<FeatureDescriptor> resolveUri(URI uri) {
