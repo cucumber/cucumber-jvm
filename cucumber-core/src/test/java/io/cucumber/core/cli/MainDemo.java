16530e0806be552b6eb6f7852af9854b628d2496
M.P. Korstanje <mpkorstanje@users.noreply.github.com>
2025-08-28 15:52:52 +0200

    Add --i18n and --i18n-languages options (#3053)
    Closes: #3052

--- cucumber-core/src/test/java/io/cucumber/core/cli/MainDemo.java
+++ cucumber-core/src/test/java/io/cucumber/core/cli/MainDemo.java
@@ -5,7 +5,7 @@ public class MainDemo {
     public static void main(String[] args) {
         // Main.main("--i18n");
         // Main.main("--i18n", "help");
-        Main.main("--i18n", "tlh");
+        Main.main("--i18n-keywords", "tlh");
     }
 
 }
