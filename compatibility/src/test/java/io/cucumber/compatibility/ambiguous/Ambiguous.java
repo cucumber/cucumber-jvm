ea44b2d5c1dacd704e65006d33b0e921de585ef5
M.P. Korstanje <rien.korstanje@gmail.com>
2025-10-01 16:18:59 +0200

    Update

--- compatibility/src/test/java/io/cucumber/compatibility/ambiguous/Ambiguous.java
+++ compatibility/src/test/java/io/cucumber/compatibility/ambiguous/Ambiguous.java
@@ -3,13 +3,14 @@ package io.cucumber.compatibility.ambiguous;
 import io.cucumber.java.en.Given;
 
 public class Ambiguous {
-    @Given("^a (.*?) with (.*?)$")
-    public void first_ambiguous_step(String a, String b) {
-
-    }
 
     @Given("^a step with (.*)$")
     public void second_ambiguous_step(String a) {
+
+    }
+
+    @Given("^a (.*?) with (.*?)$")
+    public void first_ambiguous_step(String a, String b) {
 
     }
 
