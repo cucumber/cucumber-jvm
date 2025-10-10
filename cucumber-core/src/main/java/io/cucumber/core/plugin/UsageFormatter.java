5cdfa8ad602936e8d5b50e4b88e7381d98b59119
M.P. Korstanje <rien.korstanje@gmail.com>
2025-10-10 18:03:15 +0200

    Extract to repo

--- cucumber-core/src/main/java/io/cucumber/core/plugin/UsageFormatter.java
+++ cucumber-core/src/main/java/io/cucumber/core/plugin/UsageFormatter.java
@@ -4,20 +4,10 @@ import io.cucumber.messages.types.Envelope;
 import io.cucumber.plugin.ConcurrentEventListener;
 import io.cucumber.plugin.Plugin;
 import io.cucumber.plugin.event.EventPublisher;
-import io.cucumber.query.Query;
-import io.cucumber.query.Repository;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.nio.charset.StandardCharsets;
-import java.util.function.Function;
-
-import static io.cucumber.query.Repository.RepositoryFeature.INCLUDE_GHERKIN_DOCUMENTS;
-import static io.cucumber.query.Repository.RepositoryFeature.INCLUDE_STEP_DEFINITIONS;
-import static java.util.Objects.requireNonNull;
 
 /**
  * Formatter to measure performance of steps. Includes average and median step
@@ -58,102 +48,4 @@ public final class UsageFormatter implements Plugin, ConcurrentEventListener {
         }
     }
 
-    /**
-     * Creates a usage report for step definitions based on a test run.
-     * <p>
-     * Note: Messages are first collected and only written once the stream is
-     * closed.
-     */
-    static final class MessagesToUsageWriter implements AutoCloseable {
-
-        private final OutputStreamWriter out;
-        private final Repository repository = Repository.builder()
-                .feature(INCLUDE_GHERKIN_DOCUMENTS, true)
-                .feature(INCLUDE_STEP_DEFINITIONS, true)
-                .build();
-        private final Query query = new Query(repository);
-        private final Serializer serializer;
-        private final Function<String, String> uriFormatter;
-        private boolean streamClosed = false;
-
-        MessagesToUsageWriter(OutputStream out, Serializer serializer, Function<String, String> uriFormatter) {
-            this.out = new OutputStreamWriter(
-                requireNonNull(out),
-                StandardCharsets.UTF_8);
-            this.serializer = requireNonNull(serializer);
-            this.uriFormatter = requireNonNull(uriFormatter);
-        }
-
-        public void write(Envelope envelope) throws IOException {
-            if (streamClosed) {
-                throw new IOException("Stream closed");
-            }
-            repository.update(envelope);
-        }
-
-        public static Builder builder(Serializer serializer) {
-            return new Builder(serializer);
-        }
-
-        static final class Builder {
-            private final Serializer serializer;
-            private Function<String, String> uriFormatter = Function.identity();
-
-            private Builder(Serializer serializer) {
-                this.serializer = requireNonNull(serializer);
-            }
-
-            private static Function<String, String> removePrefix(String prefix) {
-                // TODO: Needs coverage
-                return s -> {
-                    if (s.startsWith(prefix)) {
-                        return s.substring(prefix.length());
-                    }
-                    return s;
-                };
-            }
-
-            /**
-             * Removes a given prefix from all URI locations.
-             * <p>
-             * The typical usage would be to trim the current working directory.
-             * This makes the report more readable.
-             */
-            public Builder removeUriPrefix(String prefix) {
-                // TODO: Needs coverage
-                this.uriFormatter = removePrefix(requireNonNull(prefix));
-                return this;
-            }
-
-            public MessagesToUsageWriter build(OutputStream out) {
-                requireNonNull(out);
-                return new MessagesToUsageWriter(out, serializer, uriFormatter);
-            }
-        }
-
-        @Override
-        public void close() throws IOException {
-            if (streamClosed) {
-                return;
-            }
-            try {
-                UsageReport report = new UsageReportBuilder(query, uriFormatter).build();
-                String formatted = UsageReportFormatter.format(report);
-                out.write(formatted);
-            } finally {
-                try {
-                    out.close();
-                } finally {
-                    streamClosed = true;
-                }
-            }
-        }
-
-        @FunctionalInterface
-        interface Serializer {
-
-            void writeValue(Writer writer, Object value) throws IOException;
-
-        }
-    }
 }
