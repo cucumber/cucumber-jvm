81361ec45de7107fc6a3d0c82c9a76f5f03f9d42
M.P. Korstanje <mpkorstanje@users.noreply.github.com>
2025-10-13 04:02:06 +0200

    Use a message based timeline formatter (#3095)

--- cucumber-core/src/main/java/io/cucumber/core/plugin/TimelineFormatter.java
+++ cucumber-core/src/main/java/io/cucumber/core/plugin/TimelineFormatter.java
@@ -1,38 +1,53 @@
 package io.cucumber.core.plugin;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.core.JsonGenerator.Feature;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import io.cucumber.core.exception.CucumberException;
+import io.cucumber.messages.Convertor;
+import io.cucumber.messages.types.Envelope;
+import io.cucumber.messages.types.Feature;
+import io.cucumber.messages.types.Pickle;
+import io.cucumber.messages.types.PickleTag;
+import io.cucumber.messages.types.TestCaseFinished;
+import io.cucumber.messages.types.TestCaseStarted;
+import io.cucumber.messages.types.TestStepResult;
+import io.cucumber.messages.types.TestStepResultStatus;
 import io.cucumber.plugin.ConcurrentEventListener;
 import io.cucumber.plugin.event.EventPublisher;
-import io.cucumber.plugin.event.Location;
-import io.cucumber.plugin.event.Node;
-import io.cucumber.plugin.event.TestCase;
-import io.cucumber.plugin.event.TestCaseEvent;
-import io.cucumber.plugin.event.TestCaseFinished;
-import io.cucumber.plugin.event.TestCaseStarted;
-import io.cucumber.plugin.event.TestRunFinished;
-import io.cucumber.plugin.event.TestSourceParsed;
+import io.cucumber.query.Lineage;
+import io.cucumber.query.Query;
+import io.cucumber.query.Repository;
 
-import java.io.Closeable;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.TreeMap;
-import java.util.function.Predicate;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Value.construct;
+import static io.cucumber.query.Repository.RepositoryFeature.INCLUDE_GHERKIN_DOCUMENTS;
 import static java.util.Locale.ROOT;
 
+/**
+ * Writes a timeline of scenario execution.
+ * <p>
+ * Note: The report is only written once the test run is finished.
+ */
 public final class TimelineFormatter implements ConcurrentEventListener {
 
     private static final String[] TEXT_ASSETS = new String[] {
@@ -49,164 +64,195 @@ public final class TimelineFormatter implements ConcurrentEventListener {
             "/io/cucumber/core/plugin/timeline/chosen-sprite.png"
     };
 
-    private final Map<String, TestData> allTests = new HashMap<>();
-    private final Map<Long, GroupData> threadGroups = new HashMap<>();
+    private final Repository repository = Repository.builder()
+            .feature(INCLUDE_GHERKIN_DOCUMENTS, true)
+            .build();
+    private final Query query = new Query(repository);
 
     private final File reportDir;
-    private final UTF8OutputStreamWriter reportJs;
-    private final Map<URI, Collection<Node>> parsedTestSources = new HashMap<>();
 
     private final ObjectMapper objectMapper = new ObjectMapper()
-            .setSerializationInclusion(Include.NON_NULL)
+            .setDefaultPropertyInclusion(construct(Include.NON_ABSENT, Include.NON_ABSENT))
             .enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING)
-            .disable(Feature.AUTO_CLOSE_TARGET);
+            .disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
 
-    @SuppressWarnings("unused") // Used by PluginFactory
+    @SuppressWarnings({ "unused", "RedundantThrows" }) // Used by PluginFactory
     public TimelineFormatter(File reportDir) throws FileNotFoundException {
-        reportDir.mkdirs();
+        boolean dontCare = reportDir.mkdirs();
         if (!reportDir.isDirectory()) {
             throw new CucumberException(String.format("The %s needs an existing directory. Not a directory: %s",
                 getClass().getName(), reportDir.getAbsolutePath()));
         }
-
         this.reportDir = reportDir;
-        this.reportJs = new UTF8OutputStreamWriter(new FileOutputStream(new File(reportDir, "report.js")));
     }
 
     @Override
     public void setEventPublisher(EventPublisher publisher) {
-        publisher.registerHandlerFor(TestSourceParsed.class, this::handleTestSourceParsed);
-        publisher.registerHandlerFor(TestCaseStarted.class, this::handleTestCaseStarted);
-        publisher.registerHandlerFor(TestCaseFinished.class, this::handleTestCaseFinished);
-        publisher.registerHandlerFor(TestRunFinished.class, this::finishReport);
+        publisher.registerHandlerFor(Envelope.class, this::write);
     }
 
-    private void handleTestSourceParsed(TestSourceParsed event) {
-        parsedTestSources.put(event.getUri(), event.getNodes());
-    }
+    private void write(Envelope event) {
+        repository.update(event);
 
-    private void handleTestCaseStarted(TestCaseStarted event) {
-        Thread thread = Thread.currentThread();
-        threadGroups.computeIfAbsent(thread.getId(), threadId -> {
-            GroupData group = new GroupData();
-            group.setContent(thread.toString());
-            group.setId(threadId);
-            return group;
-        });
-
-        TestCase testCase = event.getTestCase();
-        TestData data = new TestData();
-        data.setId(getId(event));
-        data.setFeature(findRootNodeName(testCase));
-        data.setScenario(testCase.getName());
-        data.setStart(event.getInstant().toEpochMilli());
-        data.setTags(buildTagsValue(testCase));
-        data.setGroup(thread.getId());
-        allTests.put(data.getId(), data);
-    }
-
-    private String buildTagsValue(TestCase testCase) {
-        StringBuilder tags = new StringBuilder();
-        for (String tag : testCase.getTags()) {
-            tags.append(tag.toLowerCase()).append(",");
+        // TODO: Plugins should implement the closable interface
+        // and be closed by Cucumber
+        if (event.getTestRunFinished().isPresent()) {
+            try {
+                writeTimeLineReport();
+            } catch (IOException e) {
+                throw new IllegalStateException(e);
+            }
         }
-        return tags.toString();
     }
 
-    private void handleTestCaseFinished(TestCaseFinished event) {
-        TestData data = allTests.get(getId(event));
-        data.setEnd(event.getInstant().toEpochMilli());
-        data.setClassName(event.getResult().getStatus().name().toLowerCase(ROOT));
-    }
-
-    private String findRootNodeName(TestCase testCase) {
-        Location location = testCase.getLocation();
-        Predicate<Node> withLocation = candidate -> candidate.getLocation().equals(location);
-        return parsedTestSources.get(testCase.getUri())
-                .stream()
-                .map(node -> node.findPathTo(withLocation))
+    private void writeTimeLineReport() throws IOException {
+        Map<String, TimeLineGroup> timeLineGroups = new HashMap<>();
+        AtomicInteger nextGroupId = new AtomicInteger();
+        List<TimeLineItem> timeLineItems = query.findAllTestCaseFinished().stream()
+                .map(testCaseFinished -> query.findTestCaseStartedBy(testCaseFinished)
+                        .map(testCaseStarted -> createTestData(
+                            testCaseFinished, //
+                            testCaseStarted, //
+                            createTimeLineGroup(timeLineGroups, nextGroupId) //
+                        )))
                 .filter(Optional::isPresent)
                 .map(Optional::get)
-                .findFirst()
-                .map(nodes -> nodes.get(0))
-                .flatMap(Node::getName)
-                .orElse("Unknown");
+                .collect(Collectors.toList());
+
+        writeTimeLineReport(timeLineGroups, timeLineItems);
     }
 
-    private void finishReport(TestRunFinished event) {
+    private Function<String, TimeLineGroup> createTimeLineGroup(
+            Map<String, TimeLineGroup> timeLineGroups,
+            AtomicInteger nextGroupId
+    ) {
 
-        try {
+        return workerId -> timeLineGroups.computeIfAbsent(workerId, createTimeLineGroup(nextGroupId::incrementAndGet));
+    }
+
+    private Function<String, TimeLineGroup> createTimeLineGroup(Supplier<Integer> nextGroupId) {
+        return workerId -> {
+            TimeLineGroup timeLineGroup = new TimeLineGroup();
+            timeLineGroup.setContent(workerId);
+            timeLineGroup.setId(nextGroupId.get());
+            return timeLineGroup;
+        };
+    }
+
+    private TimeLineItem createTestData(
+            TestCaseFinished testCaseFinished, TestCaseStarted testCaseStarted,
+            Function<String, TimeLineGroup> timeLineGroupCreator
+    ) {
+        String workerId = testCaseStarted.getWorkerId().orElse("");
+        TimeLineGroup timeLineGroup = timeLineGroupCreator.apply(workerId);
+        return createTestData(testCaseFinished, testCaseStarted, timeLineGroup);
+    }
+
+    private TimeLineItem createTestData(
+            TestCaseFinished testCaseFinished, TestCaseStarted testCaseStarted, TimeLineGroup timeLineGroup
+    ) {
+        TimeLineItem data = new TimeLineItem();
+        data.setId(testCaseStarted.getTestCaseId());
+        data.setFeature(getFeatureName(testCaseStarted));
+        data.setScenario(getPickleName(testCaseStarted));
+        data.setStart(Convertor.toInstant(testCaseStarted.getTimestamp()).toEpochMilli());
+        data.setTags(getTagsValue(testCaseStarted));
+        data.setGroup(timeLineGroup.getId());
+        data.setEnd(Convertor.toInstant(testCaseFinished.getTimestamp()).toEpochMilli());
+        data.setClassName(getTestStepStatusResult(testCaseFinished));
+        return data;
+    }
+
+    private String getTestStepStatusResult(TestCaseFinished event) {
+        return query.findMostSevereTestStepResultBy(event)
+                .map(TestStepResult::getStatus)
+                .map(TestStepResultStatus::value)
+                .map(s -> s.toLowerCase(ROOT))
+                // By definition
+                .orElse("passed");
+    }
+
+    private String getPickleName(TestCaseStarted testCaseStarted) {
+        return query.findPickleBy(testCaseStarted)
+                .map(Pickle::getName)
+                .orElse("");
+    }
+
+    private String getFeatureName(TestCaseStarted testCaseStarted) {
+        return query.findLineageBy(testCaseStarted)
+                .flatMap(Lineage::feature)
+                .map(Feature::getName)
+                .orElse("");
+    }
+
+    private String getTagsValue(TestCaseStarted testCaseStarted) {
+        return query.findPickleBy(testCaseStarted)
+                .map(pickle -> {
+                    StringBuilder tags = new StringBuilder();
+                    for (PickleTag tag : pickle.getTags()) {
+                        tags.append(tag.getName().toLowerCase()).append(",");
+                    }
+                    return tags.toString();
+                }).orElse("");
+    }
+
+    private void writeTimeLineReport(Map<String, TimeLineGroup> timeLineGroups, List<TimeLineItem> timeLineItems)
+            throws IOException {
+        writeReportJs(timeLineGroups, timeLineItems);
+        copyReportFiles();
+    }
+
+    private void writeReportJs(Map<String, TimeLineGroup> timeLineGroups, List<TimeLineItem> timeLineItems)
+            throws IOException {
+        File reportJsFile = new File(reportDir, "report.js");
+        try (BufferedWriter reportJs = Files.newBufferedWriter(reportJsFile.toPath(), StandardCharsets.UTF_8)) {
             reportJs.append("$(document).ready(function() {");
             reportJs.append("\n");
-            appendAsJsonToJs(reportJs, "timelineItems", allTests.values());
+            appendAsJsonToJs(reportJs, "timelineItems", timeLineItems);
             reportJs.append("\n");
             // Need to sort groups by id, so can guarantee output of order in
             // rendered timeline
-            appendAsJsonToJs(reportJs, "timelineGroups", new TreeMap<>(threadGroups).values());
+            appendAsJsonToJs(reportJs, "timelineGroups", new TreeMap<>(timeLineGroups).values());
             reportJs.append("\n");
             reportJs.append("});");
-            reportJs.close();
-            copyReportFiles();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 
-    private static String getId(TestCaseEvent testCaseEvent) {
-        return testCaseEvent.getTestCase().getId().toString();
-    }
-
     private void appendAsJsonToJs(
-            UTF8OutputStreamWriter out, String pushTo, Collection<?> content
+            BufferedWriter out, String pushTo, Collection<?> content
     ) throws IOException {
         out.append("CucumberHTML.").append(pushTo).append(".pushArray(");
         objectMapper.writeValue(out, content);
         out.append(");");
     }
 
-    private void copyReportFiles() {
+    private void copyReportFiles() throws IOException {
         if (reportDir == null) {
             return;
         }
         File outputDir = new File(reportDir.getPath());
         for (String textAsset : TEXT_ASSETS) {
-            InputStream textAssetStream = getClass().getResourceAsStream(textAsset);
-            if (textAssetStream == null) {
-                throw new CucumberException("Couldn't find " + textAsset);
+            try (InputStream textAssetStream = getClass().getResourceAsStream(textAsset)) {
+                if (textAssetStream == null) {
+                    throw new CucumberException("Couldn't find " + textAsset);
+                }
+                String fileName = new File(textAsset).getName();
+                copyFile(textAssetStream, new File(outputDir, fileName));
             }
-            String fileName = new File(textAsset).getName();
-            copyFile(textAssetStream, new File(outputDir, fileName));
-            closeQuietly(textAssetStream);
         }
     }
 
-    private static void copyFile(InputStream source, File dest) throws CucumberException {
-        OutputStream os = null;
-        try {
-            os = new FileOutputStream(dest);
+    private static void copyFile(InputStream source, File dest) throws IOException {
+        try (OutputStream os = Files.newOutputStream(dest.toPath())) {
             byte[] buffer = new byte[1024];
             int length;
             while ((length = source.read(buffer)) > 0) {
                 os.write(buffer, 0, length);
             }
-        } catch (IOException e) {
-            throw new CucumberException("Unable to write to report file item: ", e);
-        } finally {
-            closeQuietly(os);
         }
     }
 
-    private static void closeQuietly(Closeable out) {
-        try {
-            if (out != null) {
-                out.close();
-            }
-        } catch (IOException ignored) {
-            // go gentle into that good night
-        }
-    }
-
-    static class GroupData {
+    static class TimeLineGroup {
 
         private long id;
         private String content;
@@ -229,7 +275,7 @@ public final class TimelineFormatter implements ConcurrentEventListener {
 
     }
 
-    static class TestData {
+    static class TimeLineItem {
 
         private String id;
         private String feature;
