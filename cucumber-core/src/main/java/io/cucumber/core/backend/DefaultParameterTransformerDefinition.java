ca31982a5c47212205dde99adf33c389cf01582e
Stefan Gasterstädt <157380642+antagoony@users.noreply.github.com>
2025-11-23 23:43:34 +0100

    declare a locale aware creation of the ParameterByTypeTransformer

--- cucumber-core/src/main/java/io/cucumber/core/backend/DefaultParameterTransformerDefinition.java
+++ cucumber-core/src/main/java/io/cucumber/core/backend/DefaultParameterTransformerDefinition.java
@@ -3,9 +3,15 @@ package io.cucumber.core.backend;
 import io.cucumber.cucumberexpressions.ParameterByTypeTransformer;
 import org.apiguardian.api.API;
 
+import java.util.Locale;
+
 @API(status = API.Status.STABLE)
 public interface DefaultParameterTransformerDefinition extends Located {
 
     ParameterByTypeTransformer parameterByTypeTransformer();
+
+    default ParameterByTypeTransformer parameterByTypeTransformer(Locale locale) {
+        return this.parameterByTypeTransformer();
+    }
 
 }
