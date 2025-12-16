0dd91ad46e69b92cc48e5627a3deb1686e4958bd
Stefan Gasterstädt <157380642+antagoony@users.noreply.github.com>
2025-12-16 10:22:52 +0100

    insinuate cascading pico-provider construction

--- cucumber-picocontainer/src/test/java/io/cucumber/picocontainer/annotationconfig/ExamplePicoConfiguration.java
+++ cucumber-picocontainer/src/test/java/io/cucumber/picocontainer/annotationconfig/ExamplePicoConfiguration.java
@@ -1,7 +1,17 @@
 package io.cucumber.picocontainer.annotationconfig;
 
 import io.cucumber.picocontainer.CucumberPicoProvider;
+import org.picocontainer.injectors.Provider;
 
-@CucumberPicoProvider(providers = { URLConnectionProvider.class, DatabaseConnectionProvider.class })
+import java.net.URL;
+
+@CucumberPicoProvider(providers = { ExamplePicoConfiguration.NestedUrlProvider.class, URLConnectionProvider.class, DatabaseConnectionProvider.class })
 public class ExamplePicoConfiguration {
+
+    public static class NestedUrlProvider implements Provider {
+        public URL provide() {
+            throw new UnsupportedOperationException("Intentionally not supported to detect any premature injection.");
+        }
+    }
+
 }
--- cucumber-picocontainer/src/test/java/io/cucumber/picocontainer/annotationconfig/URLConnectionProvider.java
+++ cucumber-picocontainer/src/test/java/io/cucumber/picocontainer/annotationconfig/URLConnectionProvider.java
@@ -3,10 +3,11 @@ package io.cucumber.picocontainer.annotationconfig;
 import org.picocontainer.injectors.Provider;
 
 import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class URLConnectionProvider implements Provider {
 
-    public HttpURLConnection provide() {
+    public HttpURLConnection provide(URL url) {
         throw new UnsupportedOperationException("Intentionally not supported to detect any premature injection.");
     }
 
