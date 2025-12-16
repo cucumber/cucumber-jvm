02acb370c2d4694466845e60f697bf6d8e359c01
Stefan Gasterstädt <157380642+antagoony@users.noreply.github.com>
2025-12-16 10:15:24 +0100

    keep distinction of Provider/ProviderAdapter internally

--- cucumber-picocontainer/src/main/java/io/cucumber/picocontainer/PicoFactory.java
+++ cucumber-picocontainer/src/main/java/io/cucumber/picocontainer/PicoFactory.java
@@ -38,23 +38,19 @@ public final class PicoFactory implements ObjectFactory {
             Set<Class<?>> providers = new HashSet<>();
             Set<Class<?>> providedClasses = new HashSet<>();
             for (Class<?> clazz : classes) {
-                if (isProviderAdapter(clazz)) {
+                if (isProvider(clazz)) {
                     providers.add(clazz);
-                    Class<?> providedClass = addProviderAdapter(clazz);
-                    providedClasses.add(providedClass);
-                } else if (isProvider(clazz)) {
-                    providers.add(clazz);
-                    Class<?> providedClass = addProvider(clazz);
-                    providedClasses.add(providedClass);
+                    ProviderAdapter adapter = adapterForProviderClass(clazz);
+                    pico.addAdapter(adapter);
+                    providedClasses.add(adapter.getComponentImplementation());
                 }
             }
             for (Class<?> clazz : classes) {
                 // do not add the classes that represent a picocontainer
-                // ProviderAdapter/Provider, and also do not add those raw
-                // classes that are already provided (otherwise this causes
-                // exceptional situations, e.g. PicoCompositionException
-                // with message "Duplicate Keys not allowed. Duplicate for
-                // 'class XXX'")
+                // Provider, and also do not add those raw classes that are
+                // already provided (otherwise this causes exceptional
+                // situations, e.g. PicoCompositionException with message
+                // "Duplicate Keys not allowed. Duplicate for 'class XXX'")
                 if (!providers.contains(clazz) && !providedClasses.contains(clazz)) {
                     pico.addComponent(clazz);
                 }
@@ -70,30 +66,18 @@ public final class PicoFactory implements ObjectFactory {
         pico.start();
     }
 
-    static boolean isProviderAdapter(Class<?> clazz) {
-        return ProviderAdapter.class.isAssignableFrom(clazz);
-    }
-
-    private Class<?> addProviderAdapter(Class<?> clazz) {
-        try {
-            ProviderAdapter adapter = (ProviderAdapter) clazz.getDeclaredConstructor().newInstance();
-            pico.addAdapter(adapter);
-            return adapter.getComponentImplementation();
-        } catch (ReflectiveOperationException | IllegalArgumentException | SecurityException | PicoException e) {
-            throw new CucumberBackendException(e.getMessage(), e);
-        }
-    }
-
     static boolean isProvider(Class<?> clazz) {
         return Provider.class.isAssignableFrom(clazz);
     }
 
-    private Class<?> addProvider(Class<?> clazz) {
+    static boolean isProviderAdapter(Class<?> clazz) {
+        return ProviderAdapter.class.isAssignableFrom(clazz);
+    }
+
+    private static ProviderAdapter adapterForProviderClass(Class<?> clazz) {
         try {
             Provider provider = (Provider) clazz.getDeclaredConstructor().newInstance();
-            ProviderAdapter adapter = new ProviderAdapter(provider);
-            pico.addAdapter(adapter);
-            return adapter.getComponentImplementation();
+            return isProviderAdapter(clazz) ? (ProviderAdapter) provider : new ProviderAdapter(provider);
         } catch (ReflectiveOperationException | IllegalArgumentException | SecurityException | PicoException e) {
             throw new CucumberBackendException(e.getMessage(), e);
         }
