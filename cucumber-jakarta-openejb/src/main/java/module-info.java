e2dd7e740c407bfe3b83d5b0bc18adbeeda2e9c6
M.P. Korstanje <rien.korstanje@gmail.com>
2026-01-05 20:02:01 +0100

    WIP

--- cucumber-jakarta-openejb/src/main/java/module-info.java
+++ cucumber-jakarta-openejb/src/main/java/module-info.java
@@ -4,11 +4,15 @@ import io.cucumber.jakarta.openejb.OpenEJBObjectFactory;
 module io.cucumber.jakarta.cdi {
     requires org.jspecify;
 
+    requires io.cucumber.core;
+    requires jakartaee.api;
+    requires java.naming;
+    requires org.apache.tomee.container.core;
+
     requires transitive io.cucumber.datatable;
     requires transitive io.cucumber.docstring;
     requires transitive org.apiguardian.api;
 
-    requires io.cucumber.core;
     provides ObjectFactory
             with OpenEJBObjectFactory;
 
