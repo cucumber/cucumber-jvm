e9d22f3b59ef1d4cd514b1a71f468c88178e3043
M.P. Korstanje <rien.korstanje@gmail.com>
2026-06-03 01:09:13 +0200

    Reduce ceremony in archetype

--- cucumber-archetype/src/test/resources/projects/should-generate-project/reference/src/test/java/com/example/StepDefinitions.java
+++ cucumber-archetype/src/test/resources/projects/should-generate-project/reference/src/test/java/com/example/StepDefinitions.java
@@ -7,15 +7,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StepDefinitions {
 
     @Given("an example scenario")
-    public void anExampleScenario() {
+    void anExampleScenario() {
     }
 
     @When("all step definitions are implemented")
-    public void allStepDefinitionsAreImplemented() {
+    void allStepDefinitionsAreImplemented() {
     }
 
     @Then("the scenario passes")
-    public void theScenarioPasses() {
+    void theScenarioPasses() {
     }
 
 }
