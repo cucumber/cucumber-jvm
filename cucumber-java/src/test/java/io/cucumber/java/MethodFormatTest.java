09069330a963f3ab22f3785adc1378454e77f2cf
M.P. Korstanje <rien.korstanje@gmail.com>
2026-06-03 01:08:15 +0200

    Support more methods for formatting

--- cucumber-java/src/test/java/io/cucumber/java/MethodFormatTest.java
+++ cucumber-java/src/test/java/io/cucumber/java/MethodFormatTest.java
@@ -8,13 +8,15 @@ import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
 
-import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 class MethodFormatTest {
 
     private Method methodWithArgsAndException;
     private Method methodWithoutArgs;
+    private Method packagePrivateMethod;
+    private Method protectedMethod;
 
     public void methodWithoutArgs() {
     }
@@ -23,23 +25,45 @@ class MethodFormatTest {
         return null;
     }
 
+    void packagePrivateMethod() {
+
+    }
+
+    protected void protectedMethod() {
+
+    }
+
     @BeforeEach
     void lookupMethod() throws NoSuchMethodException {
         this.methodWithoutArgs = this.getClass().getMethod("methodWithoutArgs");
         this.methodWithArgsAndException = this.getClass().getMethod("methodWithArgsAndException", String.class,
             Map.class);
+        this.packagePrivateMethod = this.getClass().getDeclaredMethod("packagePrivateMethod");
+        this.protectedMethod = this.getClass().getDeclaredMethod("protectedMethod");
     }
 
     @Test
-    void shouldUseSimpleFormatWhenMethodHasException() {
+    void formatPublicMethod() {
         assertThat(MethodFormat.FULL.format(methodWithoutArgs),
-            startsWith("io.cucumber.java.MethodFormatTest.methodWithoutArgs()"));
+            equalTo("io.cucumber.java.MethodFormatTest.methodWithoutArgs()"));
     }
 
     @Test
-    void shouldUseSimpleFormatWhenMethodHasNoException() {
+    void formatPackagePrivateMethod() {
+        assertThat(MethodFormat.FULL.format(packagePrivateMethod),
+            equalTo("io.cucumber.java.MethodFormatTest.packagePrivateMethod()"));
+    }
+
+    @Test
+    void formatProtectedMethod() {
+        assertThat(MethodFormat.FULL.format(protectedMethod),
+            equalTo("io.cucumber.java.MethodFormatTest.protectedMethod()"));
+    }
+
+    @Test
+    void formatMethodWithExceptionsAndArguments() {
         assertThat(MethodFormat.FULL.format(methodWithArgsAndException),
-            startsWith(
+            equalTo(
                 "io.cucumber.java.MethodFormatTest.methodWithArgsAndException(java.lang.String,java.util.Map<?, ?>)"));
     }
 
